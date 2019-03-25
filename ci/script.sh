aad486bdb05c8c88d099c533fd649dadfe4eb4a8
satellitex <s1220245@u-aizu.ac.jp>
2019-03-25 23:32:13 +0900

    add travis.yaml and ci script (#7)
    * add travis.yaml and ci script
    
    * add compact.wasm file

---
+++ ci/script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
+# Install rustup and the specified rust toolchain.
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
+
+# Load cargo environment. Specifically, put cargo into PATH.
+source ~/.cargo/env
+
+rustc --version
+rustup --version
+cargo --version
+
+case $TARGET in
+	"native")
+		sudo apt-get -y update
+		sudo apt-get install -y cmake pkg-config libssl-dev
+
+		cargo test --all --release --locked
+		;;
+
+	"wasm")
+		# Install prerequisites and build all wasm projects
+		./init.sh
+		./build.sh
+		cd runtime
+		cargo test --all
+		;;
+esac
