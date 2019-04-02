07611673e6c9a3d0c27ec63d001cdf14704f9ceb
satellitex <s.a.t.e.3.ths@gmail.com>
2019-04-02 23:53:01 +0900

    impl mvp utxo [WIP]

--- core/utxo/src/mvp.rs
+++ core/utxo/src/mvp.rs
@@ -10,6 +10,66 @@
 
 use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
 use system::ensure_signed;
+use primitives::{Blake2Hasher, H256};
+// use Vec<>
+use rstd::prelude::*;
+// use Encode, Decode
+use parity_codec::{Encode, Decode};
+use serde::{Serialize, Deserialize};
+
+/// Single transaction to be dispatched
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
+pub struct Transaction {
+	/// UTXOs to be used as inputs for current transaction
+	pub inputs: Vec<TransactionInput>,
+	/// UTXOs to be created as a result of current transaction dispatch
+	pub outputs: Vec<TransactionOutput>,
+}
+
+/// Single transaction input that refers to one UTXO
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
+pub struct TransactionInput {
+	/// Reference to an UTXO to be spent
+	pub parent_output: H256,
+	/// Proof that transaction owner is authorized to spend referred UTXO
+	pub signature: Signature,
+}
+
+/// Single transaction output to create upon transaction dispatch
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
+pub struct TransactionOutput {
+	/// Value associated with this output
+	pub value: Value,
+	/// Public key associated with this output. In order to spend this output
+    /// owner must provide a proof by hashing whole `TransactionOutput` and
+    /// signing it with a corresponding private key.
+	pub pubkey: H256,
+	/// Unique (potentially random) value used to distinguish this
+    /// particular output from others addressed to the same public
+    /// key with the same value. Prevents potential replay attacks.
+	pub salt: u32,
+}
+
+/// Information collected during transaction verification
+pub enum CheckInfo<'a> {
+	/// Combined value of all inputs and outputs
+	Totals { input: Value, output: Value },
+	/// Some referred UTXOs were missing
+	MissingInputs(Vec<&'a H256>),
+}
+
+/// Result of transaction verification
+pub type CheckResult<'a> = rstd::result::Result<CheckInfo<'a>, &'static str>;
+
+
+/// Check transaction for validity
+pub fn check_transaction(transaction: &Transaction) -> CheckResult<'_> {
+	// check
+}
+
 
 /// The module's configuration trait.
 pub trait Trait: system::Trait {
@@ -17,15 +77,55 @@ pub trait Trait: system::Trait {
 
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+	type Consensus: consensus::Authorities<<A as AuthoringApi>::Block> + Send + Sync + 'static;
 }
 
 /// This module's storage items.
 decl_storage! {
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
+	trait Store for Module<T: Trait> as Utxo {
+		/// All valid unspent transaction outputs are stored in this map.
+		/// Initial set of UTXO is populated from the list stored in genesis.
+		UnspentOutputs build(|config: &GenesisConfig<T>| {
+			config.initial_utxo
+				.iter()
+				.cloned()
+				.map(|u| (BlakeTwo256::hash_of(&u), u))
+				.collect::<Vec<_>>()
+		}): map H256 => Option<TransactionOutput>;
+
+		/// Total leftover value to be redistributed among authorities.
+		/// It is accumulated during block execution and then drained
+		/// on block finalization.
+		LeftoverTotal: Value;
+
+		/// Outputs that are locked
+		LockedOutputs: map H256 => Option<LockStatus<T::BlockNumber>>;
+	}
+
+	add_extra_genesis {
+		config(initial_utxo): Vec<TransactionOutput>;
+	}
+}
+
+/// Redistribute combined leftover value evenly among authorities
+fn spend_leftover(authorities: &[H256]) {
+	let leftover = <LeftoverTotal<T>>::take();
+	let share_value = leftover / authorities.len() as Value;
+	if share_value == 0 { return }
+	for authority in authorities {
+		let utxo = TransactionOutput {
+			pubkey: *authority,
+			value: share_value,
+			salt: System::block_number() as u32,
+		};
+		let hash = BlakeTwo256::hash_of(&utxo);
+		if !<UnspentOutputs<T>>::exists(hash) {
+			<UnspentOutputs<T>>::insert(hash, utxo);
+			runtime_io::print("leftover share sent to");
+			runtime_io::print(hash.as_fixed_bytes() as &[u8]);
+		} else {
+			runtime_io::print("leftover hash collision");
+		}
 	}
 }
 
@@ -36,19 +136,45 @@ decl_module! {
 		// this is needed only if you are using events in your module
 		fn deposit_event<T>() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		/// Dispatch a single transaction and update UTXO set accordingly
+		pub fn execute(origin, transaction: Transaction) -> Result {
+			ensure_inherent(origin)?;
+			let leftover = match Self::check_transaction(&transaction)? {
+				CheckInfo::MissingInputs(_) => return Err("output missing"),
+				CheckInfo::Totals { input, output } => input - output
+			};
+			Self::update_storage(&transaction, leftover)?;
+			Self::deposit_event(Event::TransactionExecuted(transaction));
+			Ok(())
+		}
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			<Something<T>>::put(something);
+		/// Handler called by the system on block finalization
+		pub fn on_finalise() {
+			let authorities: Vec<_> = consensus::authorities()
+				.iter().map(|&a| a.into()).collect();
+			Self::spend_leftover(&authorities);
+		}
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+		/// Update storage to reflect changes made by transaction
+		pub fn update_storage(transaction: &Transaction, leftover: Value) -> Result {
+			/// Calculate new leftover total
+			let new_total = <LeftoverTotal<T>>::get()
+				.checked_add(leftover)
+				.ok_or("leftover overflow")?;
+
+			/// Storing updated leftover value
+			<LeftoverTotal<T>>::put(new_total);
+
+			/// Remove all used UTXO since they are now spent
+			for input in &transaction.inputs {
+				<UnspentOutputs<T>>::remove(input.parent_output);
+			}
+
+			/// Add new UTXO to be used by future transactions
+			for output in &transaction.outputs {
+				let hash = T::Hashing::hash_of(output);
+				<UnspentOutputs<T>>::insert(hash, output);
+			}
 			Ok(())
 		}
 	}
@@ -57,10 +183,8 @@ decl_module! {
 decl_event!(
 	/// An event in this module.
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		// Just a dummy event.
-		// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		// To emit this event, we call the deposit funtion, from our runtime funtions
-		SomethingStored(u32, AccountId),
+		/// Transaction was executed successfully
+		TransactionExecuted(Transaction),
 	}
 );
 
