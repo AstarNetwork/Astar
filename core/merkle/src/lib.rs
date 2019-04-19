62c066c71970890112f2a8a851b1b70781bd1882
satellitex <s.a.t.e.3.ths@gmail.com>
2019-04-19 17:39:46 +0900

    WIP: impl merkledb

--- core/merkle/src/lib.rs
+++ core/merkle/src/lib.rs
@@ -1,17 +1,18 @@
 use rstd::marker::PhantomData;
 use rstd::prelude::*;
 use sr_primitives::traits::{SimpleBitOps, Zero};
+use support::storage::child;
 
 pub trait ConcatHash {
-	fn concat<T>(a: T, b: T) -> T where T: SimpleBitOps + Zero + Eq ;
+	fn concat<T>(a: T, b: T) -> T where T: SimpleBitOps + Zero + Eq;
 }
 
 pub struct MockConcatHasher;
 
 impl ConcatHash for MockConcatHasher {
 	fn concat<T>(a: T, b: T) -> T where T: SimpleBitOps + Zero + Eq {
-		if a == T::zero() { return b }
-		if b == T::zero() { return a }
+		if a == T::zero() { return b; }
+		if b == T::zero() { return a; }
 		a ^ b
 	}
 }
@@ -31,7 +32,7 @@ impl<H, O> MerkleTreeTrait<H, O> for MerkleTree<H, O> {
 	fn root() -> H {
 		H()
 	}
-	fn proofs(leaf: O) -> Self::Proofs {Self::Proofs()}
+	fn proofs(leaf: O) -> Self::Proofs { Self::Proofs() }
 	fn push(leaf: O) {}
 }
 
@@ -40,6 +41,28 @@ enum MockProofs<T> {
 	Node(Box<MockProofs<T>>, Box<MockProofs<T>>),
 }
 
+pub struct MerkleDb<O, Id, Key>(PhantomData<O, Id, Key>);
+
+impl<O, Id, Key> MerkleDb<O, Id, Key>
+	where
+		O: Into<&[u8]>,
+		Id: Into<&[u8]>,
+		key: Into<&[u8]>,
+{
+	fn push(o: O, trie_id: Id, key: Key) {
+		child::put_raw(o.into(), trie_id.into(), key.into());
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn merkle_mock_test() {
+		assert!(true)
+	}
+}
 // 完全二分木による実装。
 // SegmentTree っぽく予め 2*n のデータをとっておく。
 // 利点：実装が楽。
