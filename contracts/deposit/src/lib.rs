4adaedde7cc2659cb493f7cf34b5a6a854b8502e
Task Ohmori <tascii.oh@gmail.com>
2019-07-10 17:29:05 +0900

    use call

--- contracts/deposit/src/lib.rs
+++ contracts/deposit/src/lib.rs
@@ -6,6 +6,7 @@ use ink_core::{
 };
 use ink_lang::contract;
 use primitives::*;
+use ink_model::gen_input_data::gen_input_data;
 
 contract! {
     #![env = ink_core::env::DefaultSrmlTypes]
@@ -57,29 +58,34 @@ contract! {
 
     impl Deposit {
         pub(external) fn deposit(&mut self, depositer : AccountId, amount : Balance, initial_state : StateObject){
-            // //MUST keep track of the total deposited assets, totalDeposited.
-            // //MUST transfer the deposited amount from the depositer to the deposit contract’s address.
-            // //MUST create a state update with a state object equal to the provided initialState.
-            // //MUST compute the range of the created state update as totalDeposited to totalDeposited + amount.
-            // //MUST update the total amount deposited after the deposit is handled.
-            // *self.total_deposited = *self.total_deposited + amount;
-            //
-            // //MUST insert the created state update into the checkpoints mapping with challengeableUntil being the current block number - 1.
-            // let state_update = StateUpdate{
-            //     range : Range,
-            //     state_object : initial_state,
-            //     plasma_contract,
-            //     plasma_block_number,
-            // };
-            //
-            //
-            // //MUST emit a CheckpointFinalized event for the inserted checkpoint.
-            // env.emit(
-            //     CheckpointFinalized{
-            //         checkpoint,
-            //     }
-            // );
-            unimplemented!();
+            //MUST keep track of the total deposited assets, totalDeposited.
+            //MUST transfer the deposited amount from the depositer to the deposit contract’s address.
+			let params = vec!([Box::new(depositer),Box::new(env.address()),Box::new(amount)]);
+			let sent:bool = env.call(TOKEN_ADDRES,0,0,gen_input_data("transfer_from",params));
+
+            //MUST create a state update with a state object equal to the provided initialState.
+			let state_update = StateUpdate{
+				range : Range,
+				state_object : initial_state,
+				plasma_contract,
+				plasma_block_number,
+			};
+			//MUST compute the range of the created state update as totalDeposited to totalDeposited + amount.
+
+
+            //MUST update the total amount deposited after the deposit is handled.
+            *self.total_deposited = *self.total_deposited + amount;
+
+            //MUST insert the created state update into the checkpoints mapping with challengeableUntil being the current block number - 1.
+
+
+
+            //MUST emit a CheckpointFinalized event for the inserted checkpoint.
+            env.emit(
+                CheckpointFinalized{
+                    checkpoint,
+                }
+            );
         }
 
     }
