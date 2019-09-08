ace0154397f56ebf62d2fa3c65e0d24ff11292a3
satellitex <s.a.t.e.3.ths@gmail.com>
2019-09-08 18:59:14 +0900

    fix hotfix (#66)

--- node/cli/build.rs
+++ node/cli/build.rs
@@ -46,5 +46,5 @@ fn build_completion(shell: &Shell) {
 
 	fs::create_dir(&path).ok();
 
-	CoreParams::<NoCustom, NoCustom>::clap().gen_completions("substrate-node", *shell, &path);
+	CoreParams::<NoCustom, NoCustom>::clap().gen_completions("plasm-node", *shell, &path);
 }
--- node/runtime/build.rs
+++ node/runtime/build.rs
@@ -1,5 +1,11 @@
-use wasm_builder_runner::{build_current_project, WasmBuilderSource};
+use wasm_builder_runner::{build_current_project_with_rustflags, WasmBuilderSource};
 
 fn main() {
-	build_current_project("wasm_binary.rs", WasmBuilderSource::Crates("1.0.5"));
+	build_current_project_with_rustflags(
+		"wasm_binary.rs",
+		WasmBuilderSource::Crates("1.0.5"),
+		// This instructs LLD to export __heap_base as a global variable, which is used by the
+		// external memory allocator.
+		"-Clink-arg=--export=__heap_base",
+	);
 }
