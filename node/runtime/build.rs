b0f88c5921fee13bdb4c011ff69ba56dce250039
satellitex <s.a.t.e.3.ths@gmail.com>
2019-09-27 21:13:04 +0900

    fix wasm-build version

--- node/runtime/build.rs
+++ node/runtime/build.rs
@@ -3,7 +3,10 @@ use wasm_builder_runner::{build_current_project_with_rustflags, WasmBuilderSourc
 fn main() {
 	build_current_project_with_rustflags(
 		"wasm_binary.rs",
-		WasmBuilderSource::Crates("1.0.5"),
+		WasmBuilderSource::Git {
+			repo: "https://github.com/satellitex/substrate.git",
+			rev: "d2e7d660f8dbbb8f9753dfd231cef1c9b502c41c",
+		},
 		// This instructs LLD to export __heap_base as a global variable, which is used by the
 		// external memory allocator.
 		"-Clink-arg=--export=__heap_base",
