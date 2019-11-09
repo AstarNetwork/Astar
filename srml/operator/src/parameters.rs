aebff1bbeebde5f5eba2373eaf7b70b0b1c094e9
satellitex <s.a.t.e.3.ths@gmail.com>
2019-11-10 00:39:30 +0900

    fix grammary

--- srml/operator/src/parameters.rs
+++ srml/operator/src/parameters.rs
@@ -3,19 +3,19 @@ use codec::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
 pub trait Verifiable {
-    fn verify(&self) -> Result;
+	fn verify(&self) -> Result;
 }
 
 #[derive(Clone, Eq, PartialEq, Default, Encode, Decode, Hash)]
 #[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
 pub struct DefaultParameters {
-    pub can_be_nominated: bool,
-    pub option_expired: u128,
-    pub option_p: u128,
+	pub can_be_nominated: bool,
+	pub option_expired: u128,
+	pub option_p: u128,
 }
 
 impl Verifiable for DefaultParameters {
-    fn verify(&self) -> Result {
-        Ok(())
-    }
+	fn verify(&self) -> Result {
+		Ok(())
+	}
 }
