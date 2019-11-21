354343f554ff7cc6bca188ed5f7b54e6209f0ab2
satellitex <s.a.t.e.3.ths@gmail.com>
2019-11-22 02:13:36 +0900

    passed once_cases

--- srml/faucet/src/lib.rs
+++ srml/faucet/src/lib.rs
@@ -4,7 +4,11 @@ use codec::{Decode, Encode};
 use rstd::prelude::*;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
-use support::{decl_event, decl_module, decl_storage, dispatch::Result, traits::{Currency, Get}};
+use support::{
+	decl_event, decl_module, decl_storage,
+	dispatch::Result,
+	traits::{Currency, Get},
+};
 use system::ensure_signed;
 
 /// The module's configuration trait.
@@ -16,7 +20,7 @@ pub trait Trait: balances::Trait + timestamp::Trait {
 	type WaitingClaims: Get<Self::Moment>;
 
 	// A faucet can get value.
-	type FaucetValue: Get<Self::Balance>;
+	type FaucetValue: Get<BalanceOf<Self>>;
 
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -29,13 +33,13 @@ pub struct FaucetLog<Balance, Moment> {
 	pub time: Moment,
 }
 
-pub type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+pub type BalanceOf<T> =
+<<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
 
-/// This module's storage items.
 decl_storage! {
     trait Store for Module<T: Trait> as Operator {
         /// A mapping from operators to operated contracts by them.
-        pub FaucetHistory: map T::AccountId => Vec<FaucetLog<T::Balance, T::Moment>>;
+        pub FaucetHistory: map T::AccountId => Vec<FaucetLog<BalanceOf<T>, T::Moment>>;
     }
 }
 
@@ -48,20 +52,27 @@ decl_module! {
 
         /// claim
         pub fn claims(origin, ) -> Result {
-//        	let sender = ensure_signed(origin)?;
-//        	let mut history = <FaucetHistory<T>>::get(&sender);
-//        	let now = <timestamp::Module<T>>::now();
-//        	if history.empty() || history.back().time + T::WaitingClaims::get() < now {
-//        		// can get claims
-//        		history.push(
-//        			FaucetLog {
-//        				amount: T::FaucetValue::get(),
-//        				time: now,
-//        			});
-//				FaucetHistory.insert(&sender, history);
-//				// add balance of sender
-//
-//        	}
+            let sender = ensure_signed(origin)?;
+            let mut history = <FaucetHistory<T>>::get(&sender);
+            let now = <timestamp::Module<T>>::now();
+            if history.is_empty() || history.last().unwrap().time + T::WaitingClaims::get() < now {
+                // can get claims
+                history.push(
+                    FaucetLog {
+                        amount: T::FaucetValue::get(),
+                        time: now,
+                    });
+                <FaucetHistory<T>>::insert(&sender, history);
+                // get free balance.
+                let free_balance = T::Currency::free_balance(&sender);
+                // add balance of sender
+                T::Currency::make_free_balance_be(&sender, free_balance + T::FaucetValue::get());
+                // issues success events.
+                Self::deposit_event(RawEvent::SuccessClaims(sender, T::FaucetValue::get(), now));
+                return Ok(())
+            }
+            // issues failed events.
+            Self::deposit_event(RawEvent::FailedClaims(sender, history.last().unwrap().time));
             Ok(())
         }
     }
@@ -71,7 +82,7 @@ decl_event!(
     pub enum Event<T>
     where
         AccountId = <T as system::Trait>::AccountId,
-        Balance = <T as balances::Trait>::Balance,
+        Balance = BalanceOf<T>,
         Moment = <T as timestamp::Trait>::Moment,
     {
         /// SuccessClaims (sender, amount of faucet, faucet time)
@@ -86,8 +97,8 @@ mod tests {
 	use super::*;
 
 	use primitives::H256;
-	use support::{assert_ok, impl_outer_origin, impl_outer_event, parameter_types};
 	use std::cell::RefCell;
+	use support::{assert_ok, impl_outer_event, impl_outer_origin, parameter_types};
 	use system::{EventRecord, Phase};
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
@@ -106,13 +117,12 @@ mod tests {
 		// needs to give a name for the current crate.
 		// This hack is required for `impl_outer_event!`.
 		pub use super::super::*;
-		use support::impl_outer_event;
 	}
 	impl_outer_event! {
-		pub enum MetaEvent for Test {
-			balances<T>, faucet<T>,
-		}
-	}
+        pub enum MetaEvent for Test {
+            balances<T>, faucet<T>,
+        }
+    }
 
 	// For testing the module, we construct most of a mock runtime. This means
 	// first constructing a configuration type (`Test`) which `impl`s each of the
@@ -144,24 +154,33 @@ mod tests {
 	}
 
 	thread_local! {
-		static EXISTENTIAL_DEPOSIT: RefCell<u64> = RefCell::new(0);
-		static TRANSFER_FEE: RefCell<u64> = RefCell::new(0);
-		static CREATION_FEE: RefCell<u64> = RefCell::new(0);
-	}
+        static EXISTENTIAL_DEPOSIT: RefCell<u64> = RefCell::new(0);
+        static TRANSFER_FEE: RefCell<u64> = RefCell::new(0);
+        static CREATION_FEE: RefCell<u64> = RefCell::new(0);
+    }
 
 	pub struct ExistentialDeposit;
+
 	impl Get<u64> for ExistentialDeposit {
-		fn get() -> u64 { EXISTENTIAL_DEPOSIT.with(|v| *v.borrow()) }
+		fn get() -> u64 {
+			EXISTENTIAL_DEPOSIT.with(|v| *v.borrow())
+		}
 	}
 
 	pub struct TransferFee;
+
 	impl Get<u64> for TransferFee {
-		fn get() -> u64 { TRANSFER_FEE.with(|v| *v.borrow()) }
+		fn get() -> u64 {
+			TRANSFER_FEE.with(|v| *v.borrow())
+		}
 	}
 
 	pub struct CreationFee;
+
 	impl Get<u64> for CreationFee {
-		fn get() -> u64 { CREATION_FEE.with(|v| *v.borrow()) }
+		fn get() -> u64 {
+			CREATION_FEE.with(|v| *v.borrow())
+		}
 	}
 
 	impl balances::Trait for Test {
@@ -176,8 +195,8 @@ mod tests {
 		type CreationFee = CreationFee;
 	}
 	parameter_types! {
-		pub const MinimumPeriod: u64 = 5;
-	}
+        pub const MinimumPeriod: u64 = 5;
+    }
 	impl timestamp::Trait for Test {
 		type Moment = u64;
 		type OnTimestampSet = ();
@@ -203,6 +222,7 @@ mod tests {
 		monied: bool,
 		vesting: bool,
 	}
+
 	impl Default for ExtBuilder {
 		fn default() -> Self {
 			Self {
@@ -214,6 +234,7 @@ mod tests {
 			}
 		}
 	}
+
 	impl ExtBuilder {
 		pub fn existential_deposit(mut self, existential_deposit: u64) -> Self {
 			self.existential_deposit = existential_deposit;
@@ -224,19 +245,11 @@ mod tests {
 			self.transfer_fee = transfer_fee;
 			self
 		}
-		pub fn creation_fee(mut self, creation_fee: u64) -> Self {
-			self.creation_fee = creation_fee;
-			self
-		}
 		pub fn monied(mut self, monied: bool) -> Self {
 			self.monied = monied;
 			if self.existential_deposit == 0 {
 				self.existential_deposit = 1;
 			}
-			self
-		}
-		pub fn vesting(mut self, vesting: bool) -> Self {
-			self.vesting = vesting;
 			self
 		}
 		pub fn set_associated_consts(&self) {
@@ -246,7 +259,9 @@ mod tests {
 		}
 		pub fn build(self) -> sr_io::TestExternalities {
 			self.set_associated_consts();
-			let mut t = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+			let mut t = system::GenesisConfig::default()
+				.build_storage::<Test>()
+				.unwrap();
 			balances::GenesisConfig::<Test> {
 				balances: if self.monied {
 					vec![
@@ -254,7 +269,7 @@ mod tests {
 						(2, 20 * self.existential_deposit),
 						(3, 30 * self.existential_deposit),
 						(4, 40 * self.existential_deposit),
-						(12, 10 * self.existential_deposit)
+						(12, 10 * self.existential_deposit),
 					]
 				} else {
 					vec![]
@@ -263,12 +278,14 @@ mod tests {
 					vec![
 						(1, 0, 10, 5 * self.existential_deposit),
 						(2, 10, 20, 0),
-						(12, 10, 20, 5 * self.existential_deposit)
+						(12, 10, 20, 5 * self.existential_deposit),
 					]
 				} else {
 					vec![]
 				},
-			}.assimilate_storage(&mut t).unwrap();
+			}
+				.assimilate_storage(&mut t)
+				.unwrap();
 			t.into()
 		}
 	}
@@ -281,7 +298,10 @@ mod tests {
 	// This function basically just builds a genesis storage key/value store according to
 	// our desired mockup.
 	fn new_test_ext() -> sr_io::TestExternalities {
-		ExtBuilder::default().existential_deposit(1).monied(true).build()
+		ExtBuilder::default()
+			.existential_deposit(1)
+			.monied(true)
+			.build()
 	}
 
 	#[test]
@@ -292,21 +312,28 @@ mod tests {
 			assert_ok!(Faucet::claims(Origin::signed(1)));
 			assert_eq!(
 				System::events(),
-				vec![
-				EventRecord {
+				vec![EventRecord {
 					phase: Phase::ApplyExtrinsic(0),
-					event: MetaEvent::faucet(faucet::RawEvent::SuccessClaims(1, <Test as Trait>::FaucetValue::get(), Timestamp::now())),
+					event: MetaEvent::faucet(faucet::RawEvent::SuccessClaims(
+						1,
+						<Test as Trait>::FaucetValue::get(),
+						Timestamp::now(),
+					)),
 					topics: vec![],
 				}],
 			);
 
-			assert_eq!(<balances::FreeBalance<Test>>::get(&1), 10 + <Test as Trait>::FaucetValue::get());
-			assert_eq!(<FaucetHistory<Test>>::get(&1), vec![
-				FaucetLog {
+			assert_eq!(
+				<balances::FreeBalance<Test>>::get(&1),
+				10 + <Test as Trait>::FaucetValue::get()
+			);
+			assert_eq!(
+				<FaucetHistory<Test>>::get(&1),
+				vec![FaucetLog {
 					amount: <Test as Trait>::FaucetValue::get(),
 					time: Timestamp::now(),
-				}
-			]);
+				}]
+			);
 		});
 	}
 }
