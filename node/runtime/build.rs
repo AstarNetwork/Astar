015faa617223a684dd7dfab217f7fc60a1624c85
Alexander Krupenkin <mail@akru.me>
2019-11-24 22:10:32 +0900

    Fix version of used substrate (#81)
    * Fork substrate repository to organisation
    * Fix commit of used substrate in plasm-master branch

--- node/runtime/build.rs
+++ node/runtime/build.rs
@@ -3,10 +3,7 @@ use wasm_builder_runner::{build_current_project_with_rustflags, WasmBuilderSourc
 fn main() {
 	build_current_project_with_rustflags(
 		"wasm_binary.rs",
-		WasmBuilderSource::Git {
-			repo: "https://github.com/satellitex/substrate.git",
-			rev: "97643d4639868119550bd7ef6d824729cd6e587a",
-		},
+		WasmBuilderSource::Crates("1.0.8"),
 		// This instructs LLD to export __heap_base as a global variable, which is used by the
 		// external memory allocator.
 		"-Clink-arg=--export=__heap_base",
