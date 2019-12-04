30382e26e7670287bc7579abc0243a6b8f431374
satellitex <s.a.t.e.3.ths@gmail.com>
2019-12-05 04:32:00 +0900

    only use latest nightly build

--- third-party/scripts/init.sh
+++ third-party/scripts/init.sh
@@ -6,10 +6,10 @@ echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
    rustup update nightly
-   rustup update stable
 fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default nightly
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
