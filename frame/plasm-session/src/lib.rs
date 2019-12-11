313cf7ab077d6b4b42942c41d956d5595d648bf2
satellitex <s.a.t.e.3.ths@gmail.com>
2019-12-11 19:11:48 +0900

    WIP: add session-manager

---
+++ frame/plasm-session/src/lib.rs
@@ -0,0 +1,167 @@
+use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
+use system::ensure_signed;
+
+pub trait Trait: system::Trait {
+	/// The balance using rewards.
+	type Currency: LockableCurrency<Self::AccountId, Moment=Self::BlockNumber>;
+
+	/// Time used for computing era duration.
+	type Time: Time;
+
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+
+	/// Handler for the unbalanced reduction when slashing a staker.
+	type Slash: OnUnbalanced<NegativeImbalanceOf<Self>>;
+
+	/// Handler for the unbalanced increment when rewarding a staker.
+	type Reward: OnUnbalanced<PositiveImbalanceOf<Self>>;
+
+	/// Number of sessions per era.
+	type SessionsPerEra: Get<SessionIndex>;
+
+	/// Number of eras that slashes are deferred by, after computation. This
+	/// should be less than the bonding duration. Set to 0 if slashes should be
+	/// applied immediately, without opportunity for intervention.
+	type SlashDeferDuration: Get<EraIndex>;
+
+	/// The origin which can cancel a deferred slash. Root can always do this.
+	type SlashCancelOrigin: EnsureOrigin<Self::Origin>;
+
+	/// Interface for interacting with a session module.
+	type SessionInterface: self::SessionInterface<Self::AccountId>;
+
+	/// Handler for when a session is about to end.
+	type OnSessionEnding: OnSessionEnding<Self::ValidatorId>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as poa-rewards {
+		/// Any validators that may never be slashed or forcibly kicked. It's a Vec since they're
+		/// easy to initialize and the performance hit is minimal (we expect no more than four
+		/// invulnerables) and restricted to testnets.
+		pub Invulnerables get(fn invulnerables) config(): Vec<T::AccountId>;
+
+		/// The percentage of the slash that is distributed to reporters.
+		///
+		/// The rest of the slashed value is handled by the `Slash`.
+		pub SlashRewardFraction get(fn slash_reward_fraction) config(): Perbill;
+
+		/// The amount of currency given to reporters of a slash event which was
+		/// canceled by extraordinary circumstances (e.g. governance).
+		pub CanceledSlashPayout get(fn canceled_payout) config(): BalanceOf<T>;
+
+		/// All unapplied slashes that are queued for later.
+		pub UnappliedSlashes: map EraIndex => Vec<UnappliedSlash<T::AccountId, BalanceOf<T>>>;
+
+		/// All slashing events on validators, mapped by era to the highest slash proportion
+		/// and slash value of the era.
+		ValidatorSlashInEra:
+			double_map EraIndex, twox_128(T::AccountId) => Option<(Perbill, BalanceOf<T>)>;
+
+		/// Slashing spans for stash accounts.
+		SlashingSpans: map T::AccountId => Option<slashing::SlashingSpans>;
+
+		/// Records information about the maximum slash of a stash within a slashing span,
+		/// as well as how much reward has been paid out.
+		SpanSlash:
+			map (T::AccountId, slashing::SpanIndex) => slashing::SpanRecord<BalanceOf<T>>;
+
+		/// The earliest era for which we have a pending, unapplied slash.
+		EarliestUnappliedSlash: Option<EraIndex>;
+
+		/// The version of storage for upgrade.
+		StorageVersion: u32;
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		/// Number of sessions per era.
+		const SessionsPerEra: SessionIndex = T::SessionsPerEra::get();
+
+		fn deposit_event() = default;
+
+		fn on_initialize() {
+			Self::ensure_storage_upgraded();
+		}
+
+		fn on_finalize() {
+			// Set the start of the first era.
+			if !<CurrentEraStart<T>>::exists() {
+				<CurrentEraStart<T>>::put(T::Time::now());
+			}
+		}
+	}
+}
+
+decl_event!(
+	pub enum Event<T> where Balance = BalanceOf<T>, <T as system::Trait>::AccountId {
+		/// All validators have been rewarded;
+		Reward(Balance),
+		/// One validator has been slashed by the given amount.
+		Slash(AccountId, Balance),
+		/// An old slashing report from a prior era was discarded because it could
+		/// not be processed.
+		OldSlashingReportDiscarded(SessionIndex),
+	}
+);
+
+/// tests for this module
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use runtime_io::with_externalities;
+	use primitives::{H256, Blake2Hasher};
+	use support::{impl_outer_origin, assert_ok};
+	use runtime_primitives::{
+		BuildStorage,
+		traits::{BlakeTwo256, IdentityLookup},
+		testing::{Digest, DigestItem, Header}
+	};
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	// For testing the module, we construct most of a mock runtime. This means
+	// first constructing a configuration type (`Test`) which `impl`s each of the
+	// configuration traits of modules we want to use.
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type Digest = Digest;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = ();
+		type Log = DigestItem;
+	}
+	impl Trait for Test {
+		type Event = ();
+	}
+	type poa-rewards = Module<Test>;
+
+	// This function basically just builds a genesis storage key/value store according to
+	// our desired mockup.
+	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+		system::GenesisConfig::<Test>::default().build_storage().unwrap().0.into()
+	}
+
+	#[test]
+	fn it_works_for_default_value() {
+		with_externalities(&mut new_test_ext(), || {
+			// Just a dummy test for the dummy funtion `do_something`
+			// calling the `do_something` function with a value 42
+			assert_ok!(poa-rewards::do_something(Origin::signed(1), 42));
+			// asserting that the stored value is equal to what we stored
+			assert_eq!(poa-rewards::something(), Some(42));
+		});
+	}
+}
--- frame/session-manager/src/lib.rs
+++ frame/session-manager/src/lib.rs
@@ -7,17 +7,20 @@ use rstd::prelude::*;
 mod mock;
 mod tests;
 
+/// Counter for the number of eras that have passed.
+pub type EraIndex = u32;
+
 pub trait Trait: system::Trait {
-    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        fn deposit_event() = default;
+		fn deposit_event() = default;
 
         fn set_validators(origin, new_validators: Vec<T::AccountId>) -> Result {
             ensure_root(origin)?;
-            <Validators<T>>::put(&new_validators);
+            <NextValidators<T>>::put(&new_validators);
             Self::deposit_event(RawEvent::NewValidators(new_validators));
             Ok(())
         }
@@ -27,12 +30,17 @@ decl_module! {
 decl_event! {
     pub enum Event<T> where <T as system::Trait>::AccountId {
         NewValidators(Vec<AccountId>),
+        RotateValidators(Vec<AccountId>),
     }
 }
 
 decl_storage! {
     trait Store for Module<T: Trait> as SessionManager {
+    	/// The current era validators.
         pub Validators get(fn validators) config(): Vec<T::AccountId>;
+
+        /// The next era validators.
+        pub NextValidators get(fn next_validators) : Vec<T::AccountId>;
     }
 }
 
@@ -41,8 +49,14 @@ impl<T: Trait> session::OnSessionEnding<T::AccountId> for Module<T> {
         _ending: u32,
         _start_session: u32,
     ) -> Option<Vec<T::AccountId>> {
-        Some(<Validators<T>>::get())
-    }
+		let new_validators = <NextValidators<T>>::get();
+		if new_validators.is_empty() {
+			return Some(<Validators<T>>::get())
+		}
+		<Validators<T>>::put(&new_validators);
+		Self::deposit_event(RawEvent::RotateValidators(new_validators.clone()));
+		Some(new_validators)
+	}
 }
 
 impl<T: Trait> session::SelectInitialValidators<T::AccountId> for Module<T> {
