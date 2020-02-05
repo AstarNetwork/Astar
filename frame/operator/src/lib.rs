acaa2998a91239b33ab79313a08605b2968740a6
satellitex <s.a.t.e.3.ths@gmail.com>
2020-02-05 17:34:47 +0900

    refactor operator traist

--- frame/operator/src/lib.rs
+++ frame/operator/src/lib.rs
@@ -21,17 +21,29 @@ pub trait ContractFinder<AccountId, Parameter> {
     fn parameters(contract_id: &AccountId) -> Option<Parameter>;
 }
 
-pub trait OperatorFinder<AccountId> {
+pub trait OperatorFinder<AccountId: Parameter> {
     fn contracts(operator_id: &AccountId) -> Vec<AccountId>;
 }
 
-pub trait TransferOperator<AccountId> {
+pub trait TransferOperator<AccountId: Parameter> : OperatorFinder<AccountId> {
     /// Changes an operator for identified contracts with verify.
     fn transfer_operator(
         current_operator: AccountId,
         contracts: Vec<AccountId>,
         new_operator: AccountId,
-    ) -> Result<(), DispatchError>;
+    ) -> Result<(), DispatchError> {
+        let operate_contracts = Self::contracts(&current_operator);
+
+        // check the actually operate the contract.
+        if !contracts.iter().all(|c| operate_contracts.contains(&c)) {
+            Err(DispatchError::Other(
+                "The sender don't operate the contracts address.",
+            ))?
+        }
+
+        Self::force_transfer_operator(current_operator, contracts, new_operator);
+        Ok(())
+    }
     /// Force Changes an operator for identified contracts without verify.
     fn force_transfer_operator(
         current_operator: AccountId,
@@ -161,25 +173,6 @@ impl<T: Trait> OperatorFinder<T::AccountId> for Module<T> {
 }
 
 impl<T: Trait> TransferOperator<T::AccountId> for Module<T> {
-    /// Changes an operator for identified contracts with verify.
-    fn transfer_operator(
-        current_operator: T::AccountId,
-        contracts: Vec<T::AccountId>,
-        new_operator: T::AccountId,
-    ) -> Result<(), DispatchError> {
-        let operate_contracts = <OperatorHasContracts<T>>::get(&current_operator);
-
-        // check the actually operate the contract.
-        if !contracts.iter().all(|c| operate_contracts.contains(&c)) {
-            Err(DispatchError::Other(
-                "The sender don't operate the contracts address.",
-            ))?
-        }
-
-        Self::force_transfer_operator(current_operator, contracts, new_operator);
-        Ok(())
-    }
-
     /// Force Changes an operator for identified contracts without verify.
     fn force_transfer_operator(
         current_operator: T::AccountId,
