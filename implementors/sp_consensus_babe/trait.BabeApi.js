7e3843267e118285fc552b71bc6fd18899bc3c65
satellitex <s.a.t.e.3.ths@gmail.com>
2020-02-06 08:29:56 +0000

    auto update crates on gh-pages

--- implementors/sp_consensus_babe/trait.BabeApi.js
+++ implementors/sp_consensus_babe/trait.BabeApi.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["plasm_runtime"] = [{"text":"impl&lt;RuntimeApiImplCall:&nbsp;CallRuntimeAt&lt;&lt;<a class=\"struct\" href=\"plasm_runtime/struct.Runtime.html\" title=\"struct plasm_runtime::Runtime\">Runtime</a> as GetNodeBlockType&gt;::NodeBlock&gt; + 'static&gt; BabeApi&lt;&lt;<a class=\"struct\" href=\"plasm_runtime/struct.Runtime.html\" title=\"struct plasm_runtime::Runtime\">Runtime</a> as GetNodeBlockType&gt;::NodeBlock&gt; for <a class=\"struct\" href=\"plasm_runtime/struct.RuntimeApiImpl.html\" title=\"struct plasm_runtime::RuntimeApiImpl\">RuntimeApiImpl</a>&lt;RuntimeApiImplCall&gt;","synthetic":false,"types":["plasm_runtime::RuntimeApiImpl"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/sp_runtime/traits/trait.OnInitialize.js
+++ implementors/sp_runtime/traits/trait.OnInitialize.js
@@ -1,10 +1,4 @@
 (function() {var implementors = {};
 implementors["pallet_contract_operator"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_contract_operator/trait.Trait.html\" title=\"trait pallet_contract_operator::Trait\">Trait</a>&gt; OnInitialize&lt;&lt;T as Trait&gt;::BlockNumber&gt; for <a class=\"struct\" href=\"pallet_contract_operator/struct.Module.html\" title=\"struct pallet_contract_operator::Module\">Module</a>&lt;T&gt;","synthetic":false,"types":["pallet_contract_operator::Module"]}];
 implementors["pallet_plasm_staking"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_plasm_staking/trait.Trait.html\" title=\"trait pallet_plasm_staking::Trait\">Trait</a>&gt; OnInitialize&lt;&lt;T as Trait&gt;::BlockNumber&gt; for <a class=\"struct\" href=\"pallet_plasm_staking/struct.Module.html\" title=\"struct pallet_plasm_staking::Module\">Module</a>&lt;T&gt;","synthetic":false,"types":["pallet_plasm_staking::Module"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
