3dc4c1a9f07cd30d6d7e9071cbf71ede596cf43b
Alexander Krupenkin <mail@akru.me>
2020-02-09 20:48:07 +0300

    Session provided authority keys
    * Using pallet-session for providing lockdrop authority keys.
    * Using Off-chain workers HTTP api to fetch BTC transactions.
    * Set H256 as Claim identifier to prevent double requests.

--- frame/plasm-lockdrop/src/lib.rs
+++ frame/plasm-lockdrop/src/lib.rs
@@ -1,39 +1,37 @@
 //! Plasm Lockdrop module. This can be compiled with `#[no_std]`, ready for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::{Encode, Decode};
-use sp_std::{
-    prelude::*,
-    fmt::Debug,
-    collections::btree_map::BTreeMap,
-};
-use sp_core::offchain::StorageKind;
+use codec::{Encode, Decode, Codec};
+use sp_std::prelude::*;
+use sp_core::{U256, H256};
 use sp_runtime::{
     RuntimeDebug,
-    traits::{
-        Member, CheckEqual, MaybeSerializeDeserialize, Hash,
-        MaybeDisplay, SimpleBitOps,
-    },
+    traits::{Member, IdentifyAccount, BlakeTwo256, Hash},
+    app_crypto::{KeyTypeId, RuntimeAppPublic},
+    offchain::http::Request,
 };
 use frame_support::{
     decl_module, decl_event, decl_storage, decl_error,
-    debug, StorageValue, weights::SimpleDispatchInfo,
+    debug, ensure, StorageValue,
+    weights::SimpleDispatchInfo,
+    traits::{Get, Currency},
+    dispatch::Parameter,
 };
 use frame_system::{
     self as system, ensure_signed,
-    offchain::SubmitSignedTransaction
+    offchain::SubmitSignedTransaction,
 };
 
 /// Plasm Lockdrop Authority local KeyType.
 ///
 /// For security reasons the offchain worker doesn't have direct access to the keys
 /// but only to app-specific subkeys, which are defined and grouped by their `KeyTypeId`.
-pub const KEY_TYPE: app_crypto::KeyTypeId = app_crypto::KeyTypeId(*b"plas");
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"plaa");
 
 /// SR25519 keys support
 pub mod sr25519 {
     mod app_sr25519 {
-        use app_crypto::{app_crypto, sr25519};
+        use sp_runtime::app_crypto::{app_crypto, sr25519};
         use crate::KEY_TYPE;
         app_crypto!(sr25519, KEY_TYPE);
     }
@@ -52,7 +50,7 @@ pub mod sr25519 {
 /// ED25519 keys support
 pub mod ed25519 {
     mod app_ed25519 {
-        use app_crypto::{app_crypto, ed25519};
+        use sp_runtime::app_crypto::{app_crypto, ed25519};
         use crate::KEY_TYPE;
         app_crypto!(ed25519, KEY_TYPE);
     }
@@ -68,41 +66,47 @@ pub mod ed25519 {
     pub type AuthorityId = app_ed25519::Public;
 }
 
-/// The local storage database key under which the worker progress status
-/// is tracked.
-const DB_KEY: &[u8] = b"staketechnilogies/plasm-lockdrop-worker";
+// The local storage database key under which the worker progress status is tracked.
+//const DB_KEY: &[u8] = b"staketechnilogies/plasm-lockdrop-worker";
+
+pub type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
 
 /// The module's main configuration trait.
-pub trait Trait: ::Trait {
-	/// The identifier type for an authority.
-	type AuthorityId: Member + Parameter + RuntimeAppPublic + Default + Ord;
+pub trait Trait: system::Trait {
+    /// The lockdrop balance.
+    type Currency: Currency<Self::AccountId>;
+
+    /// How much authority votes module should receive to decide claim result.
+    type VoteThreshold: Get<AuthorityVote>;
+
+    /// How much positive votes requered to approve claim.
+    ///   Positive votes = approve votes - decline votes.
+    type PositiveVotes: Get<AuthorityVote>;
 
     /// A dispatchable call type.
     type Call: From<Call<Self>>;
 
-    /// How much votes module should receive to decide claim result.
-    type VoteThreshold: Get<ClaimVotes>;
-
-    /// How much positive votes requered to approve claim.
-    /// Total positive votes = positive votes - negative votes.
-    type PositiveVotes: Get<ClaimVotes>;
-
     /// Let's define the helper we use to create signed transactions.
     type SubmitTransaction: SubmitSignedTransaction<Self, <Self as Trait>::Call>;
+
+    /// The identifier type for an authority.
+    type AuthorityId: Member + Parameter + RuntimeAppPublic + Default + Ord
+        + IdentifyAccount<AccountId=<Self as system::Trait>::AccountId>;
 
     /// The regular events type.
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
-/// Enumerate up to 2^32 claim requests.
-pub type ClaimId = u32;
+/// Claim id is a hash of claim parameters and sender account id.
+pub type ClaimId = H256;
 
 /// Type for enumerating claim proof votes. 
-pub type ClaimVotes = u32;
+pub type AuthorityVote = u32;
 
 /// Plasm Lockdrop parameters.
 #[cfg_attr(feature = "std", derive(PartialEq, Eq))]
-#[derive(Encode, Decode, RuntimeDebug)]
+#[derive(Encode, Decode, RuntimeDebug, Clone)]
 pub enum Lockdrop {
     /// Bitcoin lockdrop is pretty simple:
     /// transaction sended with time-lockding opcode,
@@ -111,14 +115,25 @@ pub enum Lockdrop {
     Bitcoin { balance: U256, duration: u64, transaction_hash: H256, },
 }
 
+impl Default for Lockdrop {
+    fn default() -> Self {
+        Lockdrop::Bitcoin {
+            balance: Default::default(),
+            duration: Default::default(),
+            transaction_hash: Default::default(),
+        }
+    }
+}
+
 /// Lockdrop claim request description.
 #[cfg_attr(feature = "std", derive(PartialEq, Eq))]
-#[derive(Encode, Decode, RuntimeDebug)]
-pub struct Claim<T> {
-    sender: <T as system::Trait>::AccountId,
+#[derive(Encode, Decode, RuntimeDebug, Clone, Default)]
+pub struct Claim<AccountId: Member + Codec> {
+    sender: AccountId,
     params: Lockdrop,
-    vote_up: ClaimVotes,
-    vote_down: ClaimVotes,
+    approve: AuthorityVote,
+    decline: AuthorityVote,
+    complete: bool,
 }
 
 decl_event!(
@@ -126,7 +141,11 @@ decl_event!(
     where <T as system::Trait>::AccountId,
           <T as Trait>::AuthorityId,
     {
-        NewRequest(ClaimId, AccountId),
+        /// Lockdrop token claims requested by user
+        ClaimRequest(ClaimId),
+        /// Lockdrop token claims response by authority
+        ClaimResponse(ClaimId, AccountId, bool),
+        /// New authority list registered
         NewAuthorities(Vec<AuthorityId>),
     }
 );
@@ -138,12 +157,13 @@ decl_error! {
 
 decl_storage! {
     trait Store for Module<T: Trait> as Provider {
-        /// Requests made within this block execution.
+        /// Offchain lock check requests made within this block execution.
         Requests get(fn requests): Vec<ClaimId>;
-        /// List of lockdrop authorities keys.
+        /// List of lockdrop authority id's.
         Keys get(fn keys): Vec<T::AuthorityId>;
-        /// Claim requests.
-        Claims get(fn claims): map ClaimId => Claim<T>;
+        /// Token claim requests.
+        Claims get(fn claims): linked_map hasher(blake2_256)
+                               ClaimId => Claim<T::AccountId>;
         /// Latest claim index.
         LatestClaim get(fn latest_claim): ClaimId;
     }
@@ -159,58 +179,68 @@ decl_module! {
             // At the beginning of each block execution, system triggers all
             // `on_initialize` functions, which allows us to set up some temporary state or - like
             // in this case - clean up other states
-            <OcRequests<T>>::kill();
+            <Requests>::kill();
         }
 
-        /// Check lock transaction to claim lockdrop tokens. 
+        /// Request authorities to check locking transaction. 
         #[weight = SimpleDispatchInfo::FixedNormal(1_000_000)]
-        fn claim(
+        fn request(
             origin,
             params: Lockdrop,
         ) {
-            let sender = ensure_signed!(origin);
-            let claim = Claim { sender, params, vote_up: 0, vote_down: 0 };
+            let sender = ensure_signed(origin)?;
+            let claim_id = BlakeTwo256::hash_of(&(sender.clone(), params.clone()));
+            ensure!(!Claims::<T>::get(claim_id).complete, "claim should not be already paid"); 
 
-            let claim_id = <LatestClaim>::get();
-            <LatestClaim>::push(claim_id + 1);
+            if Claims::<T>::exists(claim_id) {
+                let claim = Claim { sender, params, .. Default::default() };
+                Claims::<T>::insert(claim_id, claim);
+            }
 
-            <Claims<T>>::insert(claim_id, claim);
-            <Requests<T>>::mutate(|requests| requests.push(claim_id));
-
-            Self::deposit_event(RawEvent::NewRequest(claim_id));
+            <Requests>::mutate(|requests| requests.push(claim_id));
+            Self::deposit_event(RawEvent::ClaimRequest(claim_id));
         }
 
-        #[weight = SimpleDispatchInfo::FreeOperational]
+        /// Claim tokens according to lockdrop procedure.
+        #[weight = SimpleDispatchInfo::FixedNormal(1_000_000)]
+        fn claim(
+            origin,
+            _claim_id: ClaimId,
+        ) {
+            let _sender = ensure_signed(origin)?;
+            unimplemented!();
+        }
+
+        /// Vote for claim request according to check results. (for authorities only) 
+        #[weight = SimpleDispatchInfo::FixedOperational(10_000)]
         fn vote(
             origin,
             claim_id: ClaimId,
             approve: bool,
         ) {
-            let sender = ensure_signed!(origin);
-            ensure!(T::is_authority(&sender), "Only lockdrop authorities can vote");
-            ensure!(<Claims<T>>::exists(claim_id), "Claim with this id doesn't exist");
+            let sender = ensure_signed(origin)?;
+            ensure!(Self::is_authority(&sender), "this method for lockdrop authorities only");
+            ensure!(Claims::<T>::exists(claim_id), "request with this id doesn't exist");
 
-            <Claims<T>>::mutate(&claim_id, |claim|
-                if approve { claim.vote_up += 1 }
-                else { claim.vote_down += 1 }
+            Claims::<T>::mutate(&claim_id, |claim|
+                if approve { claim.approve += 1 }
+                else { claim.decline += 1 }
             );
-        }
-
-        #[weight = SimpleDispatchInfo::FreeOperational]
-        fn set_authorities(
-            origin,
-            authorities: Vec<T::AuthorityId>,
-        ) {
-            let _ = ensure_root!(origin);
-            <Keys<T>>::put(authorities.clone());
-            Self::deposit_event(RawEvent::NewAuthorities(authorities));
+            Self::deposit_event(RawEvent::ClaimResponse(claim_id, sender, approve));
         }
 
         // Runs after every block within the context and current state of said block.
-        fn offchain_worker(now: T::BlockNumber) {
+        fn offchain_worker(_now: T::BlockNumber) {
             debug::RuntimeLogger::init();
+
             if sp_io::offchain::is_validator() {
-                Self::offchain(now);
+                match Self::offchain() {
+                    Err(e) => debug::error!(
+                        target: "lockdrop-offchain-worker",
+                        "Lockdrop worker failed with error: {}", e
+                    ),
+                    _ => (),
+                }
             }
         }
     }
@@ -218,19 +248,234 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     /// The main offchain worker entry point.
-    fn offchain(now: T::BlockNumber) {
-        for claim_id in <Requests>::get() {
-            let approve = Self::check_lock(claim_id);
+    fn offchain() -> Result<(), String> {
+        // TODO: use permanent storage to track request when temporary failed
+        for claim_id in Self::requests() {
+            debug::debug!(
+                target: "lockdrop-offchain-worker",
+                "new claim request: id = {}", claim_id 
+            );
+
+            let approve = Self::check_lock(claim_id)?;
+            debug::info!(
+                target: "lockdrop-offchain-worker",
+                "claim id {} => check result: {}", claim_id, approve 
+            );
+
             let call = Call::vote(claim_id, approve);
-            T::SubmitTransaction::submit_signed(call);
+            debug::debug!(
+                target: "lockdrop-offchain-worker",
+                "claim id {} => vote extrinsic: {:?}", claim_id, call
+            );
+
+            let res = T::SubmitTransaction::submit_signed(call);
+            debug::debug!(
+                target: "lockdrop-offchain-worker",
+                "claim id {} => vote extrinsic send: {:?}", claim_id, res
+            );
+        }
+
+        Ok(())
+    }
+
+    fn is_authority(account: &T::AccountId) -> bool {
+        Keys::<T>::get()
+            .binary_search_by(|key| key.clone().into_account().cmp(account))
+            .is_ok()
+    }
+
+    fn check_lock(claim_id: ClaimId) -> Result<bool, String> {
+        let Claim { sender, params, .. } = Self::claims(claim_id);
+        match params {
+            Lockdrop::Bitcoin { balance, duration, transaction_hash } => {
+                let uri = format!(
+                    // XXX: Fetch transaction description in BTC Testnet using blockcypher API.
+                    "http://api.blockcypher.com/v1/btc/test3/txs/{}",
+                    hex::encode(transaction_hash)
+                );
+                let tx = Self::fetch_json(uri)?;
+                unimplemented!()
+            },
         }
     }
 
-    fn is_authority(authority: &T::AuthorityId) -> bool {
-        <Keys<T>>::get().binary_search(authority).ok()
+    fn fetch_json(uri: String) -> Result<serde_json::Value, String> {
+        let request = Request::get(uri.as_ref()).send()
+            .map_err(|e| format!("HTTP request error: {:?}", e))?;
+        let response = request.wait()
+            .map_err(|e| format!("HTTP response error: {:?}", e))?;
+        serde_json::to_value(response.body().clone().collect::<Vec<_>>())
+            .map_err(|e| format!("JSON decode error: {}", e))
+    }
+}
+
+impl<T: Trait> sp_runtime::BoundToRuntimeAppPublic for Module<T> {
+    type Public = T::AuthorityId;
+}
+
+impl<T: Trait> pallet_session::OneSessionHandler<T::AccountId> for Module<T> {
+    type Key = T::AuthorityId;
+
+    fn on_genesis_session<'a, I: 'a>(validators: I)
+        where I: Iterator<Item=(&'a T::AccountId, T::AuthorityId)>
+    {
+        // Init authorities on genesis session.
+        let authorities: Vec<_> = validators.map(|x| x.1).collect();
+        Keys::<T>::put(authorities.clone());
+        Self::deposit_event(RawEvent::NewAuthorities(authorities));
     }
 
-    fn check_lock(claim_id: ClaimId) -> bool {
-        unimplemented!()
+    fn on_new_session<'a, I: 'a>(_changed: bool, validators: I, _queued_validators: I)
+        where I: Iterator<Item=(&'a T::AccountId, T::AuthorityId)>
+    {
+        // Remember who the authorities are for the new session.
+        let authorities: Vec<_> = validators.map(|x| x.1).collect();
+        Keys::<T>::put(authorities.clone());
+        Self::deposit_event(RawEvent::NewAuthorities(authorities));
+    }
+
+    fn on_before_session_ending() { }
+    fn on_disabled(_i: usize) { }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate as lockdrop;
+    use sp_runtime::{
+        Perbill, generic,
+        testing::Header,
+        traits::{IdentityLookup, BlakeTwo256},
+    };
+    use frame_support::{
+        impl_outer_event,
+        impl_outer_origin,
+        impl_outer_dispatch,
+        parameter_types,
+        assert_ok
+    };
+    use sp_runtime::{traits::{Verify, IdentifyAccount}};
+    use sp_core::{
+        offchain::{
+            OffchainExt, TransactionPoolExt,
+            testing::{TestOffchainExt, TestTransactionPoolExt},
+        },
+        H256, sr25519, crypto::Pair
+    };
+
+    impl_outer_event! {
+        pub enum MetaEvent for Runtime {
+            lockdrop<T>,
+            pallet_balances<T>,
+        }
+    }
+
+    impl_outer_origin!{
+        pub enum Origin for Runtime {}
+    }
+
+    impl_outer_dispatch! {
+        pub enum Call for Runtime where origin: Origin {
+            system::System,
+            lockdrop::Lockdrop,
+        }
+    }
+
+    #[derive(Clone, PartialEq, Eq, Debug)]
+    pub struct Runtime;
+
+    // Define the required constants for system module,
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: u32 = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::one();
+    }
+
+    // and add it to our test runtime.
+    impl system::Trait for Runtime {
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Call = Call;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = MetaEvent;
+        type BlockHashCount = BlockHashCount;
+        type MaximumBlockWeight = MaximumBlockWeight;
+        type MaximumBlockLength = MaximumBlockLength;
+        type AvailableBlockRatio = AvailableBlockRatio;
+        type Version = ();
+        type ModuleToIndex = ();
+    }
+
+    parameter_types! {
+        pub const ExistentialDeposit: u64 = 0;
+        pub const TransferFee: u64 = 0;
+        pub const CreationFee: u64 = 0;
+    }
+
+    impl pallet_balances::Trait for Runtime {
+        type Balance = u64;
+        type OnNewAccount = ();
+        type Event = MetaEvent;
+        type DustRemoval = ();
+        type TransferPayment = ();
+        type ExistentialDeposit = ExistentialDeposit;
+        type CreationFee = CreationFee;
+        type OnReapAccount = ();
+    }
+
+    parameter_types! {
+        pub const VoteThreshold: AuthorityVote = 10;
+        pub const PositiveVotes: AuthorityVote = 10;
+    }
+
+    type SignedExtra = (
+        frame_system::CheckEra<Runtime>,
+        frame_system::CheckNonce<Runtime>,
+        frame_system::CheckWeight<Runtime>,
+    );
+    type TestXt = sp_runtime::testing::TestXt<Call, SignedExtra>;
+
+    fn extra(nonce: u64) -> SignedExtra {
+        (
+            frame_system::CheckEra::from(frame_system::Era::Immortal),
+            frame_system::CheckNonce::from(nonce),
+            frame_system::CheckWeight::new(),
+        )
+    }
+
+    fn sign_extra(who: u64, nonce: u64) -> Option<(u64, SignedExtra)> {
+        Some((who, extra(nonce)))
+    }
+
+    impl Trait for Runtime {
+        type Event = MetaEvent;
+        type Call = Call;
+        type SubmitTransaction = frame_system::offchain::TransactionSubmitter<
+            lockdrop::sr25519::AuthorityId, Call, TestXt,
+        >;
+        type VoteThreshold = VoteThreshold;
+        type PositiveVotes = PositiveVotes;
+        type Currency = Balances;
+    }
+
+    type System = frame_system::Module<Runtime>;
+    type Balances = pallet_balances::Module<Runtime>;
+    type Lockdrop = Module<Runtime>;
+
+    pub fn new_test_ext() -> sp_io::TestExternalities {
+        let t = frame_system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+        t.into()
+    }
+
+    #[test]
+    fn test_initial_setup() {
+        new_test_ext().execute_with(|| {
+        })
     }
 }
