8f653820a065da7e7eef1805aefb8cf0554d746e
satellitex <satellitex@users.noreply.github.com>
2020-02-29 16:43:05 +0000

    deploy: f5bc902c3ac892907825783d32b517062fff4ffc

--- implementors/pallet_session/trait.OnSessionEnding.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["pallet_plasm_staking"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_plasm_staking/trait.Trait.html\" title=\"trait pallet_plasm_staking::Trait\">Trait</a>&gt; OnSessionEnding&lt;&lt;T as Trait&gt;::AccountId&gt; for <a class=\"struct\" href=\"pallet_plasm_staking/struct.Module.html\" title=\"struct pallet_plasm_staking::Module\">Module</a>&lt;T&gt;","synthetic":false,"types":["pallet_plasm_staking::Module"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
