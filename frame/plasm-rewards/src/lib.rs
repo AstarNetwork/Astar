ccb8184b5d7b30ff99ab1418e7ebd6edab411d39
satellitex <s.a.t.e.3.ths@gmail.com>
2020-03-24 02:11:06 +0900

    add plasm-rewards module

---
+++ frame/plasm-rewards/src/lib.rs
@@ -0,0 +1,410 @@
+//! # Plasm Staking Module
+//!
+//! The Plasm staking module puts together the management and compensation payment logic of the ERA.
+//! The Plasm Rewards module calls the Dapps Staking and Validator.
+//! It also allocates rewards to each module according to the [Plasm Token Ecosystem inflation model](https://docs.plasmnet.io/PlasmNetwork/TokenEcosystem.html).
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+use session::SessionManager;
+use sp_runtime::{
+    traits::{SaturatedConversion, Zero},
+    Perbill, RuntimeDebug,
+};
+use sp_std::{prelude::*, vec::Vec};
+pub use staking::Forcing;
+use support::{
+    decl_error, decl_event, decl_module, decl_storage,
+    traits::{Currency, Get, LockableCurrency, OnUnbalanced, Time},
+    weights::SimpleDispatchInfo,
+    StorageMap, StorageValue,
+};
+use system::ensure_root;
+
+mod inflation;
+#[cfg(test)]
+mod mock;
+pub mod traits;
+use traits::*;
+// #[cfg(test)]
+// mod tests;
+
+pub use sp_staking::SessionIndex;
+
+pub type EraIndex = u32;
+pub type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+pub type MomentOf<T> = <<T as Trait>::Time as Time>::Moment;
+
+type PositiveImbalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::PositiveImbalance;
+type NegativeImbalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::NegativeImbalance;
+
+// A value placed in storage that represents the current version of the Staking storage.
+// This value is used by the `on_runtime_upgrade` logic to determine whether we run
+// storage migration logic. This should match directly with the semantic versions of the Rust crate.
+#[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug)]
+enum Releases {
+    V1_0_0Ancient,
+    V2_0_0,
+}
+
+impl Default for Releases {
+    fn default() -> Self {
+        Releases::V2_0_0
+    }
+}
+
+/// Information regarding the active era (era in used in session).
+#[derive(Encode, Decode, RuntimeDebug)]
+pub struct ActiveEraInfo<Moment> {
+    /// Index of era.
+    index: EraIndex,
+    /// Moment of start
+    ///
+    /// Start can be none if start hasn't been set for the era yet,
+    /// Start is set on the first on_finalize of the era to guarantee usage of `Time`.
+    start: Option<Moment>,
+}
+
+pub trait Trait: session::Trait {
+    /// The staking balance.
+    type Currency: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
+
+    /// Number of eras that staked funds must remain bonded for.
+    type BondingDuration: Get<EraIndex>;
+
+    /// Tokens have been minted and are unused for validator-reward. Maybe, plasm-staking uses ().
+    type RewardRemainder: OnUnbalanced<NegativeImbalanceOf<Self>>;
+
+    /// Handler for the unbalanced increment when rewarding a staker. Maybe, plasm-staking uses ().
+    type Reward: OnUnbalanced<PositiveImbalanceOf<Self>>;
+
+    /// Time used for computing era duration.
+    type Time: Time;
+
+    /// Number of sessions per era.
+    type SessionsPerEra: Get<SessionIndex>;
+
+    /// Get the amount of staking for dapps per era.
+    type GetForDappsStaking: traits::GetEraStakingAmount<EraIndex, BalanceOf<Self>>;
+
+    /// Get the amount of staking for security per era.
+    type GetForSecurityStaking: traits::GetEraStakingAmount<EraIndex, BalanceOf<Self>>;
+
+    type ComputeTotalPayout: traits::ComputeTotalPayout;
+
+    type MaybeValidators: traits::MaybeValidators<EraIndex, Self::AccountId>;
+
+    /// The overarching event type.
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as PlasmStaking {
+        /// This is the compensation paid for the dapps operator of the Plasm Network.
+        /// This is stored on a per-era basis.
+        pub ForDappsEraReward get(fn for_dapps_era_reward): map hasher(twox_64_concat) EraIndex => Option<BalanceOf<T>>;
+
+        /// This is the compensation paid for the security of the Plasm Network.
+        /// This is stored on a per-era basis.
+        pub ForSecurityEraReward get(fn for_security_era_reward): map hasher(twox_64_concat) EraIndex => Option<BalanceOf<T>>;
+
+        /// Number of era to keep in history.
+        ///
+        /// Information is kept for eras in `[current_era - history_depth; current_era]`
+        ///
+        /// Must be more than the number of era delayed by session otherwise.
+        /// i.e. active era must always be in history.
+        /// i.e. `active_era > current_era - history_depth` must be guaranteed.
+        HistoryDepth get(fn history_depth) config(): u32 = 84;
+
+        /// The current era index.
+        ///
+        /// This is the latest planned era, depending on how session module queues the validator
+        /// set, it might be active or not.
+        pub CurrentEra get(fn current_era): Option<EraIndex>;
+
+        /// The active era information, it holds index and start.
+        ///
+        /// The active era is the era currently rewarded.
+        /// Validator set of this era must be equal to `SessionInterface::validators`.
+        pub ActiveEra get(fn active_era): Option<ActiveEraInfo<MomentOf<T>>>;
+
+        /// The session index at which the era start for the last `HISTORY_DEPTH` eras
+        pub ErasStartSessionIndex get(fn eras_start_session_index):
+            map hasher(twox_64_concat) EraIndex => Option<SessionIndex>;
+
+        /// True if the next session change will be a new era regardless of index.
+        pub ForceEra get(fn force_era) config(): Forcing;
+
+        /// Storage version of the pallet.
+        ///
+        /// This is set to v2.0.0 for new networks.
+        StorageVersion build(|_: &GenesisConfig| Releases::V2_0_0): Releases;
+    }
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        Balance = BalanceOf<T>,
+    {
+        /// The whole reward issued in that Era.
+        /// (era_index: EraIndex, reward: Balance)
+        WholeEraReward(EraIndex, Balance),
+    }
+);
+
+decl_error! {
+    /// Error for the staking module.
+    pub enum Error for Module<T: Trait> {
+        /// Duplicate index.
+        DuplicateIndex,
+        /// Invalid era to reward.
+        InvalidEraToReward,
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        /// Number of sessions per era.
+        const SessionsPerEra: SessionIndex = T::SessionsPerEra::get();
+
+        type Error = Error<T>;
+
+        fn deposit_event() = default;
+
+        fn on_runtime_upgrade() {
+            migrate::<T>();
+        }
+
+        fn on_finalize() {
+            // Set the start of the first era.
+            if let Some(mut active_era) = Self::active_era() {
+                if active_era.start.is_none() {
+                    active_era.start = Some(T::Time::now());
+                    <ActiveEra<T>>::put(active_era);
+                }
+            }
+        }
+
+        // ----- Root calls.
+        /// Force there to be no new eras indefinitely.
+        ///
+        /// # <weight>
+        /// - No arguments.
+        /// # </weight>
+        #[weight = SimpleDispatchInfo::FixedNormal(5_000)]
+        fn force_no_eras(origin) {
+            ensure_root(origin)?;
+            ForceEra::put(Forcing::ForceNone);
+        }
+
+        /// Force there to be a new era at the end of the next session. After this, it will be
+        /// reset to normal (non-forced) behaviour.
+        ///
+        /// # <weight>
+        /// - No arguments.
+        /// # </weight>
+        #[weight = SimpleDispatchInfo::FixedNormal(5_000)]
+        fn force_new_era(origin) {
+            ensure_root(origin)?;
+            ForceEra::put(Forcing::ForceNew);
+        }
+
+        /// Force there to be a new era at the end of sessions indefinitely.
+        ///
+        /// # <weight>
+        /// - One storage write
+        /// # </weight>
+        #[weight = SimpleDispatchInfo::FixedNormal(5_000)]
+        fn force_new_era_always(origin) {
+            ensure_root(origin)?;
+            ForceEra::put(Forcing::ForceAlways);
+        }
+
+        /// Set history_depth value.
+        ///
+        /// Origin must be root.
+        #[weight = SimpleDispatchInfo::FixedOperational(500_000)]
+        fn set_history_depth(origin, #[compact] new_history_depth: EraIndex) {
+            ensure_root(origin)?;
+            if let Some(current_era) = Self::current_era() {
+                HistoryDepth::mutate(|history_depth| {
+                    let last_kept = current_era.checked_sub(*history_depth).unwrap_or(0);
+                    let new_last_kept = current_era.checked_sub(new_history_depth).unwrap_or(0);
+                    for era_index in last_kept..new_last_kept {
+                        Self::clear_era_information(era_index);
+                    }
+                    *history_depth = new_history_depth
+                })
+            }
+        }
+    }
+}
+
+fn migrate<T: Trait>() {
+    if let Some(current_era) = CurrentEra::get() {
+        let history_depth = HistoryDepth::get();
+        for era in current_era.saturating_sub(history_depth)..=current_era {
+            ErasStartSessionIndex::migrate_key_from_blake(era);
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    // MUTABLES (DANGEROUS)
+
+    /// Plan a new session potentially trigger a new era.
+    fn new_session(session_index: SessionIndex) -> Option<Vec<T::AccountId>> {
+        if let Some(current_era) = Self::current_era() {
+            // Initial era has been set.
+
+            let current_era_start_session_index = Self::eras_start_session_index(current_era)
+                .unwrap_or_else(|| {
+                    support::print("Error: start_session_index must be set for current_era");
+                    0
+                });
+
+            let era_length = session_index
+                .checked_sub(current_era_start_session_index)
+                .unwrap_or(0); // Must never happen.
+
+            match ForceEra::get() {
+                Forcing::ForceNew => ForceEra::kill(),
+                Forcing::ForceAlways => (),
+                Forcing::NotForcing if era_length >= T::SessionsPerEra::get() => (),
+                _ => return None,
+            }
+
+            Self::new_era(session_index)
+        } else {
+            // Set initial era
+            Self::new_era(session_index)
+        }
+    }
+
+    /// Start a session potentially starting an era.
+    fn start_session(start_session: SessionIndex) {
+        let next_active_era = Self::active_era().map(|e| e.index + 1).unwrap_or(0);
+        if let Some(next_active_era_start_session_index) =
+            Self::eras_start_session_index(next_active_era)
+        {
+            if next_active_era_start_session_index == start_session {
+                Self::start_era(start_session);
+            } else if next_active_era_start_session_index < start_session {
+                // This arm should never happen, but better handle it than to stall the
+                // staking pallet.
+                support::print("Warning: A session appears to have been skipped.");
+                Self::start_era(start_session);
+            }
+        }
+    }
+
+    /// End a session potentially ending an era.
+    fn end_session(session_index: SessionIndex) {
+        if let Some(active_era) = Self::active_era() {
+            if let Some(next_active_era_start_session_index) =
+                Self::eras_start_session_index(active_era.index + 1)
+            {
+                if next_active_era_start_session_index == session_index + 1 {
+                    Self::end_era(active_era, session_index);
+                }
+            }
+        }
+    }
+
+    /// * Increment `active_era.index`,
+    /// * reset `active_era.start`,
+    /// * update `BondedEras` and apply slashes.
+    fn start_era(start_session: SessionIndex) {
+        let active_era = <ActiveEra<T>>::mutate(|active_era| {
+            let new_index = active_era.as_ref().map(|info| info.index + 1).unwrap_or(0);
+            *active_era = Some(ActiveEraInfo {
+                index: new_index,
+                // Set new active era start in next `on_finalize`. To guarantee usage of `Time`
+                start: None,
+            });
+            new_index
+        });
+    }
+
+    /// Compute payout for era.
+    fn end_era(active_era: ActiveEraInfo<MomentOf<T>>, _session_index: SessionIndex) {
+        // Note: active_era_start can be None if end era is called during genesis config.
+        if let Some(active_era_start) = active_era.start {
+            // The set of total amount of staking.
+            let now = T::Time::now();
+            let era_duration = now - active_era_start;
+
+            if !era_duration.is_zero() {
+                let total_payout = T::Currency::total_issuance();
+                let for_dapps = T::GetForDappsStaking::get_era_staking_amount(active_era.index);
+                let for_security =
+                    T::GetForSecurityStaking::get_era_staking_amount(active_era.index);
+                let (for_security_reward, for_dapps_rewards) =
+                    T::ComputeTotalPayout::compute_total_payout(
+                        total_payout,
+                        era_duration.saturated_into::<u64>(),
+                        for_dapps,
+                        for_security,
+                    );
+
+                <ForSecurityEraReward<T>>::insert(active_era.index, for_security_reward);
+                <ForDappsEraReward<T>>::insert(active_era.index, for_dapps_rewards);
+            }
+        }
+    }
+
+    /// Plan a new era. Return the potential new staking set.
+    fn new_era(start_session_index: SessionIndex) -> Option<Vec<T::AccountId>> {
+        // Increment or set current era.
+        let current_era = CurrentEra::mutate(|s| {
+            *s = Some(s.map(|s| s + 1).unwrap_or(0));
+            s.unwrap()
+        });
+        ErasStartSessionIndex::insert(&current_era, &start_session_index);
+
+        // Clean old era information.
+        if let Some(old_era) = current_era.checked_sub(Self::history_depth() + 1) {
+            Self::clear_era_information(old_era);
+        }
+
+        // Return maybe validators.
+        T::MaybeValidators::maybe_validators(current_era)
+    }
+
+    /// Clear all era information for given era.
+    fn clear_era_information(era_index: EraIndex) {
+        ErasStartSessionIndex::remove(era_index);
+        <ForDappsEraReward<T>>::remove(era_index);
+        <ForSecurityEraReward<T>>::remove(era_index);
+    }
+
+    /// Ensures that at the end of the current session there will be a new era.
+    fn ensure_new_era() {
+        match ForceEra::get() {
+            Forcing::ForceAlways | Forcing::ForceNew => (),
+            _ => ForceEra::put(Forcing::ForceNew),
+        }
+    }
+}
+
+/// In this implementation `new_session(session)` must be called before `end_session(session-1)`
+/// i.e. the new session must be planned before the ending of the previous session.
+///
+/// Once the first new_session is planned, all session must start and then end in order, though
+/// some session can lag in between the newest session planned and the latest session started.
+impl<T: Trait> SessionManager<T::AccountId> for Module<T> {
+    fn new_session(new_index: SessionIndex) -> Option<Vec<T::AccountId>> {
+        Self::new_session(new_index)
+    }
+    fn start_session(start_index: SessionIndex) {
+        Self::start_session(start_index)
+    }
+    fn end_session(end_index: SessionIndex) {
+        Self::end_session(end_index)
+    }
+}
---
+++ frame/plasm-rewards/src/traits.rs
@@ -0,0 +1,25 @@
+use super::*;
+use sp_arithmetic::traits::BaseArithmetic;
+
+/// Get the amount of staking per Era in a module in the Plasm Network.
+pub trait GetEraStakingAmount<EraIndex, Balance> {
+    fn get_era_staking_amount(era: EraIndex) -> Balance;
+}
+
+/// The reward is allocated from the total supply of tokens,
+/// the time for Era, the amount of staking for Security, and the amount of staking for Dapps.
+pub trait ComputeTotalPayout {
+    fn compute_total_payout<N>(
+        total_tokens: N,
+        era_duration: u64,
+        validator_staking: N,
+        dapps_staking: N,
+    ) -> (N, N)
+    where
+        N: BaseArithmetic + Clone + From<u32>;
+}
+
+/// Returns the next validator candidate.
+pub trait MaybeValidators<EraIndex, AccountId> {
+    fn maybe_validators(current_era: EraIndex) -> Option<Vec<AccountId>>;
+}
