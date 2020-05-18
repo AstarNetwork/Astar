f9c4380428b02fdcdd8cf23f917d00aa19fd8b2e
Alexander Krupenkin <mail@akru.me>
2020-05-18 07:19:51 +0300

    Fix fmt

--- bin/node/cli/src/oracle.rs
+++ bin/node/cli/src/oracle.rs
@@ -1,7 +1,7 @@
 //! Web-server helper for Lockdrop runtime module.
 
-use pallet_plasm_lockdrop::Transaction;
 use codec::Encode;
+use pallet_plasm_lockdrop::Transaction;
 
 const COINGECKO_BTC_API: &'static str = "https://api.coingecko.com/api/v3/coins/bitcoin";
 const COINGECKO_ETH_API: &'static str = "https://api.coingecko.com/api/v3/coins/ethereum";
@@ -21,34 +21,38 @@ pub async fn start() {
         Ok(ticker["market_data"]["current_price"]["usd"].to_string())
     });
 
-    app.at("/tx/btc/:hash").get(|req: tide::Request<()>| async move {
-        let hash: String = req.param("hash")?;
-        let uri = format!("{}/{}", BLOCKCYPHER_BTC_API, hash);
-        let tx: serde_json::Value = reqwest::blocking::get(uri.as_str())?.json()?;
-        let encoded = Transaction {
-            sender: bs58::decode(tx["inputs"][0]["addresses"].to_string()).into_vec()?,
-            recipient: bs58::decode(tx["outputs"][0]["addresses"].to_string()).into_vec()?,
-            value: tx["outputs"][0]["value"].as_u64().unwrap_or(0) as u128,
-            script: hex::decode(tx["outputs"][0]["script"].to_string())?,
-            confirmations: tx["confirmations"].as_u64().unwrap_or(0),
-        }.encode();
-        Ok(hex::encode(encoded))
-    });
+    app.at("/tx/btc/:hash")
+        .get(|req: tide::Request<()>| async move {
+            let hash: String = req.param("hash")?;
+            let uri = format!("{}/{}", BLOCKCYPHER_BTC_API, hash);
+            let tx: serde_json::Value = reqwest::blocking::get(uri.as_str())?.json()?;
+            let encoded = Transaction {
+                sender: bs58::decode(tx["inputs"][0]["addresses"].to_string()).into_vec()?,
+                recipient: bs58::decode(tx["outputs"][0]["addresses"].to_string()).into_vec()?,
+                value: tx["outputs"][0]["value"].as_u64().unwrap_or(0) as u128,
+                script: hex::decode(tx["outputs"][0]["script"].to_string())?,
+                confirmations: tx["confirmations"].as_u64().unwrap_or(0),
+            }
+            .encode();
+            Ok(hex::encode(encoded))
+        });
 
-    app.at("/tx/eth/:hash").get(|req: tide::Request<()>| async move {
-        let hash: String = req.param("hash")?;
-        let uri = format!("{}/{}", BLOCKCYPHER_ETH_API, hash);
-        let tx: serde_json::Value = reqwest::blocking::get(uri.as_str())?.json()?;
-        log::info!("Response received: {}", tx);
-        let encoded = Transaction {
-            sender: hex::decode(tx["inputs"][0]["addresses"].to_string())?,
-            recipient: hex::decode(tx["outputs"][0]["addresses"].to_string())?,
-            value: tx["outputs"][0]["value"].as_u64().unwrap_or(0) as u128,
-            script: hex::decode(tx["outputs"][0]["script"].to_string())?,
-            confirmations: tx["confirmations"].as_u64().unwrap_or(0),
-        }.encode();
-        Ok(hex::encode(encoded))
-    });
+    app.at("/tx/eth/:hash")
+        .get(|req: tide::Request<()>| async move {
+            let hash: String = req.param("hash")?;
+            let uri = format!("{}/{}", BLOCKCYPHER_ETH_API, hash);
+            let tx: serde_json::Value = reqwest::blocking::get(uri.as_str())?.json()?;
+            log::info!("Response received: {}", tx);
+            let encoded = Transaction {
+                sender: hex::decode(tx["inputs"][0]["addresses"].to_string())?,
+                recipient: hex::decode(tx["outputs"][0]["addresses"].to_string())?,
+                value: tx["outputs"][0]["value"].as_u64().unwrap_or(0) as u128,
+                script: hex::decode(tx["outputs"][0]["script"].to_string())?,
+                confirmations: tx["confirmations"].as_u64().unwrap_or(0),
+            }
+            .encode();
+            Ok(hex::encode(encoded))
+        });
 
     app.listen("127.0.0.1:34347")
         .await
