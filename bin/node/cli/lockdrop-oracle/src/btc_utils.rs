2e99ac0c7044f2f01140a78cee2345ebae6850d6
Alexander Krupenkin <mail@akru.me>
2020-07-08 01:42:48 +0900

    Dusty release 1.2.0 (#195)
    * Bump version
    
    * Fix BTC lock script duration sanity check
    
    * Fix code format
    
    * Fix version

--- bin/node/cli/lockdrop-oracle/src/btc_utils.rs
+++ bin/node/cli/lockdrop-oracle/src/btc_utils.rs
@@ -13,17 +13,21 @@ fn bip68_encode(blocks: u32) -> u32 {
 }
 
 /// Compile BTC sequence lock script for givent public key and duration in blocks.
-pub fn lock_script(public: &ecdsa::Public, duration: u32) -> Script {
-    let public_key = PublicKey::from_slice(public.as_ref()).unwrap();
-    let blocks = bip68_encode(duration) as i64;
-    let script = bitcoin_script! {
-        <blocks>
-        OP_CSV
-        OP_DROP
-        <public_key>
-        OP_CHECKSIG
-    };
-    script.to_p2sh()
+pub fn lock_script(public: &ecdsa::Public, duration: u32) -> Result<Script, String> {
+    if duration > 0 && duration < 65535 {
+        let public_key = PublicKey::from_slice(public.as_ref()).unwrap();
+        let blocks = bip68_encode(duration) as i64;
+        let script = bitcoin_script! {
+            <blocks>
+            OP_CSV
+            OP_DROP
+            <public_key>
+            OP_CHECKSIG
+        };
+        Ok(script.to_p2sh())
+    } else {
+        return Err("Lock duration sanity check failed".to_string());
+    }
 }
 
 pub fn to_address(public: &ecdsa::Public) -> String {
@@ -42,8 +46,13 @@ mod tests {
         let public = ecdsa::Public::from_slice(
             &hex!["038ea27103fb646a2cea9eca9080737e0b23640caaaef2853416c9b286b353313e"][..],
         );
+        assert_eq!(
+            lock_script(&public, 0),
+            Err("Lock duration sanity check failed".to_string())
+        );
+
         let duration = 10;
-        let script = lock_script(&public, duration);
+        let script = lock_script(&public, duration).unwrap();
         let address = Address::from_script(&script, Network::Testnet).unwrap();
         assert_eq!(address.to_string(), "2MuJcWGWe8XkPc6h7pt6vQDyaTwDZxKJZ8p");
     }
