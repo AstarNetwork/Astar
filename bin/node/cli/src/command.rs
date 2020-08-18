48c5d8dba2ee2bdb52d5de3bf439018bb00afafa
Alexander Krupenkin <mail@akru.me>
2020-08-18 19:27:04 +0300

    Collator implementation

--- bin/node/cli/src/command.rs
+++ bin/node/cli/src/command.rs
@@ -1,7 +1,21 @@
-use crate::{chain_spec, service, Cli, Subcommand};
-use plasm_runtime::Block;
-use sc_cli::{ChainSpec, Role, RuntimeVersion, SubstrateCli};
-use sc_service::ServiceParams;
+use crate::{
+    chain_spec,
+    cli::{Cli, RelayChainCli, Subcommand},
+};
+use codec::Encode;
+use cumulus_primitives::ParaId;
+use log::info;
+use plasm_primitives::Block;
+use polkadot_parachain::primitives::AccountIdConversion;
+use sc_cli::{
+    ChainSpec, CliConfiguration, DefaultConfigurationValues, ImportParams, KeystoreParams,
+    NetworkParams, Result, RuntimeVersion, SharedParams, SubstrateCli,
+};
+use sc_service::config::{BasePath, PrometheusConfig};
+use sp_core::hexdisplay::HexDisplay;
+use sp_runtime::traits::{Block as BlockT, Hash as HashT, Header as HeaderT, Zero};
+use sp_runtime::BuildStorage;
+use std::{io::Write, net::SocketAddr, sync::Arc};
 
 impl SubstrateCli for Cli {
     fn impl_name() -> String {
@@ -42,35 +56,283 @@ impl SubstrateCli for Cli {
     }
 }
 
+impl SubstrateCli for RelayChainCli {
+    fn impl_name() -> String {
+        "Plasm Parachain Collator".into()
+    }
+
+    fn impl_version() -> String {
+        env!("SUBSTRATE_CLI_IMPL_VERSION").into()
+    }
+
+    fn description() -> String {
+        env!("CARGO_PKG_DESCRIPTION").into()
+    }
+
+    fn author() -> String {
+        env!("CARGO_PKG_AUTHORS").into()
+    }
+
+    fn support_url() -> String {
+        "https://github.com/staketechnologies/plasm/issues/new".into()
+    }
+
+    fn copyright_start_year() -> i32 {
+        2017
+    }
+
+    fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
+        polkadot_cli::Cli::from_iter([RelayChainCli::executable_name().to_string()].iter())
+            .load_spec(id)
+    }
+
+    fn native_runtime_version(chain_spec: &Box<dyn ChainSpec>) -> &'static RuntimeVersion {
+        polkadot_cli::Cli::native_runtime_version(chain_spec)
+    }
+}
+
+pub fn generate_genesis_state() -> Result<Block> {
+    let storage = (&super::chain_spec::parachain_testnet_config()).build_storage()?;
+
+    let child_roots = storage.children_default.iter().map(|(sk, child_content)| {
+        let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
+            child_content.data.clone().into_iter().collect(),
+        );
+        (sk.clone(), state_root.encode())
+    });
+    let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
+        storage.top.clone().into_iter().chain(child_roots).collect(),
+    );
+
+    let extrinsics_root =
+        <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(Vec::new());
+
+    Ok(Block::new(
+        <<Block as BlockT>::Header as HeaderT>::new(
+            Zero::zero(),
+            extrinsics_root,
+            state_root,
+            Default::default(),
+            Default::default(),
+        ),
+        Default::default(),
+    ))
+}
+
+fn extract_genesis_wasm(chain_spec: &Box<dyn sc_service::ChainSpec>) -> Result<Vec<u8>> {
+    let mut storage = chain_spec.build_storage()?;
+
+    storage
+        .top
+        .remove(sp_core::storage::well_known_keys::CODE)
+        .ok_or_else(|| "Could not find wasm file in genesis state!".into())
+}
+
 /// Parse command line arguments into service configuration.
-pub fn run() -> sc_cli::Result<()> {
+pub fn run() -> Result<()> {
     let cli = Cli::from_args();
 
     match &cli.subcommand {
-        None => {
-            let runner = cli.create_runner(&cli.run)?;
-            runner.run_node_until_exit(|config| {
-                if matches!(config.role, Role::Light) {
-                    return Err("Light client not supporter!".into())
-                }
-                service::run_collator(
-                    config,
-                    cli.parachain_id,
-                    &cli.relaychain_args,
-                    cli.run.validator,
-                )
-            }
-        }
         Some(Subcommand::Base(subcommand)) => {
             let runner = cli.create_runner(subcommand)?;
-            let PartialComponents {
-                client,
-                backend,
-                task_manager,
-                import_queue,
-                ..
-            } = service::new_partial(&mut config)?;
-            Ok((client, backend, import_queue, task_manager))
+
+            runner.run_subcommand(subcommand, |mut config| {
+                let params = crate::service::new_partial::<
+                    plasm_runtime::RuntimeApi,
+                    crate::service::Executor,
+                >(&mut config)?;
+
+                Ok((
+                    params.client,
+                    params.backend,
+                    params.import_queue,
+                    params.task_manager,
+                ))
+            })
         }
+        Some(Subcommand::ExportGenesisState(params)) => {
+            sc_cli::init_logger("");
+
+            let block = generate_genesis_state()?;
+            let header_hex = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
+
+            if let Some(output) = &params.output {
+                std::fs::write(output, header_hex)?;
+            } else {
+                print!("{}", header_hex);
+            }
+
+            Ok(())
+        }
+        None => {
+            let runner = cli.create_runner(&*cli.run)?;
+
+            runner.run_node_until_exit(|config| {
+                // TODO
+                let key = Arc::new(sp_core::Pair::generate().0);
+
+                let extension = chain_spec::Extensions::try_get(&config.chain_spec);
+                let relay_chain_id = extension.map(|e| e.relay_chain.clone());
+                let para_id = extension.map(|e| e.para_id);
+
+                let polkadot_cli = RelayChainCli::new(
+                    config.base_path.as_ref().map(|x| x.path().join("polkadot")),
+                    relay_chain_id,
+                    [RelayChainCli::executable_name().to_string()]
+                        .iter()
+                        .chain(cli.relaychain_args.iter()),
+                );
+
+                let id = ParaId::from(cli.run.parachain_id.or(para_id).unwrap_or(100));
+
+                let parachain_account =
+                    AccountIdConversion::<polkadot_primitives::v0::AccountId>::into_account(&id);
+
+                let block = generate_genesis_state().map_err(|e| format!("{:?}", e))?;
+                let genesis_state = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
+
+                let task_executor = config.task_executor.clone();
+                let polkadot_config =
+                    SubstrateCli::create_configuration(&polkadot_cli, &polkadot_cli, task_executor)
+                        .map_err(|err| format!("Relay chain argument error: {}", err))?;
+
+                info!("Parachain id: {:?}", id);
+                info!("Parachain Account: {}", parachain_account);
+                info!("Parachain genesis state: {}", genesis_state);
+                info!(
+                    "Is collating: {}",
+                    if cli.run.base.validator { "yes" } else { "no" }
+                );
+
+                crate::service::start_node(
+                    config,
+                    key,
+                    polkadot_config,
+                    id,
+                    cli.run.base.validator,
+                )
+            })
+        }
+    }
+}
+
+impl DefaultConfigurationValues for RelayChainCli {
+    fn p2p_listen_port() -> u16 {
+        30334
+    }
+
+    fn rpc_ws_listen_port() -> u16 {
+        9945
+    }
+
+    fn rpc_http_listen_port() -> u16 {
+        9934
+    }
+
+    fn prometheus_listen_port() -> u16 {
+        9616
+    }
+}
+
+impl CliConfiguration<Self> for RelayChainCli {
+    fn shared_params(&self) -> &SharedParams {
+        self.base.base.shared_params()
+    }
+
+    fn import_params(&self) -> Option<&ImportParams> {
+        self.base.base.import_params()
+    }
+
+    fn network_params(&self) -> Option<&NetworkParams> {
+        self.base.base.network_params()
+    }
+
+    fn keystore_params(&self) -> Option<&KeystoreParams> {
+        self.base.base.keystore_params()
+    }
+
+    fn base_path(&self) -> Result<Option<BasePath>> {
+        Ok(self
+            .shared_params()
+            .base_path()
+            .or_else(|| self.base_path.clone().map(Into::into)))
+    }
+
+    fn rpc_http(&self, default_listen_port: u16) -> Result<Option<SocketAddr>> {
+        self.base.base.rpc_http(default_listen_port)
+    }
+
+    fn rpc_ipc(&self) -> Result<Option<String>> {
+        self.base.base.rpc_ipc()
+    }
+
+    fn rpc_ws(&self, default_listen_port: u16) -> Result<Option<SocketAddr>> {
+        self.base.base.rpc_ws(default_listen_port)
+    }
+
+    fn prometheus_config(&self, default_listen_port: u16) -> Result<Option<PrometheusConfig>> {
+        self.base.base.prometheus_config(default_listen_port)
+    }
+
+    fn init<C: SubstrateCli>(&self) -> Result<()> {
+        unreachable!("PolkadotCli is never initialized; qed");
+    }
+
+    fn chain_id(&self, is_dev: bool) -> Result<String> {
+        let chain_id = self.base.base.chain_id(is_dev)?;
+
+        Ok(if chain_id.is_empty() {
+            self.chain_id.clone().unwrap_or_default()
+        } else {
+            chain_id
+        })
+    }
+
+    fn role(&self, is_dev: bool) -> Result<sc_service::Role> {
+        self.base.base.role(is_dev)
+    }
+
+    fn transaction_pool(&self) -> Result<sc_service::config::TransactionPoolOptions> {
+        self.base.base.transaction_pool()
+    }
+
+    fn state_cache_child_ratio(&self) -> Result<Option<usize>> {
+        self.base.base.state_cache_child_ratio()
+    }
+
+    fn rpc_methods(&self) -> Result<sc_service::config::RpcMethods> {
+        self.base.base.rpc_methods()
+    }
+
+    fn rpc_ws_max_connections(&self) -> Result<Option<usize>> {
+        self.base.base.rpc_ws_max_connections()
+    }
+
+    fn rpc_cors(&self, is_dev: bool) -> Result<Option<Vec<String>>> {
+        self.base.base.rpc_cors(is_dev)
+    }
+
+    fn telemetry_external_transport(&self) -> Result<Option<sc_service::config::ExtTransport>> {
+        self.base.base.telemetry_external_transport()
+    }
+
+    fn default_heap_pages(&self) -> Result<Option<u64>> {
+        self.base.base.default_heap_pages()
+    }
+
+    fn force_authoring(&self) -> Result<bool> {
+        self.base.base.force_authoring()
+    }
+
+    fn disable_grandpa(&self) -> Result<bool> {
+        self.base.base.disable_grandpa()
+    }
+
+    fn max_runtime_instances(&self) -> Result<Option<usize>> {
+        self.base.base.max_runtime_instances()
+    }
+
+    fn announce_block(&self) -> Result<bool> {
+        self.base.base.announce_block()
     }
 }
--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -1,43 +1,33 @@
 //! Collator implementation. Specialized wrapper over substrate service.
 
-use plasm_primitives::Block;
-use plasm_runtime::RuntimeApi;
+use ansi_term::Color;
 use cumulus_network::DelayedBlockAnnounceValidator;
 use cumulus_service::{
     prepare_node_config, start_collator, start_full_node, StartCollatorParams, StartFullNodeParams,
 };
-use sc_service::{Configuration, PartialComponents, TFullBackend, TFullClient};
-use sp_runtime::traits::BlakeTwo256;
-use sp_trie::PrefixedMemoryDB;
 use polkadot_primitives::v0::CollatorPair;
+use plasm_primitives::Block;
+use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
 use sc_informant::OutputFormat;
-use sc_service::{Configuration, Role, TaskManager};
-use polkadot_parachain::primitives::AccountIdConversion;
-use polkadot_primitives::v0::Id as ParaId;
-use sc_cli::{
-    ChainSpec, CliConfiguration, ImportParams, KeystoreParams, NetworkParams, Result,
-    RuntimeVersion, SharedParams, SubstrateCli,
-};
-use sc_network::config::TransportConfig;
-use sc_service::{
-    config::{Configuration, NetworkConfiguration, NodeKeyConfig, PrometheusConfig},
-    TaskManager,
-};
-use sp_core::hexdisplay::HexDisplay;
-use sp_runtime::traits::{Block as BlockT, Hash as HashT, Header as HeaderT, Zero};
-use sp_runtime::BuildStorage;
-use std::net::SocketAddr;
-use std::path::PathBuf;
+use sc_service::{Configuration, PartialComponents, Role, TFullBackend, TFullClient, TaskManager};
+use sp_api::ConstructRuntimeApi;
+use sp_runtime::traits::BlakeTwo256;
+use sp_trie::PrefixedMemoryDB;
 use std::sync::Arc;
 
-sc_executor::native_executor_instance!(
+// Native executor instance.
+native_executor_instance!(
     pub Executor,
     plasm_runtime::api::dispatch,
     plasm_runtime::native_version,
 );
 
-pub fn new_partial(
+/// Starts a `ServiceBuilder` for a full service.
+///
+/// Use this macro if you don't actually need the full service, but just the builder in order to
+/// be able to perform chain operations.
+pub fn new_partial<RuntimeApi, Executor>(
     config: &mut Configuration,
 ) -> Result<
     PartialComponents<
@@ -48,12 +38,31 @@ pub fn new_partial(
         sc_transaction_pool::FullPool<Block, TFullClient<Block, RuntimeApi, Executor>>,
         (),
     >,
-> {
+    sc_service::Error,
+>
+where
+    RuntimeApi: ConstructRuntimeApi<Block, TFullClient<Block, RuntimeApi, Executor>>
+        + Send
+        + Sync
+        + 'static,
+    RuntimeApi::RuntimeApi: sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+        + sp_api::Metadata<Block>
+        + sp_session::SessionKeys<Block>
+        + sp_api::ApiExt<
+            Block,
+            Error = sp_blockchain::Error,
+            StateBackend = sc_client_api::StateBackendFor<TFullBackend<Block>, Block>,
+        > + sp_offchain::OffchainWorkerApi<Block>
+        + sp_block_builder::BlockBuilder<Block>,
+    sc_client_api::StateBackendFor<TFullBackend<Block>, Block>: sp_api::StateBackend<BlakeTwo256>,
+    Executor: sc_executor::NativeExecutionDispatch + 'static,
+{
     let inherent_data_providers = sp_inherents::InherentDataProviders::new();
 
     let (client, backend, keystore, task_manager) =
         sc_service::new_full_parts::<Block, RuntimeApi, Executor>(&config)?;
     let client = Arc::new(client);
+
     let registry = config.prometheus_registry();
 
     let transaction_pool = sc_transaction_pool::BasicPool::new_full(
@@ -86,13 +95,39 @@ pub fn new_partial(
     Ok(params)
 }
 
-fn new_collator(
+/// Start a node with the given parachain `Configuration` and relay chain `Configuration`.
+///
+/// This is the actual implementation that is abstract over the executor and the runtime api.
+fn start_node_impl<RuntimeApi, Executor, RB>(
     parachain_config: Configuration,
-    para_id: polkadot_primitives::v0::Id,
     collator_key: Arc<CollatorPair>,
     mut polkadot_config: polkadot_collator::Configuration,
+    id: polkadot_primitives::v0::Id,
     validator: bool,
-) -> sc_service::error::Result<TaskManager> {
+    rpc_ext_builder: RB,
+) -> sc_service::error::Result<(TaskManager, Arc<TFullClient<Block, RuntimeApi, Executor>>)>
+where
+    RuntimeApi: ConstructRuntimeApi<Block, TFullClient<Block, RuntimeApi, Executor>>
+        + Send
+        + Sync
+        + 'static,
+    RuntimeApi::RuntimeApi: sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+        + sp_api::Metadata<Block>
+        + sp_session::SessionKeys<Block>
+        + sp_api::ApiExt<
+            Block,
+            Error = sp_blockchain::Error,
+            StateBackend = sc_client_api::StateBackendFor<TFullBackend<Block>, Block>,
+        > + sp_offchain::OffchainWorkerApi<Block>
+        + sp_block_builder::BlockBuilder<Block>,
+    sc_client_api::StateBackendFor<TFullBackend<Block>, Block>: sp_api::StateBackend<BlakeTwo256>,
+    Executor: sc_executor::NativeExecutionDispatch + 'static,
+    RB: Fn(
+            Arc<TFullClient<Block, RuntimeApi, Executor>>,
+        ) -> jsonrpc_core::IoHandler<sc_rpc::Metadata>
+        + Send
+        + 'static,
+{
     if matches!(parachain_config.role, Role::Light) {
         return Err("Light client not supported!".into());
     }
@@ -101,14 +136,14 @@ fn new_collator(
 
     parachain_config.informant_output_format = OutputFormat {
         enable_color: true,
-        prefix: "[Parachain] ".to_string(),
+        prefix: format!("[{}] ", Color::Yellow.bold().paint("Parachain")),
     };
     polkadot_config.informant_output_format = OutputFormat {
         enable_color: true,
-        prefix: "[Relaychain] ".to_string(),
+        prefix: format!("[{}] ", Color::Blue.bold().paint("Relaychain")),
     };
 
-    let params = super::new_partial(&mut parachain_config)?;
+    let params = new_partial::<RuntimeApi, Executor>(&mut parachain_config)?;
     params
         .inherent_data_providers
         .register_provider(sp_timestamp::InherentDataProvider)
@@ -139,7 +174,12 @@ fn new_collator(
             finality_proof_provider: None,
         })?;
 
-    let rpc_extensions_builder = Box::new(|_| ());
+    let rpc_extensions_builder = {
+        let client = client.clone();
+
+        Box::new(move |_deny_unsafe| rpc_ext_builder(client.clone()))
+    };
+
     sc_service::spawn_tasks(sc_service::SpawnTasksParams {
         on_demand: None,
         remote_blockchain: None,
@@ -164,8 +204,9 @@ fn new_collator(
             transaction_pool,
             prometheus_registry.as_ref(),
         );
+
         let params = StartCollatorParams {
-            para_id,
+            para_id: id,
             block_import: client.clone(),
             proposer_factory,
             inherent_data_providers: params.inherent_data_providers,
@@ -177,6 +218,7 @@ fn new_collator(
             polkadot_config,
             collator_key,
         };
+
         start_collator(params)?;
     } else {
         let params = StartFullNodeParams {
@@ -186,7 +228,7 @@ fn new_collator(
             collator_key,
             block_announce_validator,
             task_manager: &mut task_manager,
-            para_id,
+            para_id: id,
         };
 
         start_full_node(params)?;
@@ -194,222 +236,30 @@ fn new_collator(
 
     start_network.start_network();
 
-    Ok(task_manager)
+    Ok((task_manager, client))
 }
 
-fn generate_genesis_state() -> sc_service::error::Result<Block> {
-    let storage = (&crate::chain_spec::parachain_testnet_config()).build_storage()?;
-
-    let child_roots = storage.children_default.iter().map(|(sk, child_content)| {
-        let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
-            child_content.data.clone().into_iter().collect(),
-        );
-        (sk.clone(), state_root.encode())
-    });
-    let state_root = <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(
-        storage.top.clone().into_iter().chain(child_roots).collect(),
-    );
-
-    let extrinsics_root =
-        <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::trie_root(Vec::new());
-
-    Ok(Block::new(
-        <<Block as BlockT>::Header as HeaderT>::new(
-            Zero::zero(),
-            extrinsics_root,
-            state_root,
-            Default::default(),
-            Default::default(),
-        ),
-        Default::default(),
-    ))
-}
-
-/// Run a collator node with the given parachain `Configuration`
-pub fn run_collator(
-    config: Configuration,
-    parachain_id: u32,
-    relaychain_args: &Vec<String>,
+/// Start a contracts parachain node.
+pub fn start_node(
+    parachain_config: Configuration,
+    collator_key: Arc<CollatorPair>,
+    polkadot_config: polkadot_collator::Configuration,
+    id: polkadot_primitives::v0::Id,
     validator: bool,
 ) -> sc_service::error::Result<TaskManager> {
-    let key = Arc::new(sp_core::Pair::generate().0);
-    let parachain_id = ParaId::from(parachain_id);
+    start_node_impl::<plasm_runtime::RuntimeApi, Executor, _>(
+        parachain_config,
+        collator_key,
+        polkadot_config,
+        id,
+        validator,
+        |client| {
+            let mut io = jsonrpc_core::IoHandler::default();
 
-    let block = generate_genesis_state()?;
-    let header_hex = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
-    let parachain_account =
-        AccountIdConversion::<polkadot_primitives::v0::AccountId>::into_account(&parachain_id);
-
-    info!("[Para] ID: {}", parachain_id);
-    info!("[Para] Account: {}", parachain_account);
-    info!("[Para] Genesis State: {}", header_hex);
-
-    let mut polkadot_cli = PolkadotCli::from_iter(
-        [PolkadotCli::executable_name().to_string()]
-            .iter()
-            .chain(relaychain_args.iter()),
-    );
-    polkadot_cli.base_path = config.base_path.as_ref().map(|x| x.path().join("polkadot"));
-
-    let task_executor = config.task_executor.clone();
-    let polkadot_config =
-        SubstrateCli::create_configuration(&polkadot_cli, &polkadot_cli, task_executor).unwrap();
-
-    new_collator(config, parachain_id, key, polkadot_config, validator)
-}
-
-#[derive(Debug, structopt::StructOpt)]
-pub struct PolkadotCli {
-    #[structopt(flatten)]
-    pub base: polkadot_cli::RunCmd,
-
-    #[structopt(skip)]
-    pub base_path: Option<std::path::PathBuf>,
-}
-
-impl SubstrateCli for PolkadotCli {
-    fn impl_name() -> String {
-        "Plasm Test Parachain Collator".into()
-    }
-
-    fn impl_version() -> String {
-        env!("SUBSTRATE_CLI_IMPL_VERSION").into()
-    }
-
-    fn description() -> String {
-        format!(
-            "plasm test parachain collator\n\nThe command-line arguments provided first will be \
-        passed to the parachain node, while the arguments provided after -- will be passed \
-        to the relaychain node.\n\n\
-        {} [parachain-args] -- [relaychain-args]",
-            Self::executable_name()
-        )
-    }
-
-    fn author() -> String {
-        env!("CARGO_PKG_AUTHORS").into()
-    }
-
-    fn support_url() -> String {
-        "https://github.com/staketechnologies/plasm/issues/new".into()
-    }
-
-    fn copyright_start_year() -> i32 {
-        2019
-    }
-
-    fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
-        let chain_spec = match id {
-            "" => polkadot_service::WestendChainSpec::from_json_bytes(
-                &include_bytes!("../../res/polkadot_chainspec.json")[..],
-            )?,
-            path => {
-                polkadot_service::WestendChainSpec::from_json_file(std::path::PathBuf::from(path))?
-            }
-        };
-        Ok(Box::new(chain_spec))
-    }
-
-    fn native_runtime_version(chain_spec: &Box<dyn ChainSpec>) -> &'static RuntimeVersion {
-        polkadot_cli::Cli::native_runtime_version(chain_spec)
-    }
-}
-
-impl CliConfiguration for PolkadotCli {
-    fn shared_params(&self) -> &SharedParams {
-        self.base.base.shared_params()
-    }
-
-    fn import_params(&self) -> Option<&ImportParams> {
-        self.base.base.import_params()
-    }
-
-    fn network_params(&self) -> Option<&NetworkParams> {
-        self.base.base.network_params()
-    }
-
-    fn keystore_params(&self) -> Option<&KeystoreParams> {
-        self.base.base.keystore_params()
-    }
-
-    fn base_path(&self) -> Result<Option<sc_service::config::BasePath>> {
-        Ok(self
-            .shared_params()
-            .base_path()
-            .or_else(|| self.base_path.clone().map(Into::into)))
-    }
-
-    fn rpc_http(&self) -> Result<Option<SocketAddr>> {
-        let rpc_port = self.base.base.rpc_port;
-        Ok(Some(parse_address(
-            &format!("127.0.0.1:{}", 9934),
-            rpc_port,
-        )?))
-    }
-
-    fn rpc_ws(&self) -> Result<Option<SocketAddr>> {
-        let ws_port = self.base.base.ws_port;
-        Ok(Some(parse_address(
-            &format!("127.0.0.1:{}", 9945),
-            ws_port,
-        )?))
-    }
-
-    fn prometheus_config(&self) -> Result<Option<PrometheusConfig>> {
-        Ok(None)
-    }
-
-    // TODO: we disable mdns for the polkadot node because it prevents the process to exit
-    //       properly. See https://github.com/paritytech/cumulus/issues/57
-    fn network_config(
-        &self,
-        chain_spec: &Box<dyn sc_service::ChainSpec>,
-        is_dev: bool,
-        net_config_dir: PathBuf,
-        client_id: &str,
-        node_name: &str,
-        node_key: NodeKeyConfig,
-    ) -> Result<NetworkConfiguration> {
-        let (mut network, allow_private_ipv4) = self
-            .network_params()
-            .map(|x| {
-                (
-                    x.network_config(
-                        chain_spec,
-                        is_dev,
-                        Some(net_config_dir),
-                        client_id,
-                        node_name,
-                        node_key,
-                    ),
-                    !x.no_private_ipv4,
-                )
-            })
-            .expect("NetworkParams is always available on RunCmd; qed");
-
-        network.transport = TransportConfig::Normal {
-            enable_mdns: false,
-            allow_private_ipv4,
-            wasm_external_transport: None,
-            use_yamux_flow_control: false,
-        };
-
-        Ok(network)
-    }
-
-    fn init<C: SubstrateCli>(&self) -> Result<()> {
-        unreachable!("PolkadotCli is never initialized; qed");
-    }
-}
-
-// copied directly from substrate
-fn parse_address(address: &str, port: Option<u16>) -> std::result::Result<SocketAddr, String> {
-    let mut address: SocketAddr = address
-        .parse()
-        .map_err(|_| format!("Invalid address: {}", address))?;
-    if let Some(port) = port {
-        address.set_port(port);
-    }
-
-    Ok(address)
+            use pallet_contracts_rpc::{Contracts, ContractsApi};
+            io.extend_with(ContractsApi::to_delegate(Contracts::new(client)));
+            io
+        },
+    )
+    .map(|r| r.0)
 }
