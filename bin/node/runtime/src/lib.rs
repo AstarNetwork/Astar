7b71077bab37f38d0e28dbbe6de50acc02229803
Alexander Krupenkin <mail@akru.me>
2020-08-23 16:30:42 +0300

    Bump version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -67,8 +67,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to equal spec_version. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 2,
-    impl_version: 2,
+    spec_version: 3,
+    impl_version: 3,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
 };
