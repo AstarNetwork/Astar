8cba888e893eac0e4973681f601b412024e2ce13
akru <akru@users.noreply.github.com>
2020-10-07 14:27:42 +0000

    deploy: 25fbc0e6b1342885ea9798585346d4a4efe3073e

---
+++ implementors/pallet_evm/trait.FeeCalculator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["plasm_runtime"] = [{"text":"impl FeeCalculator for <a class=\"struct\" href=\"plasm_runtime/struct.FixedGasPrice.html\" title=\"struct plasm_runtime::FixedGasPrice\">FixedGasPrice</a>","synthetic":false,"types":["plasm_runtime::FixedGasPrice"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
