f784f23d76d843acfffd55e77b1c4ec8fe47ac50
Alexander Krupenkin <mail@akru.me>
2020-12-16 09:08:36 +0300

    Added scheduler pallet

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -246,6 +246,23 @@ impl pallet_session::historical::Trait for Runtime {
 }
 
 parameter_types! {
+    pub MaximumSchedulerWeight: Weight = Perbill::from_percent(80) *
+        RuntimeBlockWeights::get().max_block;
+    pub const MaxScheduledPerBlock: u32 = 50;
+}
+
+impl pallet_scheduler::Config for Runtime {
+    type Event = Event;
+    type Origin = Origin;
+    type PalletsOrigin = OriginCaller;
+    type Call = Call;
+    type MaximumWeight = MaximumSchedulerWeight;
+    type ScheduleOrigin = EnsureRoot<AccountId>;
+    type MaxScheduledPerBlock = MaxScheduledPerBlock;
+    type WeightInfo = ();
+}
+
+parameter_types! {
     pub const SessionsPerEra: pallet_plasm_rewards::SessionIndex = 6;
     pub const BondingDuration: pallet_plasm_rewards::EraIndex = 24 * 28;
 }
