19f970b154c31f737239d864fa15351d0b9a0474
akru <akru@users.noreply.github.com>
2021-01-17 07:00:27 +0000

    deploy: 0f2fb58c116e833c9e7ac276c6cbdfe254ee9a0a

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_custom_signatures"] = [{"text":"impl TryFrom&lt;Vec&lt;u8&gt;&gt; for EthereumSignature","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
