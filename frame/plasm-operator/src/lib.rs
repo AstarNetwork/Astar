d2b939573ae84d4f2d6b7d54f0a26ed6534d6435
Mario <mariovukelic@yahoo.com>
2021-03-19 00:10:29 +0100

    use generics

--- frame/plasm-operator/src/lib.rs
+++ frame/plasm-operator/src/lib.rs
@@ -31,10 +31,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*; // reexport in crate namespace for `construct_runtime!`
-use codec::{Encode, Decode};
 use sp_core::{H160};
-use sp_runtime::{RuntimeDebug};
 use sp_std::{prelude::*};
+use frame_support::{dispatch::DispatchResultWithPostInfo};
+
+
+
 #[frame_support::pallet]
 // NOTE: The name of the pallet is provided by `construct_runtime` and is used as
 // the unique identifier for the pallet's storage. It is not defined in the pallet itself.
@@ -42,6 +44,14 @@ pub mod pallet {
     use frame_support::pallet_prelude::*; // Import various types used in the pallet definition
 	use frame_system::pallet_prelude::*; // Import some system helper types.
     use super::*;
+
+    #[derive(Clone, Encode, Decode, Eq, PartialEq, Debug)]
+    pub enum SmartContract<T: Config>
+    {
+        Wasm(T::AccountId),
+        EVM(H160),
+    }
+
     #[pallet::pallet]
     #[pallet::generate_store(trait Store)]
     pub struct Pallet<T>(PhantomData<T>);
@@ -51,41 +61,28 @@ pub mod pallet {
         /// The overarching event type.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
-
+    
     #[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
+    
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-        /// Sets the owner for the given wasm contract.
+        /// Sets the owner for the given smart contract.
         #[pallet::weight(0)] // TODO
-        pub(super) fn claim_contract_wasm(
+        pub(super) fn claim_contract(
             origin: OriginFor<T>,
-            contract: SmartContract<T::AccountId>,
+            contract: SmartContract<T>,
         ) -> DispatchResultWithPostInfo {
-            let owner = ensure_signed(origin)?;
-            // add owner of the contracts
-            <OperatorHasWasmContract<T>>::insert(&owner, contract.clone());
-            // assigns the contract to owner for staking purposes
-            <ContractHasOperator<T>>::insert(&contract, owner.clone());
-
-            Self::deposit_event(Event::SetOperator(owner, contract));
-            Ok(().into())
-        }
-
-        /// Sets the owner for the given EVM contract.
-        #[pallet::weight(0)] // TODO
-        pub(super) fn claim_contract_evm(
-            origin: OriginFor<T>,
-            contract: SmartContract<H160>,
-        ) -> DispatchResultWithPostInfo {
-            let owner = ensure_signed(origin)?;
-            // add owner of the contracts
-            <OperatorHasEvmContract<T>>::insert(&owner, contract.clone());
-            // assigns the contract to owner for staking purposes
-            <ContractHasOperator<T>>::insert(&contract, owner.clone());
-
-            Self::deposit_event(Event::ClaimContract(owner, contract));
+            let operator = ensure_signed(origin)?;
+            if !<ContractHasOperator<T>>::contains_key(&contract) &&
+                !<OperatorHasContract<T>>::contains_key(&operator) {
+                    // add owner of the contracts
+                    <OperatorHasContract<T>>::insert(&operator, contract.clone());
+                    // assigns the contract to owner for staking purposes
+                    <ContractHasOperator<T>>::insert(&contract, operator.clone());
+                    
+                    Self::deposit_event(Event::ContractClaimed(operator, contract));
+                }
             Ok(().into())
         }
 	}
@@ -96,51 +93,28 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
         /// Claim Contract (Owner AccountId, Contract AccountId).
-        ClaimContract(T::AccountId, T::AccountId),
-        
+        ContractClaimed(T::AccountId, SmartContract<T>),
+
 	}
 
     /// A mapping from operators to operated contracts by them.
 	#[pallet::storage]
 	#[pallet::getter(fn get_contract_wasm)]
-    pub(super) type OperatorHasWasmContract<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, SmartContract<T::AccountId> >; 
-
-    /// A mapping from operators to operated contracts by them.
-	#[pallet::storage]
-	#[pallet::getter(fn get_contract_evm)]
-    pub(super) type OperatorHasEvmContract<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, SmartContract<H160> >; 
+    pub(super) type OperatorHasContract<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, SmartContract<T> >; 
 
     /// A mapping from operated contract by operator to it.
     #[pallet::storage]
 	#[pallet::getter(fn get_operator)]
-    pub(super) type ContractHasOperator<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, T::AccountId >;
+    pub(super) type ContractHasOperator<T: Config> = StorageMap<_, Blake2_128Concat, SmartContract<T>, T::AccountId >;
         
 }
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug)]
-pub enum SmartContract<
-AccountId: Encode + Decode + Clone + Eq + PartialEq,
-> 
-{
-    Wasm(AccountId),
-    EVM(H160),
-}
-pub trait IsContract {
-    fn is_contract(&self) -> bool;
-    fn claim_contract(&self) -> Result<bool, sp_runtime::DispatchError>;
+pub trait IsContract<T> {
+    fn claim_contract(&self) -> DispatchResultWithPostInfo;
 }
 
-impl<T: Config> IsContract for SmartContract<T::AccountId> {
-    fn is_contract(smart_contract: SmartContract<T>) -> bool {
-        <ContractHasOperator<T>>::contains_key(smart_contract)
-    }
-    fn claim_contract(origin: T::Origin, smart_contract: SmartContract<T>) -> Result<bool, sp_runtime::DispatchError>{
-        T::claim_contract_wasm(origin, smart_contract)?;
-        Ok(())
 
-    }
-}
-
+// 
 // The main implementation block for the module.
 impl<T: Config> Pallet<T> {
 }
