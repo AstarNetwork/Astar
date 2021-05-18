4e652c44c2749e9e15df736945b0dc0a6abcc4fb
Alexander Krupenkin <mail@akru.me>
2021-05-18 07:26:19 +0300

    Upgrade to latest cumulus

--- runtime/shiden-runtime/src/lib.rs
+++ runtime/shiden-runtime/src/lib.rs
@@ -426,6 +426,12 @@ impl_runtime_apis! {
             Some(Vec::new())
         }
     }
+
+    impl cumulus_primitives_core::CollectCollationInfo<Block> for Runtime {
+        fn collect_collation_info() -> cumulus_primitives_core::CollationInfo {
+            ParachainSystem::collect_collation_info()
+        }
+    }
 }
 
 cumulus_pallet_parachain_system::register_validate_block!(Runtime, Executive);
