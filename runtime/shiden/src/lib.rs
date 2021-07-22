4939768fde15594c7e0c72d8882cc78f58e87ab3
Alexander Krupenkin <mail@akru.me>
2021-07-22 13:38:31 +0300

    Upgrade to polkadot 0.9.8

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -68,7 +68,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 3,
+    spec_version: 4,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
