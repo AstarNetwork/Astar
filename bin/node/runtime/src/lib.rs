9841a01cd0bb3c7d1b0e922b2dcccd7c322d32c9
Mar.io <mario@stake.co.jp>
2021-07-26 09:43:39 +0200

    fix dusty high fee

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -210,7 +210,7 @@ impl pallet_balances::Config for Runtime {
 }
 
 parameter_types! {
-    pub const TransactionByteFee: Balance = 10 * MILLIPLM;
+    pub const TransactionByteFee: Balance = MILLIPLM / 100;
     pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
     pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(1, 100_000);
     pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 1_000_000_000u128);
