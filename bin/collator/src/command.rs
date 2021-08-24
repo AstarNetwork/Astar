480a0c4ddf90806933aa00a4587e1f8fbde81b2b
Aleksandr Krupenkin <mail@akru.me>
2021-08-24 17:03:36 +0900

    Fix shiden spec (drop `lightSyncState` field) (#380)

--- bin/collator/src/command.rs
+++ bin/collator/src/command.rs
@@ -57,7 +57,7 @@ impl SubstrateCli for Cli {
     }
 
     fn support_url() -> String {
-        "https://github.com/staketechnologies/Plasm/issues/new".into()
+        "https://github.com/PlasmNetwork/Astar/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
