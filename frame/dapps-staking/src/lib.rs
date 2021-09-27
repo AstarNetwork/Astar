fdc29ee6c5bbad9e0d8e27bf91a8fd7481f46c46
Dinonard <3002868+Dinonard@users.noreply.github.com>
2021-09-27 09:25:49 +0200

    Feature/dapps staking cleanup (#437)
    * Initial dapps staking cleanup
    
    * Dapps staking cleanup 2nd part
    
    * Dapps staking cleanup part3
    
    * Improved empty current era handling
    
    * Era reward bug fix
    
    * Revert some fixes
    
    * Addressed comments
    
    * Fixed issue with era staking and reward. 3 more UTs need to be changed
    
    * Claim UTs few fixes, more remains to be done

--- frame/dapps-staking/src/lib.rs
+++ frame/dapps-staking/src/lib.rs
@@ -1,20 +1,13 @@
-#![allow(unused)] // TODO: remove this later
-
 //! # dApps Staking Module
 //!
 //! The dApps staking module manages era, total amounts of rewards and how to distribute.
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::{Decode, Encode, HasCompact};
-use frame_support::traits::{Currency, Get};
+use frame_support::traits::Currency;
 use frame_system::{self as system};
-use sp_core::crypto::UncheckedFrom;
-use sp_runtime::{
-    curve::PiecewiseLinear,
-    traits::{AtLeast32BitUnsigned, Saturating, StaticLookup, Zero},
-    RuntimeDebug,
-};
-use sp_std::{collections::btree_map::BTreeMap, prelude::*, vec::Vec};
+use sp_runtime::RuntimeDebug;
+use sp_std::{collections::btree_map::BTreeMap, prelude::*};
 
 pub mod pallet;
 pub mod weights;
@@ -33,73 +26,8 @@ pub use weights::WeightInfo;
 pub type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as system::Config>::AccountId>>::Balance;
 
-pub(crate) type PositiveImbalanceOf<T> =
-    <<T as Config>::Currency as Currency<<T as system::Config>::AccountId>>::PositiveImbalance;
-pub(crate) type NegativeImbalanceOf<T> =
-    <<T as Config>::Currency as Currency<<T as system::Config>::AccountId>>::NegativeImbalance;
-
 /// Counter for the number of eras that have passed.
 pub type EraIndex = u32;
-
-const MAX_UNLOCKING_CHUNKS: usize = 32;
-const MAX_VOTES: usize = 128;
-const VOTES_REQUIREMENT: u32 = 12;
-
-/// A destination account for payment.
-#[derive(PartialEq, Eq, Copy, Clone, Encode, Decode, RuntimeDebug)]
-pub enum RewardDestination<AccountId> {
-    /// Pay into the stash account, increasing the amount at stake accordingly.
-    Staked,
-    /// Pay into the stash account, not increasing the amount at stake.
-    Stash,
-    /// Pay into the controller account.
-    Controller,
-    /// Pay into a specified account.
-    Account(AccountId),
-    /// Receive no reward.
-    None,
-}
-
-impl<AccountId> Default for RewardDestination<AccountId> {
-    fn default() -> Self {
-        RewardDestination::Staked
-    }
-}
-
-/// Information regarding the active era (era in used in session).
-#[derive(Encode, Decode, RuntimeDebug)]
-pub struct ActiveEraInfo {
-    /// Index of era.
-    pub index: EraIndex,
-    /// Moment of start expressed as millisecond from `$UNIX_EPOCH`.
-    ///
-    /// Start can be none if start hasn't been set for the era yet,
-    /// Start is set on the first on_finalize of the era to guarantee usage of `Time`.
-    start: Option<u64>,
-}
-
-/// Handler for determining how much of a balance should be paid out on the current era.
-pub trait EraPayout<Balance> {
-    /// Determine the payout for this era.
-    ///
-    /// Returns the amount to be paid to stakers in this era, as well as whatever else should be
-    /// paid out ("the rest").
-    fn era_payout(
-        total_staked: Balance,
-        total_issuance: Balance,
-        era_duration_millis: u64,
-    ) -> (Balance, Balance);
-}
-
-impl<Balance: Default> EraPayout<Balance> for () {
-    fn era_payout(
-        _total_staked: Balance,
-        _total_issuance: Balance,
-        _era_duration_millis: u64,
-    ) -> (Balance, Balance) {
-        (Default::default(), Default::default())
-    }
-}
 
 /// Mode of era-forcing.
 #[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug)]
@@ -132,19 +60,6 @@ pub struct EraRewardAndStake<Balance> {
     staked: Balance,
 }
 
-/// A record of the nominations made by a specific account.
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
-pub struct Nominations<AccountId, Balance> {
-    /// The targets of nomination and amounts of staking.
-    pub targets: Vec<(AccountId, Balance)>,
-    /// The era the nominations were submitted.
-    ///
-    /// Except for initial nominations which are considered submitted at era 0.
-    pub submitted_in: EraIndex,
-    /// Whether the nominations have been suppressed.
-    pub suppressed: bool,
-}
-
 /// Used to split total EraPayout among contracts.
 /// Each tuple (contract, era) has this structure.
 /// This will be used to reward contracts developer and his stakers.
@@ -156,31 +71,18 @@ pub struct EraStakingPoints<AccountId: Ord, Balance: HasCompact> {
     stakers: BTreeMap<AccountId, Balance>,
 }
 
-/// Just a Balance/BlockNumber tuple to encode when a chunk of funds will be unlocked.
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
-pub struct UnlockChunk<Balance: HasCompact> {
-    /// Amount of funds to be unlocked.
-    #[codec(compact)]
-    value: Balance,
-    /// Era number at which point it'll be unlocked.
-    #[codec(compact)]
-    era: EraIndex,
-}
-
 /// Multi-VM pointer to smart contract instance.
 #[derive(PartialEq, Eq, Copy, Clone, Encode, Decode, RuntimeDebug)]
 pub enum SmartContract<AccountId> {
-    /// Wasm smart contract instance.
-    Wasm(AccountId),
     /// EVM smart contract instance.
     Evm(sp_core::H160),
+    /// Wasm smart contract instance.
+    Wasm(AccountId),
 }
 
 /// The ledger of a (bonded) stash.
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
-pub struct StakingLedger<AccountId, Balance: HasCompact> {
-    /// The stash account whose balance is actually locke,ed and at stake.
-    pub stash: AccountId,
+#[derive(PartialEq, Eq, Clone, Encode, Decode, Default, RuntimeDebug)]
+pub struct StakingLedger<Balance: HasCompact + Default> {
     /// The total amount of the stash's balance that we are currently accounting for.
     /// It's just `active` plus all the `unlocking` balances.
     #[codec(compact)]
@@ -189,94 +91,4 @@ pub struct StakingLedger<AccountId, Balance: HasCompact> {
     /// rounds.
     #[codec(compact)]
     pub active: Balance,
-    /// Any balance that is becoming free, which may eventually be transferred out
-    /// of the stash (assuming it doesn't get slashed first).
-    pub unlocking: Vec<UnlockChunk<Balance>>,
-    /// The latest and highest era which the staker has claimed reward for.
-    pub last_reward: EraIndex,
-}
-
-impl<AccountId, Balance: HasCompact + Copy + Saturating + Ord + Zero>
-    StakingLedger<AccountId, Balance>
-{
-    /// Remove entries from `unlocking` that are sufficiently old and reduce the
-    /// total by the sum of their balances.
-    fn consolidate_unlocked(self, current_era: EraIndex) -> Self {
-        let mut total = self.total;
-        let unlocking: Vec<UnlockChunk<Balance>> = self
-            .unlocking
-            .into_iter()
-            .filter(|chunk| {
-                if chunk.era > current_era {
-                    true
-                } else {
-                    total = total.saturating_sub(chunk.value);
-                    false
-                }
-            })
-            .collect();
-
-        Self {
-            total,
-            active: self.active,
-            stash: self.stash,
-            unlocking,
-            last_reward: self.last_reward,
-        }
-    }
-}
-
-impl<AccountId, Balance> StakingLedger<AccountId, Balance>
-where
-    Balance: AtLeast32BitUnsigned + Saturating + Copy,
-{
-    /// Slash the account for a given amount of balance.
-    ///
-    /// Slashes from `active` funds first, and then `unlocking`, starting with the
-    /// chunks that are closest to unlocking.
-    fn slash(&mut self, mut value: Balance) -> Balance {
-        let pre_total = self.total;
-        let total = &mut self.total;
-        let active = &mut self.active;
-
-        let slash_out_of =
-            |total_remaining: &mut Balance, target: &mut Balance, value: &mut Balance| {
-                let slash_from_target = (*value).min(*target);
-
-                if !slash_from_target.is_zero() {
-                    *target -= slash_from_target;
-                    *total_remaining = total_remaining.saturating_sub(slash_from_target);
-                    *value -= slash_from_target;
-                }
-            };
-
-        slash_out_of(total, active, &mut value);
-
-        let i = self
-            .unlocking
-            .iter_mut()
-            .map(|chunk| {
-                slash_out_of(total, &mut chunk.value, &mut value);
-                chunk.value
-            })
-            .take_while(|value| value.is_zero()) // take all fully-consumed chunks out.
-            .count();
-
-        // kill all drained chunks.
-        let _ = self.unlocking.drain(..i);
-
-        pre_total.saturating_sub(*total)
-    }
-}
-
-#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
-pub enum Vote {
-    Bad,
-    Good,
-}
-
-#[derive(PartialEq, Eq, Clone, Encode, Decode, Default, RuntimeDebug)]
-pub struct VoteCounts {
-    bad: u32,
-    good: u32,
 }
