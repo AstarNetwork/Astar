2635698062f0de982ada487ebef88877beee0408
Dinonard <dino.pacandi@gmail.com>
2021-09-30 17:16:16 +0200

    Aura and BLockReward tuple

--- runtime/local/src/lib.rs
+++ runtime/local/src/lib.rs
@@ -208,7 +208,7 @@ parameter_types! {
 impl pallet_timestamp::Config for Runtime {
     /// A timestamp: milliseconds since the unix epoch.
     type Moment = u64;
-    type OnTimestampSet = BlockReward;
+    type OnTimestampSet = (Aura, BlockReward);
     type MinimumPeriod = MinimumPeriod;
     type WeightInfo = ();
 }
