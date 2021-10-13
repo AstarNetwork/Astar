eec37b605ff9464d3dcff32175ef23aa1a54e6a3
Aleksandr Krupenkin <mail@akru.me>
2021-10-13 16:47:28 +0300

    Astar Collator release 2.6.0 (#473)
    * Add staking precompiles & bump collator version
    
    * Weights adjustment for Shiden and Shibuya (#475)
    
    * Shiden: fix dapps staking block reward handle (#474)
    
    * Migrate Shibuya into Azores relay
    
    Co-authored-by: Dinonard <3002868+Dinonard@users.noreply.github.com>

--- runtime/shiden/src/precompiles.rs
+++ runtime/shiden/src/precompiles.rs
@@ -11,6 +11,7 @@ use pallet_evm_precompile_dispatch::Dispatch;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+use pallet_precompile_staking::Staking;
 use sp_core::H160;
 use sp_std::fmt::Debug;
 use sp_std::marker::PhantomData;
@@ -23,7 +24,7 @@ impl<R> ShidenNetworkPrecompiles<R> {
     /// Return all addresses that contain precompiles. This can be used to populate dummy code
     /// under the precompile.
     pub fn used_addresses<AccountId: From<H160>>() -> impl Iterator<Item = AccountId> {
-        sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 1024, 1025, 1026]
+        sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 1024, 1025, 1026, 20480]
             .into_iter()
             .map(|x| hash(x).into())
     }
@@ -32,10 +33,15 @@ impl<R> ShidenNetworkPrecompiles<R> {
 /// The following distribution has been decided for the precompiles
 /// 0-1023: Ethereum Mainnet Precompiles
 /// 1024-2047 Precompiles that are not in Ethereum Mainnet
-impl<R: pallet_evm::Config> PrecompileSet for ShidenNetworkPrecompiles<R>
+impl<R> PrecompileSet for ShidenNetworkPrecompiles<R>
 where
-    R::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
+    R: pallet_evm::Config + pallet_session::Config + pallet_collator_selection::Config,
     <R::Call as Dispatchable>::Origin: From<Option<R::AccountId>>,
+    R::Call: From<pallet_session::Call<R>>
+        + From<pallet_collator_selection::Call<R>>
+        + Dispatchable<PostInfo = PostDispatchInfo>
+        + GetDispatchInfo
+        + Decode,
 {
     fn execute(
         address: H160,
@@ -57,6 +63,8 @@ where
             a if a == hash(1024) => Some(Sha3FIPS256::execute(input, target_gas, context)),
             a if a == hash(1025) => Some(Dispatch::<R>::execute(input, target_gas, context)),
             a if a == hash(1026) => Some(ECRecoverPublicKey::execute(input, target_gas, context)),
+            // Astar precompiles (starts from 0x5000):
+            a if a == hash(20480) => Some(Staking::<R>::execute(input, target_gas, context)),
             // Default
             _ => None,
         }
