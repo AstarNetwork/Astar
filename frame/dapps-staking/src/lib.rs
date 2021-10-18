92fc32b36ba35f7f2b338ca6b2cc8249490a47be
Alexander Krupenkin <mail@akru.me>
2021-10-18 13:43:55 +0300

    dapp-staking: mark former era field as unused

--- frame/dapps-staking/src/lib.rs
+++ frame/dapps-staking/src/lib.rs
@@ -74,7 +74,7 @@ pub struct EraStakingPoints<AccountId: Ord, Balance: HasCompact> {
     /// The map of stakers and the amount they staked.
     stakers: BTreeMap<AccountId, Balance>,
     // TODO: Get rid of this
-    former_staked_era: EraIndex,
+    _former_staked_era: EraIndex,
     /// Accrued and claimed rewards on this contract both for stakers and the developer
     claimed_rewards: Balance,
 }
