1a6550efd25c0ccecba651a8495f1382fd8206c5
Dinonard <dino.pacandi@gmail.com>
2021-10-20 19:56:03 +0200

    Bump up versions

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -93,7 +93,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 11,
+    spec_version: 12,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
