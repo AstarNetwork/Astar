e15a93163e4dd130c4dc896f860d3e2a072db013
Dinonard <dino.pacandi@gmail.com>
2021-11-29 17:56:35 +0100

    Update versions

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -87,7 +87,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 23,
+    spec_version: 24,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
