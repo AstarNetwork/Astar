9544d5fc5d4decf18e20073fc2a1855e573df22e
Dinonard <dino.pacandi@gmail.com>
2021-12-14 12:50:27 +0100

    Derive ord for SmartContract

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -297,7 +297,9 @@ impl pallet_dapps_staking::Config for Runtime {
 }
 
 /// Multi-VM pointer to smart contract instance.
-#[derive(PartialEq, Eq, Copy, Clone, Encode, Decode, RuntimeDebug, scale_info::TypeInfo)]
+#[derive(
+    PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Encode, Decode, RuntimeDebug, scale_info::TypeInfo,
+)]
 pub enum SmartContract<AccountId> {
     /// EVM smart contract instance.
     Evm(sp_core::H160),
