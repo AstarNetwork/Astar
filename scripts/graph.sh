e2d4df693de849e98c82cd1205672f536ad976cc
Hyungsuk Kang <hskang9@gmail.com>
2022-01-04 02:47:42 +0900

    Add graph startup script

---
+++ scripts/graph.sh
@@ -0,0 +1,28 @@
+function usage {
+   cat << EOF
+Usage: graph.sh -chain <chain name> -rpc-url <RPC url>
+
+Runs Graph node from docker-compose settings configured from input
+EOF
+   exit 1
+}
+
+if [ $# -ne 2 ]; then
+   usage;
+fi
+
+if [ "$1" != "-chain" ]; then
+  usage;
+fi
+
+if [ "$3" != "-rpc-url"]
+Chain=$2
+RPC=$4
+
+git clone https://github.com/graphprotocol/graph-node/ \
+&& cd graph-node/docker
+sudo bash ./setup.sh
+
+sed -Ei "s|mainnet:http://172.19.0.1:8545|$2:$4|g" docker-compose.yml 
+
+sudo docker-compose up
