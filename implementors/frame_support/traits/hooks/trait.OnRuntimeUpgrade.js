bb7a4c9c5eefc2bc3e7e8af3ccb578713de02fe3
Dinonard <Dinonard@users.noreply.github.com>
2022-02-01 10:48:02 +0000

    deploy: 08c5843f01de89fa227aeb20ca8c2d6771426d31

--- implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
+++ implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["shibuya_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shibuya_runtime/struct.DappsStakingMigrationV2.html\" title=\"struct shibuya_runtime::DappsStakingMigrationV2\">DappsStakingMigrationV2</a>","synthetic":false,"types":["shibuya_runtime::DappsStakingMigrationV2"]}];
+implementors["shiden_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shiden_runtime/struct.DappsStakingMigrationV2.html\" title=\"struct shiden_runtime::DappsStakingMigrationV2\">DappsStakingMigrationV2</a>","synthetic":false,"types":["shiden_runtime::DappsStakingMigrationV2"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
