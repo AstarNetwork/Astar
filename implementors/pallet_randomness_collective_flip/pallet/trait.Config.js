8fbbb6f4a9d10000f940c04bbd22cf2301d5fd17
akru <akru@users.noreply.github.com>
2022-02-17 15:54:57 +0000

    deploy: 5f495586ab5e52e6e95dd366a97862c9c7c3e977

--- implementors/pallet_randomness_collective_flip/pallet/trait.Config.js
+++ implementors/pallet_randomness_collective_flip/pallet/trait.Config.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["local_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"local_runtime/struct.Runtime.html\" title=\"struct local_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["local_runtime::Runtime"]}];
+implementors["shibuya_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"shibuya_runtime/struct.Runtime.html\" title=\"struct shibuya_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["shibuya_runtime::Runtime"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
