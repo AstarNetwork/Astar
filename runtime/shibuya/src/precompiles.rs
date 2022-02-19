16a03dd5cd7d27c3b615d5975f87b1af66bfa0b2
Aleksandr Krupenkin <mail@akru.me>
2022-02-19 05:06:34 +0300

    SR25519 precompile integration (#565)
    * Added Sr25519Precompile to local runtime
    
    * Plasm -> Astar
    
    * Update Cargo.lock
    
    * Add sr25519 precompile into shibuya
    
    * Update Cargo.lock

--- runtime/shibuya/src/precompiles.rs
+++ runtime/shibuya/src/precompiles.rs
@@ -1,7 +1,5 @@
 //! The Shibuya Network EVM precompiles. This can be compiled with ``#[no_std]`, ready for Wasm.
 
-#![cfg_attr(not(feature = "std"), no_std)]
-
 use codec::Decode;
 use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo};
 use pallet_evm::{Context, Precompile, PrecompileResult, PrecompileSet};
@@ -10,6 +8,7 @@ use pallet_evm_precompile_dispatch::Dispatch;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+use pallet_evm_precompile_sr25519::Sr25519Precompile;
 use pallet_precompile_dapps_staking::DappsStakingWrapper;
 use sp_core::H160;
 use sp_std::fmt::Debug;
@@ -27,7 +26,7 @@ impl<R> ShibuyaNetworkPrecompiles<R> {
     /// Return all addresses that contain precompiles. This can be used to populate dummy code
     /// under the precompile.
     pub fn used_addresses() -> impl Iterator<Item = H160> {
-        sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 1024, 1025, 1026, 20481]
+        sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 1024, 1025, 1026, 20481, 20482]
             .into_iter()
             .map(|x| hash(x))
     }
@@ -76,6 +75,10 @@ where
             // Astar precompiles (starts from 0x5000):
             // DappStaking 0x5001
             a if a == hash(20481) => Some(DappsStakingWrapper::<R>::execute(
+                input, target_gas, context, is_static,
+            )),
+            // Sr25519     0x5002
+            a if a == hash(20482) => Some(Sr25519Precompile::<R>::execute(
                 input, target_gas, context, is_static,
             )),
             // Default
