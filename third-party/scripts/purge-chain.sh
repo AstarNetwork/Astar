db506107d36356977f5035354d7230d0752df854
Raymond Z <raykyri@gmail.com>
2022-02-23 13:51:16 -0500

    Fix purge chain script

--- third-party/scripts/purge-chain.sh
+++ third-party/scripts/purge-chain.sh
@@ -2,61 +2,46 @@
 db=$1
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  echo "Clearing local data from home dir: $HOME/.local/share/plasm-node"
-	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/staging_testnet/db/
-	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/dev/db/
-		rm -rf ~/.local/share/plasm-node/chains/development/db/
-	elif [[ "$db" == "plasm-node" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node_testnet/db/
+  echo "Clearing local data from home dir: $HOME/.local/share/astar-collator"
+	if [[ "$db" == "dev" ]]; then
+		rm -rf ~/.local/share/astar-collator/chains/dev/db/
+		rm -rf ~/.local/share/astar-collator/chains/development/db/
+	elif [[ "$db" == "shiden" ]]; then
+		rm -rf ~/.local/share/astar-collator/chains/shiden/db/
 	else
 		db="all"
-		rm -rf ~/.local/share/plasm-node/chains/dev/db/
-		rm -rf ~/.local/share/plasm-node/chains/development/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node_testnet/db/
-		rm -rf ~/.local/share/plasm-node/chains/staging_testnet/db/
-		rm -rf ~/.local/share/plasm-node/chains/local_testnet/db/
+		rm -rf ~/.local/share/astar-collator/chains/dev/db/
+		rm -rf ~/.local/share/astar-collator/chains/development/db/
+		rm -rf ~/.local/share/astar-collator/chains/shiden/db/
+		rm -rf ~/.local/share/astar-collator/chains/astar/db/
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "Clearing local data from home dir: $HOME/Library/Application Support/plasm-node"
-	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/staging_testnet/db/
-	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/dev/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/development/db/
-	elif [[ "$db" == "plasm-node" ]]; then
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/plasm-node/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/plasm-node_testnet/db/
+  echo "Clearing local data from home dir: $HOME/Library/Application Support/astar-collator"
+	if [[ "$db" == "dev" ]]; then
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/dev/db/
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/development/db/
+	elif [[ "$db" == "shiden" ]]; then
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/shiden/db/
 	else
 		db="all"
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/dev/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/development/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/plasm-node/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/plasm-node_testnet/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/staging_testnet/db/
-		rm -rf ~/Library/Application\ Support/plasm-node/chains/local_testnet/db/
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/dev/db/
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/development/db/
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/shiden/db/
+		rm -rf ~/Library/Application\ Support/astar-collator/chains/astar/db/
 	fi
 else
-  echo "Clearing local data from home dir: $HOME/.local/share/plasm-node"
-	if [[ "$db" == "staging" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/staging_testnet/db/
-	elif [[ "$db" == "dev" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/dev/db/
-		rm -rf ~/.local/share/plasm-node/chains/development/db/
-	elif [[ "$db" == "plasm-node" ]]; then
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node_testnet/db/
+  echo "Clearing local data from home dir: $HOME/.local/share/astar-collator"
+	if [[ "$db" == "dev" ]]; then
+		rm -rf ~/.local/share/astar-collator/chains/dev/db/
+		rm -rf ~/.local/share/astar-collator/chains/development/db/
+	elif [[ "$db" == "shiden" ]]; then
+		rm -rf ~/.local/share/astar-collator/chains/shiden/db/
 	else
 		db="all"
-		rm -rf ~/.local/share/plasm-node/chains/dev/db/
-		rm -rf ~/.local/share/plasm-node/chains/development/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node/db/
-		rm -rf ~/.local/share/plasm-node/chains/plasm-node_testnet/db/
-		rm -rf ~/.local/share/plasm-node/chains/staging_testnet/db/
-		rm -rf ~/.local/share/plasm-node/chains/local_testnet/db/
+		rm -rf ~/.local/share/astar-collator/chains/dev/db/
+		rm -rf ~/.local/share/astar-collator/chains/development/db/
+		rm -rf ~/.local/share/astar-collator/chains/shiden/db/
+		rm -rf ~/.local/share/astar-collator/chains/astar/db/
 	fi
 fi
 
