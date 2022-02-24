b5d966302fc2f646faad767636221a04b0bd5849
Dinonard <3002868+Dinonard@users.noreply.github.com>
2022-02-24 12:32:13 +0100

    Tag usage & version uplift (#576)

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -87,7 +87,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 39,
+    spec_version: 40,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
