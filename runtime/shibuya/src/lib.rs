969626d31f3a748d49e8b4d12c97d2f28416a923
Sven | Supercolony <sven.seven@supercolony.net>
2022-03-15 11:49:36 +0200

    Turn on contracts unstable interface feature (#593)
    * turn on contracts unstable interface feature
    
    * turn on unstable interface feature for pallet-contracts
    
    * bump shibuya runtime spec_version, as new feature was enabled

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 35,
+    spec_version: 36,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
