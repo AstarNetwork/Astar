230364ff752ef1829cac2d50f2e8d6f2dd830958
akru <akru@users.noreply.github.com>
2022-03-22 05:35:45 +0000

    deploy: fe17b422c0e061a7a1211f2a41e67df32883c01d

--- implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
+++ implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["local_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"local_runtime/struct.DappsStakingMigrationV3.html\" title=\"struct local_runtime::DappsStakingMigrationV3\">DappsStakingMigrationV3</a>","synthetic":false,"types":["local_runtime::DappsStakingMigrationV3"]}];
 implementors["shibuya_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shibuya_runtime/struct.DappsStakingMigrationV3.html\" title=\"struct shibuya_runtime::DappsStakingMigrationV3\">DappsStakingMigrationV3</a>","synthetic":false,"types":["shibuya_runtime::DappsStakingMigrationV3"]}];
+implementors["shiden_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shiden_runtime/struct.DappsStakingMigrationV3.html\" title=\"struct shiden_runtime::DappsStakingMigrationV3\">DappsStakingMigrationV3</a>","synthetic":false,"types":["shiden_runtime::DappsStakingMigrationV3"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
