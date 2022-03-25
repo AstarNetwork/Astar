ce649961d721b6898ab238d703f69a13c25793c4
Dinonard <dino.pacandi@gmail.com>
2022-03-25 16:31:21 +0100

    Dynamic inflation for local & Shibuya
    Spec uplift, dependency fix
    
    benchmark features for new palelt
    
    Adapt to latest block reward pallet

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -7,7 +7,7 @@
 use codec::{Decode, Encode};
 use frame_support::{
     construct_runtime, parameter_types,
-    traits::{Contains, Currency, FindAuthor, Imbalance, Nothing, OnRuntimeUpgrade, OnUnbalanced},
+    traits::{Contains, Currency, FindAuthor, Imbalance, Nothing, OnRuntimeUpgrade, OnUnbalanced, Get},
     weights::{
         constants::{BlockExecutionWeight, ExtrinsicBaseWeight, WEIGHT_PER_SECOND},
         DispatchClass, Weight, WeightToFeeCoefficient, WeightToFeeCoefficients,
@@ -275,7 +275,6 @@ impl pallet_custom_signatures::Config for Runtime {
 parameter_types! {
     pub const BlockPerEra: BlockNumber = 4 * HOURS;
     pub const RegisterDeposit: Balance = 100 * SDN;
-    pub const DeveloperRewardPercentage: Perbill = Perbill::from_percent(50);
     pub const MaxNumberOfStakersPerContract: u32 = 2048;
     pub const MinimumStakingAmount: Balance = 5 * SDN;
     pub const MinimumRemainingAmount: Balance = 1 * SDN;
@@ -289,7 +288,6 @@ impl pallet_dapps_staking::Config for Runtime {
     type BlockPerEra = BlockPerEra;
     type SmartContract = SmartContract<AccountId>;
     type RegisterDeposit = RegisterDeposit;
-    type DeveloperRewardPercentage = DeveloperRewardPercentage;
     type Event = Event;
     type WeightInfo = weights::pallet_dapps_staking::WeightInfo<Runtime>;
     type MaxNumberOfStakersPerContract = MaxNumberOfStakersPerContract;
@@ -439,19 +437,25 @@ impl OnUnbalanced<NegativeImbalance> for ToStakingPot {
     }
 }
 
-pub struct OnBlockReward;
-impl OnUnbalanced<NegativeImbalance> for OnBlockReward {
-    fn on_nonzero_unbalanced(amount: NegativeImbalance) {
-        let (dapps, maintain) = amount.ration(50, 50);
+pub struct DappsStakingTvlProvider();
+impl Get<Balance> for DappsStakingTvlProvider {
+    fn get() -> Balance {
+        DappsStaking::tvl()
+    }
+}
 
-        // dapp staking block reward
-        DappsStaking::on_unbalanced(dapps);
+pub struct BeneficiaryPayout();
+impl pallet_block_reward::BeneficiaryPayout<NegativeImbalance> for BeneficiaryPayout {
+    fn treasury(reward: NegativeImbalance) {
+        Balances::resolve_creating(&TreasuryPalletId::get().into_account(), reward);
+    }
 
-        let (treasury, collators) = maintain.ration(40, 10);
-        // treasury slice of block reward
-        Balances::resolve_creating(&TreasuryPalletId::get().into_account(), treasury);
-        // collators block reward
-        ToStakingPot::on_unbalanced(collators);
+    fn collators(reward: NegativeImbalance) {
+        ToStakingPot::on_unbalanced(reward);
+    }
+
+    fn dapps_staking(stakers: NegativeImbalance, dapps: NegativeImbalance) {
+        DappsStaking::rewards(stakers, dapps)
     }
 }
 
@@ -461,8 +465,11 @@ parameter_types! {
 
 impl pallet_block_reward::Config for Runtime {
     type Currency = Balances;
-    type OnBlockReward = OnBlockReward;
+    type DappsStakingTvlProvider = DappsStakingTvlProvider;
+    type BeneficiaryPayout = BeneficiaryPayout;
     type RewardAmount = RewardAmount;
+    type Event = Event;
+    type WeightInfo = pallet_block_reward::weights::SubstrateWeight<Runtime>;
 }
 
 parameter_types! {
@@ -730,8 +737,8 @@ construct_runtime!(
         TransactionPayment: pallet_transaction_payment::{Pallet, Storage} = 30,
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>} = 31,
         Vesting: pallet_vesting::{Pallet, Call, Storage, Config<T>, Event<T>} = 32,
-        BlockReward: pallet_block_reward::{Pallet} = 33,
         DappsStaking: pallet_dapps_staking::{Pallet, Call, Storage, Event<T>} = 34,
+        BlockReward: pallet_block_reward::{Pallet, Call, Storage, Config, Event<T>} = 35,
 
         Authorship: pallet_authorship::{Pallet, Call, Storage, Inherent} = 40,
         CollatorSelection: pallet_collator_selection::{Pallet, Call, Storage, Event<T>, Config<T>} = 41,
@@ -1179,6 +1186,7 @@ impl_runtime_apis! {
             let mut list = Vec::<BenchmarkList>::new();
 
             list_benchmark!(list, extra, pallet_dapps_staking, DappsStaking);
+            list_benchmark!(list, extra, pallet_block_reward, BlockReward);
 
             let storage_info = AllPalletsWithSystem::storage_info();
 
@@ -1211,6 +1219,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_balances, Balances);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
             add_benchmark!(params, batches, pallet_dapps_staking, DappsStaking);
+            add_benchmark!(params, batches, pallet_block_reward, BlockReward);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
