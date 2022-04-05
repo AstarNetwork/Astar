c8cc3c601051354dd3e0cec59b5ef5404eaf546b
Dinonard <3002868+Dinonard@users.noreply.github.com>
2022-04-05 09:59:32 +0200

    Shiden upgrade - block reward v2 (#607)
    * Shiden upgrade with dynamic inflation
    
    * Update Shiden chaispec
    
    * Remove incorrect comment
    
    * Shibuya version uplift

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -7,7 +7,7 @@
 use codec::{Decode, Encode};
 use frame_support::{
     construct_runtime, parameter_types,
-    traits::{Contains, Currency, FindAuthor, Imbalance, OnRuntimeUpgrade, OnUnbalanced},
+    traits::{Contains, Currency, FindAuthor, Get, Imbalance, OnRuntimeUpgrade, OnUnbalanced},
     weights::{
         constants::{BlockExecutionWeight, ExtrinsicBaseWeight, WEIGHT_PER_SECOND},
         DispatchClass, Weight, WeightToFeeCoefficient, WeightToFeeCoefficients,
@@ -271,7 +271,6 @@ impl pallet_custom_signatures::Config for Runtime {
 parameter_types! {
     pub const BlockPerEra: BlockNumber = 1 * DAYS;
     pub const RegisterDeposit: Balance = 100 * SDN;
-    pub const DeveloperRewardPercentage: Perbill = Perbill::from_percent(50);
     pub const MaxNumberOfStakersPerContract: u32 = 1024;
     pub const MinimumStakingAmount: Balance = 50 * SDN;
     pub const MinimumRemainingAmount: Balance = 1 * SDN;
@@ -285,7 +284,6 @@ impl pallet_dapps_staking::Config for Runtime {
     type BlockPerEra = BlockPerEra;
     type SmartContract = SmartContract<AccountId>;
     type RegisterDeposit = RegisterDeposit;
-    type DeveloperRewardPercentage = DeveloperRewardPercentage;
     type Event = Event;
     type WeightInfo = weights::pallet_dapps_staking::WeightInfo<Runtime>;
     type MaxNumberOfStakersPerContract = MaxNumberOfStakersPerContract;
@@ -435,18 +433,25 @@ impl OnUnbalanced<NegativeImbalance> for ToStakingPot {
     }
 }
 
-pub struct OnBlockReward;
-impl OnUnbalanced<NegativeImbalance> for OnBlockReward {
-    fn on_nonzero_unbalanced(amount: NegativeImbalance) {
-        let (dapps, maintain) = amount.ration(50, 50);
-        // dapp staking block reward
-        DappsStaking::on_unbalanced(dapps);
+pub struct DappsStakingTvlProvider();
+impl Get<Balance> for DappsStakingTvlProvider {
+    fn get() -> Balance {
+        DappsStaking::tvl()
+    }
+}
 
-        let (treasury, collators) = maintain.ration(40, 10);
-        // treasury slice of block reward
-        Balances::resolve_creating(&TreasuryPalletId::get().into_account(), treasury);
-        // collators block reward
-        ToStakingPot::on_unbalanced(collators);
+pub struct BeneficiaryPayout();
+impl pallet_block_reward::BeneficiaryPayout<NegativeImbalance> for BeneficiaryPayout {
+    fn treasury(reward: NegativeImbalance) {
+        Balances::resolve_creating(&TreasuryPalletId::get().into_account(), reward);
+    }
+
+    fn collators(reward: NegativeImbalance) {
+        ToStakingPot::on_unbalanced(reward);
+    }
+
+    fn dapps_staking(stakers: NegativeImbalance, dapps: NegativeImbalance) {
+        DappsStaking::rewards(stakers, dapps)
     }
 }
 
@@ -456,8 +461,11 @@ parameter_types! {
 
 impl pallet_block_reward::Config for Runtime {
     type Currency = Balances;
-    type OnBlockReward = OnBlockReward;
+    type DappsStakingTvlProvider = DappsStakingTvlProvider;
+    type BeneficiaryPayout = BeneficiaryPayout;
     type RewardAmount = RewardAmount;
+    type Event = Event;
+    type WeightInfo = pallet_block_reward::weights::SubstrateWeight<Runtime>;
 }
 
 parameter_types! {
@@ -683,8 +691,8 @@ construct_runtime!(
         TransactionPayment: pallet_transaction_payment::{Pallet, Storage} = 30,
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>} = 31,
         Vesting: pallet_vesting::{Pallet, Call, Storage, Config<T>, Event<T>} = 32,
-        BlockReward: pallet_block_reward::{Pallet} = 33,
         DappsStaking: pallet_dapps_staking::{Pallet, Call, Storage, Event<T>} = 34,
+        BlockReward: pallet_block_reward::{Pallet, Call, Storage, Config, Event<T>} = 35,
 
         Authorship: pallet_authorship::{Pallet, Call, Storage, Inherent} = 40,
         CollatorSelection: pallet_collator_selection::{Pallet, Call, Storage, Event<T>, Config<T>} = 41,
@@ -748,32 +756,37 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
-    DappsStakingFixEraLength,
+    (InitRewardConfigSettings,),
 >;
 
-// Migration for fixing era length in dapps staking.
-pub struct DappsStakingFixEraLength;
-
-impl OnRuntimeUpgrade for DappsStakingFixEraLength {
+pub struct InitRewardConfigSettings;
+impl OnRuntimeUpgrade for InitRewardConfigSettings {
     fn on_runtime_upgrade() -> frame_support::weights::Weight {
-        use frame_system::pallet::Config;
-        use pallet_dapps_staking::{Config as DappStakingConfig, CurrentEra, NextEraStartingBlock};
-        use sp_runtime::SaturatedConversion;
+        // TODO: discuss these params on sync & with community
+        let mut reward_config = pallet_block_reward::RewardDistributionConfig {
+            base_treasury_percent: Perbill::from_percent(40),
+            base_staker_percent: Perbill::from_percent(25),
+            dapps_percent: Perbill::from_percent(25),
+            collators_percent: Perbill::from_percent(10),
+            adjustable_percent: Perbill::from_percent(0),
+            ideal_dapps_staking_tvl: Perbill::from_percent(0),
+        };
+        // This HAS to be tested prior to update - we need to ensure that config is consistent
+        #[cfg(feature = "try-runtime")]
+        assert!(reward_config.is_consistent());
 
-        let dapps_staking_block_offset: u32 = 489600; // calculated 2022/03/29
-                                                      // Shiden: 1384490 - (1384490 % 7200) - (124 * 7200)
+        // This should never execute but we need to have code in place that ensures config is consistent
+        if !reward_config.is_consistent() {
+            reward_config = Default::default();
+        }
+        pallet_block_reward::RewardDistributionConfigStorage::<Runtime>::put(reward_config);
 
-        let blocks_per_era =
-            <Runtime as DappStakingConfig>::BlockPerEra::get().saturated_into::<u32>();
-        let current_era = CurrentEra::<Runtime>::get();
+        // Do some storage cleanup for dapps-staking so we can remove these DB entries in the future
+        pallet_dapps_staking::MigrationStateV2::<Runtime>::kill();
+        pallet_dapps_staking::MigrationStateV3::<Runtime>::kill();
+        pallet_dapps_staking::MigrationUndergoingUnbonding::<Runtime>::kill();
 
-        let next_era_starting_block =
-            dapps_staking_block_offset + ((current_era + 1) * blocks_per_era);
-
-        NextEraStartingBlock::<Runtime>::put(
-            next_era_starting_block.saturated_into::<<Runtime as Config>::BlockNumber>(),
-        );
-        <Runtime as Config>::DbWeight::get().reads_writes(1, 1)
+        <Runtime as frame_system::pallet::Config>::DbWeight::get().writes(4)
     }
 }
 
@@ -1085,6 +1098,7 @@ impl_runtime_apis! {
             let mut list = Vec::<BenchmarkList>::new();
 
             list_benchmark!(list, extra, pallet_dapps_staking, DappsStaking);
+            list_benchmark!(list, extra, pallet_block_reward, BlockReward);
 
             let storage_info = AllPalletsWithSystem::storage_info();
 
@@ -1117,6 +1131,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_balances, Balances);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
             add_benchmark!(params, batches, pallet_dapps_staking, DappsStaking);
+            add_benchmark!(params, batches, pallet_block_reward, BlockReward);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
