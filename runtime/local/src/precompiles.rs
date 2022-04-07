be4c36bd424ceec42cc1ff30fc6398df7303b29a
Mar.io <mario@stake.co.jp>
2022-04-07 22:09:42 +0200

    added ed25519

--- runtime/local/src/precompiles.rs
+++ runtime/local/src/precompiles.rs
@@ -4,6 +4,7 @@ use pallet_evm::{Context, Precompile, PrecompileResult, PrecompileSet};
 use pallet_evm_precompile_blake2::Blake2F;
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_dispatch::Dispatch;
+use pallet_evm_precompile_ed25519::Ed25519Verify;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
@@ -59,7 +60,6 @@ where
             a if a == hash(7) => Some(Bn128Mul::execute(input, target_gas, context, is_static)),
             a if a == hash(8) => Some(Bn128Pairing::execute(input, target_gas, context, is_static)),
             a if a == hash(9) => Some(Blake2F::execute(input, target_gas, context, is_static)),
-
             // nor Ethereum precompiles :
             a if a == hash(1024) => {
                 Some(Sha3FIPS256::execute(input, target_gas, context, is_static))
@@ -68,6 +68,9 @@ where
                 input, target_gas, context, is_static,
             )),
             a if a == hash(1026) => Some(ECRecoverPublicKey::execute(
+                input, target_gas, context, is_static,
+            )),
+            a if a == hash(1027) => Some(Ed25519Verify::execute(
                 input, target_gas, context, is_static,
             )),
             // Astar precompiles (starts from 0x5000):
