4f7c04ad7241e4c39fb8a204329b43b8ca87257d
Dinonard <Dinonard@users.noreply.github.com>
2022-04-12 14:09:37 +0000

    deploy: 9f204f178b3a7f76122526d7dbf5d51929969f51

--- implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
+++ implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["astar_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"astar_runtime/struct.FestivalReset.html\" title=\"struct astar_runtime::FestivalReset\">FestivalReset</a>","synthetic":false,"types":["astar_runtime::FestivalReset"]}];
 implementors["shibuya_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shibuya_runtime/struct.InitRewardConfigSettings.html\" title=\"struct shibuya_runtime::InitRewardConfigSettings\">InitRewardConfigSettings</a>","synthetic":false,"types":["shibuya_runtime::InitRewardConfigSettings"]}];
 implementors["shiden_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shiden_runtime/struct.InitRewardConfigSettings.html\" title=\"struct shiden_runtime::InitRewardConfigSettings\">InitRewardConfigSettings</a>","synthetic":false,"types":["shiden_runtime::InitRewardConfigSettings"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
