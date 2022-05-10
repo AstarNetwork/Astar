c3eccfb2e1434b717cfdab610fabfdff3caf7ee7
akru <akru@users.noreply.github.com>
2022-05-10 05:54:41 +0000

    deploy: ecdca1cf881b65f2b658cee98673695c36d08577

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["astar_collator"] = {"name":"","dirs":[{"name":"local","files":["chain_spec.rs","mod.rs","service.rs"]},{"name":"parachain","dirs":[{"name":"chain_spec","files":["astar.rs","mod.rs","shibuya.rs","shiden.rs"]}],"files":["mod.rs","service.rs","shell_upgrade.rs"]}],"files":["cli.rs","command.rs","lib.rs","primitives.rs","rpc.rs"]};
-sourcesIndex["astar_runtime"] = {"name":"","dirs":[{"name":"weights","files":["mod.rs","pallet_dapps_staking.rs"]}],"files":["lib.rs","precompiles.rs"]};
+sourcesIndex["astar_runtime"] = {"name":"","dirs":[{"name":"weights","files":["mod.rs","pallet_dapps_staking.rs"]}],"files":["lib.rs","precompiles.rs","xcm_config.rs"]};
 sourcesIndex["local_runtime"] = {"name":"","dirs":[{"name":"weights","files":["mod.rs","pallet_dapps_staking.rs"]}],"files":["lib.rs","precompiles.rs"]};
 sourcesIndex["shibuya_runtime"] = {"name":"","dirs":[{"name":"weights","files":["mod.rs","pallet_dapps_staking.rs"]}],"files":["lib.rs","precompiles.rs","xcm_config.rs"]};
 sourcesIndex["shiden_runtime"] = {"name":"","dirs":[{"name":"weights","files":["mod.rs","pallet_dapps_staking.rs"]}],"files":["lib.rs","precompiles.rs","xcm_config.rs"]};
