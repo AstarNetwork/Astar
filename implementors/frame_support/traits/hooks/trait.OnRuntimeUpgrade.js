b3175f4ce450ef77bb1a4baa594c08829cf49945
Dinonard <Dinonard@users.noreply.github.com>
2022-05-11 09:30:38 +0000

    deploy: efc419a7d34f28d9441d32ddc608d57d074fee48

--- implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
+++ implementors/frame_support/traits/hooks/trait.OnRuntimeUpgrade.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["shibuya_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shibuya_runtime/struct.RestakeStorageFix.html\" title=\"struct shibuya_runtime::RestakeStorageFix\">RestakeStorageFix</a>","synthetic":false,"types":["shibuya_runtime::RestakeStorageFix"]}];
 implementors["shiden_runtime"] = [{"text":"impl OnRuntimeUpgrade for <a class=\"struct\" href=\"shiden_runtime/struct.RestakeStorageFix.html\" title=\"struct shiden_runtime::RestakeStorageFix\">RestakeStorageFix</a>","synthetic":false,"types":["shiden_runtime::RestakeStorageFix"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
