2ec86b6ad5019984ca29cc09da9b29d96612817a
Dinonard <Dinonard@users.noreply.github.com>
2022-06-16 08:14:36 +0000

    deploy: 9838ec28cee6507f42f28cb7099e33c6056f72c2

--- implementors/shiden_runtime/trait.BuildStorage.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["shiden_runtime"] = [{"text":"impl <a class=\"trait\" href=\"shiden_runtime/trait.BuildStorage.html\" title=\"trait shiden_runtime::BuildStorage\">BuildStorage</a> for <a class=\"struct\" href=\"shiden_runtime/struct.GenesisConfig.html\" title=\"struct shiden_runtime::GenesisConfig\">GenesisConfig</a>","synthetic":false,"types":["shiden_runtime::GenesisConfig"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
