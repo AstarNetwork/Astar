7fc8fe70cfa7b97ffd02a1ddde913d7f30a0bb3b
Dinonard <3002868+Dinonard@users.noreply.github.com>
2022-07-04 10:25:45 +0200

    Shiden XCM fee handler (#691)

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 59,
+    spec_version: 60,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
@@ -440,6 +440,7 @@ impl pallet_collator_selection::Config for Runtime {
 parameter_types! {
     pub const TreasuryPalletId: PalletId = PalletId(*b"py/trsry");
     pub const DappsStakingPalletId: PalletId = PalletId(*b"py/dpsst");
+    pub TreasuryAccountId: AccountId = TreasuryPalletId::get().into_account_truncating();
 }
 
 type NegativeImbalance = <Balances as Currency<AccountId>>::NegativeImbalance;
