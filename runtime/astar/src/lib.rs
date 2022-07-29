88cbfe49db4b00f3879307e470aa0e0d56c3e1c7
Dinonard <dino.pacandi@gmail.com>
2022-07-29 07:24:37 +0200

    Bump spec versions

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("astar"),
     impl_name: create_runtime_str!("astar"),
     authoring_version: 1,
-    spec_version: 29,
+    spec_version: 30,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
