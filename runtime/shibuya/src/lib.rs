76701dd79d3b038b20be05299ae46cd1b0060da0
Dinonard <dino.pacandi@gmail.com>
2022-09-12 11:05:55 +0200

    Shiden & Shibuya bump

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -136,7 +136,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 66,
+    spec_version: 67,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
