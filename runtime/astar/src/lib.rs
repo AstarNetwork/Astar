1fccce0b2eae9d505934fd1e7e99e56ef7b7b30e
Alexander Krupenkin <mail@akru.me>
2022-09-22 03:46:08 +0300

    Bump substrate version

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("astar"),
     impl_name: create_runtime_str!("astar"),
     authoring_version: 1,
-    spec_version: 34,
+    spec_version: 35,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
