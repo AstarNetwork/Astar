5e99f9a9d7729ecc0abc257f1f66deb188fcced1
Dino Pačandi <3002868+Dinonard@users.noreply.github.com>
2022-10-18 12:49:11 +0200

    Add account32hash to xcm-tools (#766)
    * Add account32hash to xcm-tools
    
    * Polish & small changes
    
    * Shorten macros
    
    * taplo fmt
    
    * Review comments addressed

--- bin/xcm-tools/src/command.rs
+++ bin/xcm-tools/src/command.rs
@@ -7,9 +7,9 @@ use cumulus_primitives_core::ParaId;
 use polkadot_parachain::primitives::Sibling;
 use polkadot_primitives::v2::AccountId;
 use sp_core::hexdisplay::HexDisplay;
-use sp_runtime::traits::AccountIdConversion;
+use sp_runtime::traits::{AccountIdConversion, Get};
 use xcm::latest::prelude::*;
-use xcm_builder::SiblingParachainConvertsVia;
+use xcm_builder::{Account32Hash, SiblingParachainConvertsVia};
 use xcm_executor::traits::Convert;
 
 /// CLI error type.
@@ -40,6 +40,40 @@ pub fn run() -> Result<(), Error> {
             data[4..20].copy_from_slice(&cmd.asset_id.to_be_bytes());
             println!("pallet_assets: {}", cmd.asset_id);
             println!("EVM XC20: 0x{}", HexDisplay::from(&data));
+        }
+        Some(Subcommand::Account32Hash(cmd)) => {
+            let sender_multilocation = if let Some(parachain_id) = cmd.parachain_id {
+                MultiLocation {
+                    parents: 1,
+                    interior: X2(
+                        Parachain(parachain_id),
+                        AccountId32 {
+                            network: NetworkId::Any,
+                            id: cmd.account_id_32,
+                        },
+                    ),
+                }
+            } else {
+                MultiLocation {
+                    parents: 1,
+                    interior: X1(AccountId32 {
+                        network: NetworkId::Any,
+                        id: cmd.account_id_32,
+                    }),
+                }
+            };
+
+            // Not important for the functionality, totally redundant
+            struct AnyNetwork;
+            impl Get<NetworkId> for AnyNetwork {
+                fn get() -> NetworkId {
+                    NetworkId::Any
+                }
+            }
+
+            let derived_acc =
+                Account32Hash::<AnyNetwork, AccountId>::convert_ref(&sender_multilocation).unwrap();
+            println!("{}", derived_acc);
         }
         None => {}
     }
