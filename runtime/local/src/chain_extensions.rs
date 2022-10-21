77efd3cd90bbf5658dcaa925d8a2f6fe7a159b5f
Alexander Krupenkin <mail@akru.me>
2022-10-21 07:50:29 +0300

    Refactored chain extensions integration

---
+++ runtime/local/src/chain_extensions.rs
@@ -0,0 +1,13 @@
+//!
+/// Registered WASM contracts chain extensions.
+///
+use pallet_contracts::chain_extension::RegisteredChainExtension;
+use super::Runtime;
+
+pub use pallet_chain_extension_dapps_staking::DappsStakingExtension;
+
+// Following impls defines chain extension IDs.
+
+impl RegisteredChainExtension<Runtime> for DappsStakingExtension<Runtime> {
+    const ID: u16 = 00;
+}
