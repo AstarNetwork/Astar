4b5d8fc2bc0630fa0667352a0804544d3f8c712b
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2022-10-27 00:07:36 +0900

    Enable pallet contracts runtime upgrade migration

--- runtime/local/src/lib.rs
+++ runtime/local/src/lib.rs
@@ -894,6 +894,7 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
+    pallet_contracts::Migration<Runtime>
 >;
 
 impl fp_self_contained::SelfContainedCall for RuntimeCall {
