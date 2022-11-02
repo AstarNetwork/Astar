93e1d818dfbd1be4197d0a97790580b856c59394
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2022-11-02 13:36:46 +0900

    remove local chain storage migration

--- runtime/local/src/lib.rs
+++ runtime/local/src/lib.rs
@@ -894,7 +894,6 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
-    pallet_contracts::Migration<Runtime>,
 >;
 
 impl fp_self_contained::SelfContainedCall for RuntimeCall {
