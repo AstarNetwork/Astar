4b4c4b8058fd43c3873c7b3d950e12d3a4519d9f
Alexander Krupenkin <mail@akru.me>
2022-11-09 12:43:16 +0300

    Fix clippy errors

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -57,7 +57,6 @@ pub use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 pub use sp_runtime::BuildStorage;
 
 mod chain_extensions;
-mod migration;
 mod precompiles;
 mod weights;
 mod xcm_config;
@@ -68,7 +67,6 @@ pub use precompiles::{ShibuyaNetworkPrecompiles, ASSET_PRECOMPILE_ADDRESS_PREFIX
 pub type Precompiles = ShibuyaNetworkPrecompiles<Runtime, ShibuyaAssetLocationIdConverter>;
 
 use chain_extensions::*;
-use migration::*;
 
 /// Constant values used within the runtime.
 pub const MILLISDN: Balance = 1_000_000_000_000_000;
