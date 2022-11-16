0699b002debb980e12cfbdbc4f5ea1146a2a95eb
Dmitry Kashitsyn <korvin@deeptown.org>
2022-11-16 14:37:54 +0600

    Bump versions

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("astar"),
     impl_name: create_runtime_str!("astar"),
     authoring_version: 1,
-    spec_version: 42,
+    spec_version: 43,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
