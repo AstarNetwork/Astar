6e0fe53f9dc43c3bfad8f8a630523f22cd4e7f1e
Nikhil Ranjan <niklabh811@gmail.com>
2022-12-20 15:13:16 +0100

    Version bump

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 84,
+    spec_version: 85,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
