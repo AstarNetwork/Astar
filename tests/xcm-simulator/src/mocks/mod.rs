859c3f26c30192e2dc6680f80e650ec60aaadda1
Dinonard <dino.pacandi@gmail.com>
2023-01-27 10:27:05 +0100

    Init commit

---
+++ tests/xcm-simulator/src/mocks/mod.rs
@@ -0,0 +1,89 @@
+// This file is part of Astar.
+
+// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Astar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Astar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Astar. If not, see <http://www.gnu.org/licenses/>.
+
+mod astar;
+mod msg_queue;
+mod relay_chain;
+
+use polkadot_parachain::primitives::Id as ParaId;
+use sp_runtime::traits::AccountIdConversion;
+use xcm_simulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
+
+pub const ALICE: sp_runtime::AccountId32 = sp_runtime::AccountId32::new([0u8; 32]);
+pub const INITIAL_BALANCE: u128 = 1_000_000_000;
+
+// decl_test_parachain! {
+// 	pub struct ParaA {
+// 		Runtime = parachain::Runtime,
+// 		XcmpMessageHandler = parachain::MsgQueue,
+// 		DmpMessageHandler = parachain::MsgQueue,
+// 		new_ext = para_ext(1),
+// 	}
+// }
+
+// decl_test_parachain! {
+// 	pub struct ParaB {
+// 		Runtime = parachain::Runtime,
+// 		XcmpMessageHandler = parachain::MsgQueue,
+// 		DmpMessageHandler = parachain::MsgQueue,
+// 		new_ext = para_ext(2),
+// 	}
+// }
+
+decl_test_relay_chain! {
+    pub struct Relay {
+        Runtime = relay_chain::Runtime,
+        XcmConfig = relay_chain::XcmConfig,
+        new_ext = relay_ext(),
+    }
+}
+
+decl_test_network! {
+    pub struct MockNet {
+        relay_chain = Relay,
+        parachains = vec![
+            // (1, ParaA),
+            // (2, ParaB),
+        ],
+    }
+}
+
+pub fn para_account_id(id: u32) -> relay_chain::AccountId {
+    ParaId::from(id).into_account_truncating()
+}
+
+pub fn relay_ext() -> sp_io::TestExternalities {
+    use relay_chain::{Runtime, System};
+
+    let mut t = frame_system::GenesisConfig::default()
+        .build_storage::<Runtime>()
+        .unwrap();
+
+    pallet_balances::GenesisConfig::<Runtime> {
+        balances: vec![
+            (ALICE, INITIAL_BALANCE),
+            (para_account_id(1), INITIAL_BALANCE),
+        ],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(t);
+    ext.execute_with(|| System::set_block_number(1));
+    ext
+}
