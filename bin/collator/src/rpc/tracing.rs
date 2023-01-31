11e1c37cb1d247e5de74019686dc0a21929e7148
Alexander Krupenkin <mail@akru.me>
2023-01-31 06:45:54 +0300

    Fix CI errors

--- bin/collator/src/rpc/tracing.rs
+++ bin/collator/src/rpc/tracing.rs
@@ -1,18 +1,18 @@
-// Copyright 2019-2022 PureStake Inc.
-// This file is part of Moonbeam.
+// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-// Moonbeam is free software: you can redistribute it and/or modify
+// Astar is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Moonbeam is distributed in the hope that it will be useful,
+// Astar is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+// along with Astar. If not, see <http://www.gnu.org/licenses/>.
 
 use crate::cli::EthApi as EthApiCmd;
 
