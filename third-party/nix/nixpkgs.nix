851c46c312f4815857627b32a3abcebbf1fe12ff
Ashutosh Varma <ashutoshvarma11@live.com>
2023-02-24 15:37:13 +0530

    chore: switch to `rust-overlay` for nix rust

--- third-party/nix/nixpkgs.nix
+++ third-party/nix/nixpkgs.nix
@@ -1,7 +1,7 @@
-{ moz_overlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+{ rust-overlay ? import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz)
 , nixpkgs ? import (builtins.fetchTarball https://github.com/nixos/nixpkgs-channels/archive/nixos-21.11.tar.gz)
 }:
 
 nixpkgs {
-  overlays = [ moz_overlay ];
+  overlays = [ rust-overlay ];
 }
