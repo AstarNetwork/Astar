771f3377838aa2d4d7fbf4e6915512687bd17370
Ashutosh Varma <ashutoshvarma11@live.com>
2023-02-24 15:37:13 +0530

    chore: switch to `rust-overlay` for nix rust

--- third-party/nix/release.nix
+++ third-party/nix/release.nix
@@ -1,13 +1,9 @@
-{ moz_overlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+{ rust-overlay ? import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz)
 }:
 
 let
-  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  channel = pkgs.rustChannelOf { date = "2022-08-05"; channel = "nightly"; };
+  pkgs = import <nixpkgs> { overlays = [ rust-overlay ]; };
 in {
   inherit pkgs;
-  rust-nightly = channel.rust.override {
-    targets = [ "wasm32-unknown-unknown" ];
-    extensions = [ "rustfmt-preview" ];
-  };
+  rust-nightly = pkgs.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
 }
