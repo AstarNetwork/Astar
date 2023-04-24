f2c09e9bd3abfc4f9e4e3293cc9a34832b0a0705
Dinonard <dino.pacandi@gmail.com>
2023-04-24 08:48:36 +0200

    Preparation for v5.4.0 release

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -169,7 +169,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 96,
+    spec_version: 98,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
