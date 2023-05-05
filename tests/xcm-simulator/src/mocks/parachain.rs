f1a18c5cde009e8fcf206b9c470619e32ab19a10
Shaun Wang <spxwang@gmail.com>
2023-05-05 21:41:26 +1200

    Duplicate docstring to parachain mock.

--- tests/xcm-simulator/src/mocks/parachain.rs
+++ tests/xcm-simulator/src/mocks/parachain.rs
@@ -577,6 +577,7 @@ impl pallet_xcm::Config for Runtime {
     type ReachableDest = ReachableDest;
 }
 
+/// Convert `AccountId` to `MultiLocation`.
 pub struct AccountIdToMultiLocation;
 impl Convert<AccountId, MultiLocation> for AccountIdToMultiLocation {
     fn convert(account: AccountId) -> MultiLocation {
@@ -589,6 +590,7 @@ impl Convert<AccountId, MultiLocation> for AccountIdToMultiLocation {
 }
 
 parameter_types! {
+    /// The absolute location in perspective of the whole network.
     pub ShidenLocationAbsolute: MultiLocation = MultiLocation {
         parents: 1,
         interior: X1(
@@ -598,6 +600,8 @@ parameter_types! {
     pub const MaxAssetsForTransfer: usize = 2;
 }
 
+/// Convert `AssetId` to optional `MultiLocation`. The impl is a wrapper
+/// on `ShibuyaAssetLocationIdConverter`.
 pub struct AssetIdConvert;
 impl Convert<AssetId, Option<MultiLocation>> for AssetIdConvert {
     fn convert(asset_id: AssetId) -> Option<MultiLocation> {
@@ -617,6 +621,7 @@ impl orml_xtokens::Config for Runtime {
     type BaseXcmWeight = UnitWeightCost;
     type UniversalLocation = UniversalLocation;
     type MaxAssetsForTransfer = MaxAssetsForTransfer;
+    // Default impl. Refer to `orml-xtokens` docs for more details.
     type MinXcmFee = DisabledParachainFee;
     type MultiLocationsFilter = Everything;
     type ReserveProvider = AbsoluteReserveProvider;
