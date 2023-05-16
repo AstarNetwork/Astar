da61516b2e1769a31c5fec348105e6032b0c0765
Shaun Wang <spxwang@gmail.com>
2023-05-16 16:44:58 +1200

    Before and after runtime upgrade hooks.

--- .github/scripts/tests/runtime-upgrade.test.ts
+++ .github/scripts/tests/runtime-upgrade.test.ts
@@ -36,20 +36,36 @@ describe('runtime upgrade', async () => {
         await cxt.teardown();
     });
 
+    // Execution hook before runtime upgrade. To test storage migrations, set up the storage items
+    // via transactions, set storage etc.
+    const beforeUpgrade = async () => {};
+
+    // Execution hook after runtime upgrade. To verify storage migrations work, query the migrated
+    // storage items or send transactions that interact with them.
+    const afterUpgrade = async () => {
+        // Dummy test. Update it to test your storage migrations.
+        await api.tx.system.remark('Hello World').signAndSend(alice);
+    };
+
     it('runtime upgrade works', async () => {
+        await beforeUpgrade();
+
         const prevSpecVersion = await specVersion(api);
         console.log('SpecVersion before upgrade: ', prevSpecVersion);
 
         console.log(`Upgrading ${runtime} runtime...`);
         const codePath = path.join(__dirname, `../../../target/release/wbuild/${runtime}-runtime/${runtime}_runtime.compact.compressed.wasm`)
         const code = readFileSync(codePath);
-
         await api.tx.sudo.sudoUncheckedWeight(api.tx.system.setCode('0x' + code.toString('hex')), 0).signAndSend(alice);
-        await dev.newBlock({ count: 2 })
 
+        // Do block production.
+        await dev.newBlock({ count: 2 });
+
+        // The spec version is increased.
         const curSpecVersion = await specVersion(api);
         console.log('SpecVersion before upgrade: ', curSpecVersion);
-
         expect(curSpecVersion).toBeGreaterThan(prevSpecVersion);
+
+        await afterUpgrade();
     });
 });
