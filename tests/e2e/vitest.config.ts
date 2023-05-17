445ff83599c5eb275b197fe9b685f42950311223
Shaun Wang <spxwang@gmail.com>
2023-05-17 19:48:56 +1200

    Refactor.

--- tests/e2e/vitest.config.ts
+++ tests/e2e/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         hookTimeout: 30000,
-        testTimeout: 120000,
+        testTimeout: 180000,
     }
 })
