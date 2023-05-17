74d9d0e05ca726b8762f14cec9e3013cd68d331d
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2023-05-17 13:07:08 +0530

    change spec_version for local back to 1

--- runtime/local/src/lib.rs
+++ runtime/local/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("local"),
     impl_name: create_runtime_str!("local"),
     authoring_version: 1,
-    spec_version: 2,
+    spec_version: 1,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
