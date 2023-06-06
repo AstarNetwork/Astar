daf66996f52fc6f70e18a8151ad7c349ff87539b
Dino Pacandi <dino.pacandi@gmail.com>
2023-06-06 15:04:26 +0200

    Bump shiden spec version

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -139,7 +139,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 99,
+    spec_version: 100,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
