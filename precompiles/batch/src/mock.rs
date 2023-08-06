b04e1a808e9489c2d6f1e43a50775d9ca557f250
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2023-08-06 21:26:05 +0530

    update pallet_balances::config in mock

--- precompiles/batch/src/mock.rs
+++ precompiles/batch/src/mock.rs
@@ -98,6 +98,10 @@ impl pallet_balances::Config for Runtime {
     type ExistentialDeposit = ExistentialDeposit;
     type AccountStore = System;
     type WeightInfo = ();
+    type HoldIdentifier = ();
+    type FreezeIdentifier = ();
+    type MaxHolds = ();
+    type MaxFreezes = ();
 }
 
 pub fn precompile_address() -> H160 {
