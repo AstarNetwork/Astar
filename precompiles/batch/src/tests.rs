4044dbfd147cc390646398faf5aca223cf5317bb
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2023-08-07 14:26:05 +0530

    update license

--- precompiles/batch/src/tests.rs
+++ precompiles/batch/src/tests.rs
@@ -1,9 +1,8 @@
 // This file is part of Astar.
 
-// Copyright 2019-2022 PureStake Inc.
-// Copyright (C) 2022-2023 Stake Technologies Pte.Ltd.
+// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later
-//
+
 // Astar is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +15,24 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Astar. If not, see <http://www.gnu.org/licenses/>.
+
+// Copyright 2019-2022 PureStake Inc.
+// Copyright 2022 Stake Technologies
+// This file is part of pallet-evm-precompile-batch package, originally developed by Purestake Inc.
+// pallet-evm-precompile-batch package used in Astar Network in terms of GPLv3.
+//
+// pallet-evm-precompile-batch is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// pallet-evm-precompile-batch is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with pallet-evm-precompile-batch.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::mock::{precompile_address, BatchPrecompileMock, ExtBuilder, PrecompilesValue, Runtime};
 use crate::{log_subcall_failed, log_subcall_succeeded, Mode, *};
