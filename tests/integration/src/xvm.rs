72cff6dafcf26cd1bb352494a7d16b878bcf7dbf
Shaun Wang <spxwang@gmail.com>
2023-08-30 21:01:30 +1000

    Add wasm dummy error contract source code.

--- tests/integration/src/xvm.rs
+++ tests/integration/src/xvm.rs
@@ -477,6 +477,37 @@ fn evm_call_via_xvm_fails_if_revert() {
     });
 }
 
+/* Dummy Error:
+
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod dummy_error {
+
+    #[ink(storage)]
+    pub struct DummyError {}
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        #[codec(index = 7)]
+        DummyError,
+    }
+
+    impl DummyError {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message, selector = 42)]
+        pub fn do_revert(&self) -> Result<(), Error> {
+            Err(Error::DummyError)
+        }
+    }
+}
+
+ */
 const WASM_DUMMY_ERROR_NAME: &'static str = "dummy_error";
 
 #[test]
