c4ce8f1d26d95acd0651ee3566593d9c14352a99
Dino Pačandi <3002868+Dinonard@users.noreply.github.com>
2023-09-18 22:51:18 +0200

    Remove unused XCM consts (#1026)

--- runtime/astar/src/xcm_config.rs
+++ runtime/astar/src/xcm_config.rs
@@ -20,7 +20,6 @@ use super::{
     AccountId, AllPalletsWithSystem, AssetId, Assets, AstarAssetLocationIdConverter, Balance,
     Balances, DealWithFees, ParachainInfo, ParachainSystem, PolkadotXcm, Runtime, RuntimeCall,
     RuntimeEvent, RuntimeOrigin, TreasuryAccountId, WeightToFee, XcAssetConfig, XcmpQueue,
-    MAXIMUM_BLOCK_WEIGHT,
 };
 use frame_support::{
     match_types, parameter_types,
@@ -268,10 +267,6 @@ impl xcm_executor::Config for XcmConfig {
     type UniversalAliases = Nothing;
     type CallDispatcher = WithOriginFilter<SafeCallFilter>;
     type SafeCallFilter = SafeCallFilter;
-}
-
-parameter_types! {
-    pub const MaxDownwardMessageWeight: Weight = MAXIMUM_BLOCK_WEIGHT.saturating_div(10);
 }
 
 /// Local origins on this chain are allowed to dispatch XCM sends/executions.
