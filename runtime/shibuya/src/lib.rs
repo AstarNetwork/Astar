08a4b7de6749dd5dac5cb293fc1d1fda7b89b796
Dino Pačandi <3002868+Dinonard@users.noreply.github.com>
2023-11-08 09:59:39 +0100

    Introduce precompile v2 utils & minor refactoring (#1081)

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -166,7 +166,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 113,
+    spec_version: 114,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
