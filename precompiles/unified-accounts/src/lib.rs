e0f9c3a7b711995fff251e26cdeb86c54b568f18
Ashutosh Varma <ashutoshvarma11@live.com>
2023-12-12 15:39:45 +0530

    feat: update unified accounts PR

--- precompiles/unified-accounts/src/lib.rs
+++ precompiles/unified-accounts/src/lib.rs
@@ -20,54 +20,21 @@
 
 use astar_primitives::evm::{UnifiedAddress, UnifiedAddressMapper};
 use core::marker::PhantomData;
-use fp_evm::Precompile;
-use fp_evm::{PrecompileHandle, PrecompileOutput};
+use fp_evm::PrecompileHandle;
 use frame_support::dispatch::Dispatchable;
 use frame_support::traits::IsType;
-use precompile_utils::{
-    succeed, Address, EvmDataWriter, EvmResult, FunctionModifier, PrecompileHandleExt,
-};
+use precompile_utils::prelude::*;
 use sp_core::{crypto::AccountId32, H256};
-use sp_std::prelude::*;
 
 #[cfg(test)]
 mod mock;
 #[cfg(test)]
 mod tests;
 
-#[precompile_utils::generate_function_selector]
-#[derive(Debug, PartialEq)]
-pub enum Action {
-    GetEvmAddressOrDefault = "get_evm_address_or_default(bytes32)",
-    GetNativeAddressOrDefault = "get_native_address_or_default(address)",
-}
-
 /// A precompile that expose AU related functions.
 pub struct UnifiedAccountsPrecompile<T, UA>(PhantomData<(T, UA)>);
 
-impl<R, UA> Precompile for UnifiedAccountsPrecompile<R, UA>
-where
-    R: pallet_evm::Config + pallet_unified_accounts::Config,
-    <<R as frame_system::Config>::RuntimeCall as Dispatchable>::RuntimeOrigin:
-        From<Option<R::AccountId>>,
-    <R as frame_system::Config>::AccountId: IsType<AccountId32>,
-    UA: UnifiedAddressMapper<R::AccountId>,
-{
-    fn execute(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
-        log::trace!(target: "au-precompile", "Execute input = {:?}", handle.input());
-
-        let selector = handle.read_selector()?;
-
-        handle.check_function_modifier(FunctionModifier::View)?;
-
-        match selector {
-            // Dispatchables
-            Action::GetEvmAddressOrDefault => Self::get_evm_address_or_default(handle),
-            Action::GetNativeAddressOrDefault => Self::get_native_address_or_default(handle),
-        }
-    }
-}
-
+#[precompile_utils::precompile]
 impl<R, UA> UnifiedAccountsPrecompile<R, UA>
 where
     R: pallet_evm::Config + pallet_unified_accounts::Config,
@@ -76,31 +43,31 @@ where
     <R as frame_system::Config>::AccountId: IsType<AccountId32>,
     UA: UnifiedAddressMapper<R::AccountId>,
 {
+    #[precompile::public("get_evm_address_or_default(bytes32)")]
+    #[precompile::view]
     fn get_evm_address_or_default(
-        handle: &mut impl PrecompileHandle,
-    ) -> EvmResult<PrecompileOutput> {
-        let mut input = handle.read_input()?;
-        input.expect_arguments(1)?;
-        let account_id = AccountId32::new(input.read::<H256>()?.into()).into();
+        _: &mut impl PrecompileHandle,
+        account_id: H256,
+    ) -> EvmResult<(Address, bool)> {
+        let account_id = AccountId32::new(account_id.into()).into();
 
         let output: (Address, bool) = match UA::to_h160_or_default(&account_id) {
             UnifiedAddress::Mapped(address) => (address.into(), true),
             UnifiedAddress::Default(address) => (address.into(), false),
         };
-        Ok(succeed(EvmDataWriter::new().write(output).build()))
+        Ok(output)
     }
 
+    #[precompile::public("get_native_address_or_default(address)")]
+    #[precompile::view]
     fn get_native_address_or_default(
-        handle: &mut impl PrecompileHandle,
-    ) -> EvmResult<PrecompileOutput> {
-        let mut input = handle.read_input()?;
-        input.expect_arguments(1)?;
-        let evm_address = input.read::<Address>()?;
-
+        _: &mut impl PrecompileHandle,
+        evm_address: Address,
+    ) -> EvmResult<(H256, bool)> {
         let output: (H256, bool) = match UA::to_account_id_or_default(&evm_address.into()) {
             UnifiedAddress::Mapped(account_id) => (H256::from(account_id.into().as_ref()), true),
             UnifiedAddress::Default(account_id) => (H256::from(account_id.into().as_ref()), false),
         };
-        Ok(succeed(EvmDataWriter::new().write(output).build()))
+        Ok(output)
     }
 }
