4f4596404c2cdd144cbd629585be35eb1bfc12fc
Ashutosh Varma <ashutoshvarma11@live.com>
2024-01-10 03:06:30 +0530

    feat: add generic xcm benchmarks wrapper

---
+++ pallets/astar-xcm-benchmarks/src/generic/benchmarking.rs
@@ -0,0 +1,193 @@
+// This file is part of Astar.
+
+// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Astar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Astar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Astar. If not, see <http://www.gnu.org/licenses/>.
+
+use super::*;
+use frame_benchmarking::v2::*;
+// use frame_benchmarking::{benchmarks, BenchmarkError, BenchmarkResult};
+use frame_support::dispatch::Weight;
+use pallet_xcm_benchmarks::{new_executor, XcmCallOf};
+use sp_std::vec;
+use sp_std::vec::Vec;
+use xcm::latest::prelude::*;
+
+#[benchmarks]
+mod benchmarks {
+    use super::*;
+
+    /// We need re-write buy_execution benchmark becuase our runtime
+    /// needs 1 additional DB read (XcAssetConfig) for fetching unit per sec
+    /// for a fungible asset. The upstream benchmark use native assets thus
+    /// won't accout for it.
+    #[benchmark]
+    fn buy_execution() -> Result<(), BenchmarkError> {
+        let holding = T::worst_case_holding(0).into();
+
+        let mut executor = new_executor::<T>(Default::default());
+        executor.set_holding(holding);
+
+        // A fungible asset
+        let fee_asset = Concrete(MultiLocation::parent());
+
+        let instruction = Instruction::<XcmCallOf<T>>::BuyExecution {
+            fees: (fee_asset, 100_000_000u128).into(), // should be something inside of holding
+            weight_limit: WeightLimit::Unlimited,
+        };
+
+        let xcm = Xcm(vec![instruction]);
+
+        #[block]
+        {
+            executor.bench_process(xcm)?;
+        }
+        // The completion of execution above is enough to validate this is completed.
+        Ok(())
+    }
+
+    /// Re-write as upstream one has hardcoded system pallet index as 1 whereas our runtimes
+    /// uses index 10.
+    #[benchmark]
+    fn expect_pallet() -> Result<(), BenchmarkError> {
+        let mut executor = new_executor::<T>(Default::default());
+
+        let instruction = Instruction::ExpectPallet {
+            // used index 10 for our runtimes
+            index: 10,
+            name: b"System".to_vec(),
+            module_name: b"frame_system".to_vec(),
+            crate_major: 4,
+            min_crate_minor: 0,
+        };
+        let xcm = Xcm(vec![instruction]);
+
+        #[block]
+        {
+            executor.bench_process(xcm)?;
+        }
+        Ok(())
+    }
+
+    #[benchmark]
+    fn exchange_asset() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn export_message() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn lock_asset() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn unlock_asset() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn note_unlockable() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn request_unlock() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    #[benchmark]
+    fn universal_origin() -> Result<(), BenchmarkError> {
+        #[block]
+        {}
+        Err(BenchmarkError::Override(BenchmarkResult::from_weight(
+            Weight::MAX,
+        )))
+    }
+
+    impl_benchmark_test_suite!(
+        Pallet,
+        crate::generic::mock::new_test_ext(),
+        crate::generic::mock::Test
+    );
+}
+
+pub struct XcmGenericBenchmarks<T>(sp_std::marker::PhantomData<T>);
+// Benchmarks wrapper
+impl<T: Config> frame_benchmarking::Benchmarking for XcmGenericBenchmarks<T> {
+    fn benchmarks(extra: bool) -> Vec<frame_benchmarking::BenchmarkMetadata> {
+        // all the generic xcm benchmarks
+        use pallet_xcm_benchmarks::generic::Pallet as PalletXcmGenericBench;
+        PalletXcmGenericBench::<T>::benchmarks(extra)
+    }
+    fn run_benchmark(
+        extrinsic: &[u8],
+        c: &[(frame_benchmarking::BenchmarkParameter, u32)],
+        whitelist: &[frame_benchmarking::TrackedStorageKey],
+        verify: bool,
+        internal_repeats: u32,
+    ) -> Result<Vec<frame_benchmarking::BenchmarkResult>, frame_benchmarking::BenchmarkError> {
+        use pallet_xcm_benchmarks::generic::Pallet as PalletXcmGenericBench;
+
+        use crate::generic::Pallet as AstarXcmGenericBench;
+        if AstarXcmGenericBench::<T>::benchmarks(true)
+            .iter()
+            .any(|x| x.name == extrinsic)
+        {
+            AstarXcmGenericBench::<T>::run_benchmark(
+                extrinsic,
+                c,
+                whitelist,
+                verify,
+                internal_repeats,
+            )
+        } else {
+            PalletXcmGenericBench::<T>::run_benchmark(
+                extrinsic,
+                c,
+                whitelist,
+                verify,
+                internal_repeats,
+            )
+        }
+    }
+}
---
+++ pallets/astar-xcm-benchmarks/src/mock.rs
@@ -0,0 +1,92 @@
+// This file is part of Astar.
+
+// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Astar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Astar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Astar. If not, see <http://www.gnu.org/licenses/>.
+
+use frame_support::{dispatch::Weight, parameter_types, traits::ContainsPair};
+use xcm::latest::prelude::*;
+
+// An xcm sender/receiver akin to > /dev/null
+pub struct DevNull;
+impl SendXcm for DevNull {
+    type Ticket = ();
+
+    fn validate(
+        _destination: &mut Option<MultiLocation>,
+        _message: &mut Option<opaque::Xcm>,
+    ) -> SendResult<Self::Ticket> {
+        Ok(((), MultiAssets::new()))
+    }
+
+    fn deliver(_: Self::Ticket) -> Result<XcmHash, SendError> {
+        Ok(XcmHash::default())
+    }
+}
+
+impl xcm_executor::traits::OnResponse for DevNull {
+    fn expecting_response(_: &MultiLocation, _: u64, _: Option<&MultiLocation>) -> bool {
+        false
+    }
+    fn on_response(
+        _: &MultiLocation,
+        _: u64,
+        _: Option<&MultiLocation>,
+        _: Response,
+        _: Weight,
+        _: &XcmContext,
+    ) -> Weight {
+        Weight::zero()
+    }
+}
+
+pub struct AccountIdConverter;
+impl xcm_executor::traits::Convert<MultiLocation, u64> for AccountIdConverter {
+    fn convert(ml: MultiLocation) -> Result<u64, MultiLocation> {
+        match ml {
+            MultiLocation {
+                parents: 0,
+                interior: X1(Junction::AccountId32 { id, .. }),
+            } => Ok(<u64 as parity_scale_codec::Decode>::decode(&mut &*id.to_vec()).unwrap()),
+            _ => Err(ml),
+        }
+    }
+
+    fn reverse(acc: u64) -> Result<MultiLocation, u64> {
+        Err(acc)
+    }
+}
+
+parameter_types! {
+    pub Ancestry: MultiLocation = Junction::Parachain(101).into();
+    pub UnitWeightCost: u64 = 10;
+    pub WeightPrice: (AssetId, u128, u128) = (Concrete(MultiLocation::parent()), 1_000_000, 1024);
+}
+
+pub struct AllAssetLocationsPass;
+impl ContainsPair<MultiAsset, MultiLocation> for AllAssetLocationsPass {
+    fn contains(_: &MultiAsset, _: &MultiLocation) -> bool {
+        true
+    }
+}
+
+#[cfg(feature = "runtime-benchmarks")]
+pub fn mock_worst_case_holding() -> MultiAssets {
+    let assets: Vec<MultiAsset> = vec![MultiAsset {
+        id: Concrete(MultiLocation::parent()),
+        fun: Fungible(u128::MAX),
+    }];
+    assets.into()
+}
