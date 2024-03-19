15ec557a6b1ef122977c5a73ddfcfe10387908a0
Dino Pacandi <dino.pacandi@gmail.com>
2024-03-19 08:31:10 +0100

    v5.35.0 release prep

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 121,
+    spec_version: 122,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
