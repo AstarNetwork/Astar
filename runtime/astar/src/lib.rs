e72e37d70660aa4b31a3b2cec055d7ea4463b8bb
Dino Pacandi <dino.pacandi@gmail.com>
2024-04-04 09:57:15 +0200

    Prep for v5.36.0 release

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -148,7 +148,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("astar"),
     impl_name: create_runtime_str!("astar"),
     authoring_version: 1,
-    spec_version: 83,
+    spec_version: 84,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -174,7 +174,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 125,
+    spec_version: 126,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
