a642a443dccf2b067d2717407642c639797baacc
Ermal Kaleci <ermalkaleci@gmail.com>
2024-08-09 16:58:17 +0200

    prep v5.43.0 (#1323)
    * prep v5.43.0
    
    * bump spec_version

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -174,7 +174,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shiden"),
     impl_name: create_runtime_str!("shiden"),
     authoring_version: 1,
-    spec_version: 130,
+    spec_version: 131,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
