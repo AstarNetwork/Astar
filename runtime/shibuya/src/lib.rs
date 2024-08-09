9d145c6c66a0693ceb8bc7bd15239f21405cd8f0
Ermal Kaleci <ermalkaleci@gmail.com>
2024-08-09 16:29:18 +0200

    bump spec_version

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -204,7 +204,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 133,
+    spec_version: 134,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
