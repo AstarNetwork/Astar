106fea80c9b39393055f344a9b0e483d7f333555
ermalkaleci <ermalkaleci@users.noreply.github.com>
2024-08-27 13:52:28 +0000

    deploy: d2714e5f0d318a998096d9fc436347365bf5d4b8

---
+++ trait.impl/frame_support/migrations/trait.FailedMigrationHandler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"astar_primitives":[["impl FailedMigrationHandler for <a class=\"struct\" href=\"astar_primitives/struct.UnfreezeChainOnFailedMigration.html\" title=\"struct astar_primitives::UnfreezeChainOnFailedMigration\">UnfreezeChainOnFailedMigration</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
