bc39f9ce38524f0577d0468b982f436aed9050c5
Ermal Kaleci <ermalkaleci@gmail.com>
2024-09-08 19:22:05 +0200

    prep v5.44.0 (#1353)
    * prep v5.44.0
    
    * update chain_spec

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -204,7 +204,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 136,
+    spec_version: 137,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
@@ -761,7 +761,7 @@ impl pallet_contracts::Config for Runtime {
     type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
     type Debug = ();
     type Environment = ();
-    type Migrations = (pallet_contracts::migration::v16::Migration<Runtime>,);
+    type Migrations = ();
     type Xcm = ();
     type UploadOrigin = EnsureSigned<<Self as frame_system::Config>::AccountId>;
     type InstantiateOrigin = EnsureSigned<<Self as frame_system::Config>::AccountId>;
