02324f594395bd293b37ad2181d0a4392daefdc9
ipapandinas <ipapandinas@users.noreply.github.com>
2024-09-16 10:18:12 +0000

    deploy: ae88fd40253d7d98ef80548598a16655ca4fa618

---
+++ trait.impl/staging_xcm_executor/config/trait.Config.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"astar_runtime":[["impl Config for <a class=\"struct\" href=\"astar_runtime/xcm_config/struct.XcmConfig.html\" title=\"struct astar_runtime::xcm_config::XcmConfig\">XcmConfig</a>"]],
+"shibuya_runtime":[["impl Config for <a class=\"struct\" href=\"shibuya_runtime/xcm_config/struct.XcmConfig.html\" title=\"struct shibuya_runtime::xcm_config::XcmConfig\">XcmConfig</a>"]],
+"shiden_runtime":[["impl Config for <a class=\"struct\" href=\"shiden_runtime/xcm_config/struct.XcmConfig.html\" title=\"struct shiden_runtime::xcm_config::XcmConfig\">XcmConfig</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
