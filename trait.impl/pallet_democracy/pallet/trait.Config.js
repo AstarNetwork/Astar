c359e6e5745539e9a700ab5f625d8e18d7e128ba
Dinonard <Dinonard@users.noreply.github.com>
2024-11-21 15:07:41 +0000

    deploy: d72239b6ac78a8454de0fab2b6ab0c9846b3a72e

--- trait.impl/pallet_democracy/pallet/trait.Config.js
+++ trait.impl/pallet_democracy/pallet/trait.Config.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {
+"astar_runtime":[["impl Config for <a class=\"struct\" href=\"astar_runtime/struct.Runtime.html\" title=\"struct astar_runtime::Runtime\">Runtime</a>"]],
 "local_runtime":[["impl Config for <a class=\"struct\" href=\"local_runtime/struct.Runtime.html\" title=\"struct local_runtime::Runtime\">Runtime</a>"]],
 "shibuya_runtime":[["impl Config for <a class=\"struct\" href=\"shibuya_runtime/struct.Runtime.html\" title=\"struct shibuya_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
