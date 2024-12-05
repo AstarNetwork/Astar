0d0c9bfded266bcd1c6ee96cd73dfea8654a9e7f
Dinonard <Dinonard@users.noreply.github.com>
2024-12-05 16:35:01 +0000

    deploy: 29b3bd6a702876d45f5b6dc8dd5450f6b84d81e9

--- astar_primitives/evm/sidebar-items.js
+++ astar_primitives/evm/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["EVM_REVERT_CODE"],"enum":["UnifiedAddress"],"struct":["EvmRevertCodeHandler","HashedDefaultMappings"],"trait":["UnifiedAddressMapper"],"type":["EvmAddress"]};
+window.SIDEBAR_ITEMS = {"constant":["EVM_REVERT_CODE"],"enum":["UnifiedAddress"],"struct":["EVMFungibleAdapterWrapper","EvmRevertCodeHandler","HashedDefaultMappings"],"trait":["UnifiedAddressMapper"],"type":["EvmAddress"]};
