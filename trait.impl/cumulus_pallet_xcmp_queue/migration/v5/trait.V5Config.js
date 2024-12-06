215ea9ca62ff50e0847d494e555802ce57f59629
Dinonard <Dinonard@users.noreply.github.com>
2024-12-06 13:07:09 +0000

    deploy: d3f8fd5ab6509a75382db7527de5d8d015e706d8

--- trait.impl/cumulus_pallet_xcmp_queue/migration/v5/trait.V5Config.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"astar_runtime":[["impl V5Config for <a class=\"struct\" href=\"astar_runtime/struct.Runtime.html\" title=\"struct astar_runtime::Runtime\">Runtime</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
