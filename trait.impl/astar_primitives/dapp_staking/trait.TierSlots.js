6ae284910f86e4e255484f7a49b2848788336da8
Dinonard <Dinonard@users.noreply.github.com>
2025-02-19 15:04:56 +0000

    deploy: 8b238f8af9bc976c45b20028aa5d3cdc98d327f7

--- trait.impl/astar_primitives/dapp_staking/trait.TierSlots.js
+++ trait.impl/astar_primitives/dapp_staking/trait.TierSlots.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"astar_primitives":[],
-"shiden_runtime":[["impl <a class=\"trait\" href=\"astar_primitives/dapp_staking/trait.TierSlots.html\" title=\"trait astar_primitives::dapp_staking::TierSlots\">TierSlots</a> for <a class=\"struct\" href=\"shiden_runtime/struct.ShidenTierSlots.html\" title=\"struct shiden_runtime::ShidenTierSlots\">ShidenTierSlots</a>"]]
+"astar_primitives":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
