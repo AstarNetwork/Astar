a0a635ecdc0323dd750532ff2860e78b1abf13e6
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2025-03-04 15:21:06 +0100

    runtime-1302 prep

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -206,7 +206,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("shibuya"),
     impl_name: create_runtime_str!("shibuya"),
     authoring_version: 1,
-    spec_version: 1301,
+    spec_version: 1302,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
