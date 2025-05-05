de93437069543f0d86779dd0bea937e8e1200440
Ashutosh Varma <ashutoshvarma11@live.com>
2025-05-05 18:17:08 +0530

    feat: revert spec version change

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -210,7 +210,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shibuya"),
     impl_name: Cow::Borrowed("shibuya"),
     authoring_version: 1,
-    spec_version: 1500,
+    spec_version: 1401,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
