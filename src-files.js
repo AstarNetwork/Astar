bd080085ab630a145033d39f9d87f28e28a6a17d
ashutoshvarma <ashutoshvarma@users.noreply.github.com>
2025-05-26 15:46:44 +0000

    deploy: 753bff0f5616bb86c09f92d894f1f4404720f8c8

--- src-files.js
+++ src-files.js
@@ -2,7 +2,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["assets_chain_extension_types",["",[],["lib.rs"]]],\
 ["astar_collator",["",[["local",[],["chain_spec.rs","mod.rs","service.rs"]],["parachain",[["chain_spec",[],["astar.rs","mod.rs","shibuya.rs","shiden.rs"]]],["fake_runtime_api.rs","mod.rs","service.rs","shell_upgrade.rs"]],["rpc",[],["tracing.rs"]]],["cli.rs","command.rs","evm_tracing_types.rs","lib.rs","rpc.rs"]]],\
 ["astar_primitives",["",[["xcm",[],["mod.rs"]]],["dapp_staking.rs","ethereum_checked.rs","evm.rs","genesis.rs","governance.rs","lib.rs","oracle.rs","parachain.rs","precompiles.rs","testing.rs"]]],\
-["astar_runtime",["",[["weights",[["xcm",[],["fungible.rs","generic.rs","mod.rs"]]],["frame_system_extensions.rs","mod.rs","orml_oracle.rs","pallet_assets.rs","pallet_dapp_staking.rs","pallet_inflation.rs","pallet_transaction_payment.rs","pallet_xcm.rs"]]],["chain_extensions.rs","genesis_config.rs","lib.rs","precompiles.rs","xcm_config.rs"]]],\
+["astar_runtime",["",[["weights",[["xcm",[],["fungible.rs","generic.rs","mod.rs"]]],["frame_system_extensions.rs","mod.rs","orml_oracle.rs","pallet_assets.rs","pallet_dapp_staking.rs","pallet_inflation.rs","pallet_scheduler.rs","pallet_transaction_payment.rs","pallet_xcm.rs"]]],["chain_extensions.rs","genesis_config.rs","lib.rs","precompiles.rs","xcm_config.rs"]]],\
 ["astar_test_utils",["",[],["lib.rs"]]],\
 ["astar_xcm_benchmarks",["",[["fungible",[],["mod.rs"]],["generic",[],["mod.rs"]]],["lib.rs"]]],\
 ["dapp_staking_runtime_api",["",[],["lib.rs"]]],\
@@ -40,7 +40,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["pallet_treasury",["",[],["lib.rs","weights.rs"]]],\
 ["pallet_unified_accounts",["",[],["lib.rs","weights.rs"]]],\
 ["pallet_xc_asset_config",["",[],["lib.rs","migrations.rs","weights.rs"]]],\
-["shibuya_runtime",["",[["weights",[["xcm",[],["fungible.rs","generic.rs","mod.rs"]]],["frame_system_extensions.rs","mod.rs","orml_oracle.rs","pallet_assets.rs","pallet_balances.rs","pallet_dapp_staking.rs","pallet_inflation.rs","pallet_transaction_payment.rs","pallet_xcm.rs"]]],["chain_extensions.rs","genesis_config.rs","lib.rs","precompiles.rs","xcm_config.rs"]]],\
+["shibuya_runtime",["",[["weights",[["xcm",[],["fungible.rs","generic.rs","mod.rs"]]],["frame_system_extensions.rs","mod.rs","orml_oracle.rs","pallet_assets.rs","pallet_balances.rs","pallet_dapp_staking.rs","pallet_inflation.rs","pallet_scheduler.rs","pallet_transaction_payment.rs","pallet_xcm.rs"]]],["chain_extensions.rs","genesis_config.rs","lib.rs","precompiles.rs","xcm_config.rs"]]],\
 ["shiden_runtime",["",[["weights",[["xcm",[],["fungible.rs","generic.rs","mod.rs"]]],["frame_system_extensions.rs","mod.rs","orml_oracle.rs","pallet_assets.rs","pallet_dapp_staking.rs","pallet_inflation.rs","pallet_transaction_payment.rs","pallet_xcm.rs"]]],["chain_extensions.rs","genesis_config.rs","lib.rs","precompiles.rs","xcm_config.rs"]]],\
 ["unified_accounts_chain_extension_types",["",[],["lib.rs"]]],\
 ["vesting_mbm",["",[],["lib.rs","weights.rs"]]],\
