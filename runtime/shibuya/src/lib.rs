0aacb3f8a45fb58f4185daeb3c4400bec3b9cb40
Dino Pacandi <dino.pacandi@gmail.com>
2025-06-23 09:22:16 +0200

    runtime-1601 prep

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -210,7 +210,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shibuya"),
     impl_name: Cow::Borrowed("shibuya"),
     authoring_version: 1,
-    spec_version: 1600,
+    spec_version: 1601,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
