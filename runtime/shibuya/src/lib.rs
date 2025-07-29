6e3a6474ba6d7c055f0922defc5323fb6fd82228
PierreOssun <pierre.giraud@mailfence.com>
2025-07-29 14:11:30 +0200

    runtime-1700 prep

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -211,7 +211,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shibuya"),
     impl_name: Cow::Borrowed("shibuya"),
     authoring_version: 1,
-    spec_version: 1602,
+    spec_version: 1700,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
