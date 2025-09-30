bd0907fce9b001240e7a4c7a8ff6bf4c57e085a8
PierreOssun <pierre.giraud@mailfence.com>
2025-09-30 10:39:15 +0200

    runtime-1800

--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -184,7 +184,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shiden"),
     impl_name: Cow::Borrowed("shiden"),
     authoring_version: 1,
-    spec_version: 1700,
+    spec_version: 1800,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
