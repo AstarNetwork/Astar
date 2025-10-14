5890cd53df9e77ed034c2428241486e8cfabac0c
Ashutosh Varma <ashutoshvarma11@live.com>
2025-10-14 19:21:39 +0900

    fix: revert bacl unreleased migration from astar

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -1735,7 +1735,8 @@ parameter_types! {
 }
 
 /// Unreleased migrations. Add new ones here:
-pub type Unreleased = ();
+pub type Unreleased =
+    (pallet_inflation::migration::versioned_migrations::V1ToV2<Runtime, DecayRate, DecayFactor>,);
 
 /// Migrations/checks that do not need to be versioned and can run on every upgrade.
 pub type Permanent = (pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,);
