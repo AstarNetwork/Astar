25580a806d8416ff7ec18ff14fc48d3054152517
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-02-20 18:42:31 +0400

    chore: bump deps (#1592)
    * bump deps
    
    * fix deps

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -195,7 +195,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("astar"),
     impl_name: Cow::Borrowed("astar"),
     authoring_version: 1,
-    spec_version: 2100,
+    spec_version: 2101,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
