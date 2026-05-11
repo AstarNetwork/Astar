0be9d093c4597a6a832527c64494584c764e91d3
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-05-11 13:12:06 +0100

    chore: release prep runtime 2102

--- runtime/shibuya/src/lib.rs
+++ runtime/shibuya/src/lib.rs
@@ -214,7 +214,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shibuya"),
     impl_name: Cow::Borrowed("shibuya"),
     authoring_version: 1,
-    spec_version: 2101,
+    spec_version: 2102,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
--- runtime/shiden/src/lib.rs
+++ runtime/shiden/src/lib.rs
@@ -186,7 +186,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("shiden"),
     impl_name: Cow::Borrowed("shiden"),
     authoring_version: 1,
-    spec_version: 2101,
+    spec_version: 2102,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
