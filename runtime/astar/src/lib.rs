d9d77834be374b7ffbc723c2983d3f4f6eb108eb
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-06-01 12:43:14 +0100

    chore: release prep runtime 2206 (#1640)

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("astar"),
     impl_name: Cow::Borrowed("astar"),
     authoring_version: 1,
-    spec_version: 2205,
+    spec_version: 2206,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
@@ -545,7 +545,8 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
     type ConsensusHook = ConsensusHook;
     type SelectCore = cumulus_pallet_parachain_system::DefaultCoreSelector<Runtime>;
     type WeightInfo = cumulus_pallet_parachain_system::weights::SubstrateWeight<Runtime>;
-    type RelayParentOffset = RelayParentOffset;
+    // TODO: Set offset to RelayParentOffset once the majority of collators have migrated to slot_based
+    type RelayParentOffset = ConstU32<0>;
 }
 
 type ConsensusHook = cumulus_pallet_aura_ext::FixedVelocityConsensusHook<
