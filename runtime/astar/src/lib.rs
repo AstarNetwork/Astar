88eaa9eb9c4647185bcf5dfe5c249764ddbd4606
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-06-09 16:57:54 +0100

    Update spec_version to 2207

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("astar"),
     impl_name: Cow::Borrowed("astar"),
     authoring_version: 1,
-    spec_version: 2206,
+    spec_version: 2207,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
