32e439855c40de81f9a715aee22b5f43102a4e62
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-07-09 12:08:32 +0200

    chore: prep release runtime 2209

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("astar"),
     impl_name: Cow::Borrowed("astar"),
     authoring_version: 1,
-    spec_version: 2208,
+    spec_version: 2209,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
