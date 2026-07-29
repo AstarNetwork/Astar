b91f763a9847ec048eecd0a2c857b5416f33e1aa
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-07-29 16:11:43 +0100

    Remove migrations (#1668)
    * fix: CI jobs
    
    * remove migrations
    
    * remove migrations Astar/Shiden

--- runtime/astar/src/lib.rs
+++ runtime/astar/src/lib.rs
@@ -1708,10 +1708,7 @@ pub type Executive = frame_executive::Executive<
 pub type Migrations = (Unreleased, Permanent);
 
 /// Unreleased migrations. Add new ones here:
-pub type Unreleased = (
-    cumulus_pallet_xcmp_queue::migration::v6::MigrateV5ToV6<Runtime>,
-    cumulus_pallet_xcmp_queue::migration::v7::MigrateV6ToV7<Runtime>,
-);
+pub type Unreleased = ();
 
 /// Migrations/checks that do not need to be versioned and can run on every upgrade.
 pub type Permanent = (pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,);
